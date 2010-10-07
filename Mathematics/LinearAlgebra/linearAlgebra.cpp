3012105d6e02a8080528cf2ec80e3236e70edd09
Jeroen Melman <hidden@hidden.com>
2010-10-07 09:30:50 +0000

    Small layout changes in linearAlgebra.cpp and .h. Tudat's Qt project file Tudat.pro added.
    git-svn-id: file:///srv/svn/repos/tudat/trunk@21 ac7aa084-b6da-11df-bf42-7fac39f0224f

--- Mathematics/LinearAlgebra/linearAlgebra.cpp
+++ Mathematics/LinearAlgebra/linearAlgebra.cpp
@@ -1,9 +1,9 @@
 /*! \file linearAlgebra.cpp
- *    This file contains include statements for common Eiegn components,
+ *    This file contains include statements for common Eigen components,
  *    typedef for vectors and a number of useful vector operation definitions.
  *
  *    Path              : /Mathematics/LinearAlgebra/
- *    Version           : 2
+ *    Version           : 3
  *    Check status      : Checked
  *
  *    Author            : J. Melman
@@ -24,7 +24,7 @@
  *    Copyright (c) 2010 Delft University of Technology.
  *
  *    This software is protected by national and international copyright.
- *    Any unauthorized use, reproduction or modificaton is unlawful and
+ *    Any unauthorized use, reproduction or modification is unlawful and
  *    will be prosecuted. Commercial and non-private application of the
  *    software in any form is strictly prohibited unless otherwise granted
  *    by the authors.
@@ -36,9 +36,15 @@
  *      YYMMDD    author        comment
  *      090807    J. Melman     First creation of code.
  *      100930    D. Dirkx      Modified to comply with Tudat standards
+ *      100930    J. Melman     Implemented namespace, minor comment
+ *                              changes.
  */
 
 #include "linearAlgebra.h"
+
+//! linear_algebra namespace.
+namespace linear_algebra
+{
 
 //! Determine the cosine of the angle between two vectors.
 double determineCosineOfAngleBetweenVectors( const Vector3d& vector0,
@@ -48,21 +54,19 @@ double determineCosineOfAngleBetweenVectors( const Vector3d& vector0,
     double normOfVector0 = vector0.norm( );
     double normOfVector1 = vector1.norm( );
 
-
     // Normalize both vectors.
     Vector3d vector0Normalized = vector0 / normOfVector0;
     Vector3d vector1Normalized = vector1 / normOfVector1;
 
-    // Get the cosine of the angle by dotting the normalized
-    // vectors.
+    // Get the cosine of the angle by dotting the normalized vectors.
     double dotProductOfNormalizedVectors =
-            vector0Normalized.dot( vector1Normalized );
+    vector0Normalized.dot( vector1Normalized );
 
     // Explicitly define the extreme cases, which can give problems
     // with the acos function.
-    if (dotProductOfNormalizedVectors >= 1.0)
+    if ( dotProductOfNormalizedVectors >= 1.0 )
         return 1.0;
-    else if (dotProductOfNormalizedVectors <= -1.0)
+    else if ( dotProductOfNormalizedVectors <= -1.0 )
         return -1.0;
 
     // Determine the actual angle.
@@ -77,14 +81,13 @@ double determineAngleBetweenVectors( const Vector3d& vector0,
 {
     // Determine the cosine of the angle by using another routine.
     double dotProductOfNormalizedVectors =
-            determineCosineOfAngleBetweenVectors( vector0, vector1 );
+    determineCosineOfAngleBetweenVectors( vector0, vector1 );
 
-    // return arccosine of the above.
+    // Return arccosine of the above, which is effectively the angle.
     return acos( dotProductOfNormalizedVectors );
-
 }
 
-//! Determine the average of the components of a vector
+//! Determine the average of the components of a vector.
 double determineAverageOfVectorComponents( const VectorXd& vector0 )
 {
     return vector0.sum( ) / vector0.rows( );
@@ -101,11 +104,14 @@ double determineStandardDeviationOfVectorComponents( const VectorXd& vector0 )
     // Determine variance of entries.
     for( int i = 0 ; i < vector0.rows( ) ; i++ )
     {
-        varianceOfEntries += pow( ( vector0( i ) -
-                                    averageOfEntries ), 2.0 );
+        varianceOfEntries += pow( ( vector0( i ) - averageOfEntries ), 2.0 );
     }
     varianceOfEntries /= vector0.rows( ) - 1;
 
     // Return square root of variance ( = standard deviation ).
     return sqrt( varianceOfEntries );
 }
+
+}
+
+// End of file.
--- Mathematics/LinearAlgebra/linearAlgebra.h
+++ Mathematics/LinearAlgebra/linearAlgebra.h
@@ -1,9 +1,9 @@
 /*! \file linearAlgebra.h
- *    This file contains include statements for common Eiegn components,
+ *    This file contains include statements for common Eigen components,
  *    typedef for vectors and a number of useful vector operation definitions.
  *
  *    Path              : /Mathematics/LinearAlgebra/
- *    Version           : 2
+ *    Version           : 3
  *    Check status      : Checked
  *
  *    Author            : J. Melman
@@ -24,7 +24,7 @@
  *    Copyright (c) 2010 Delft University of Technology.
  *
  *    This software is protected by national and international copyright.
- *    Any unauthorized use, reproduction or modificaton is unlawful and
+ *    Any unauthorized use, reproduction or modification is unlawful and
  *    will be prosecuted. Commercial and non-private application of the
  *    software in any form is strictly prohibited unless otherwise granted
  *    by the authors.
@@ -35,16 +35,17 @@
  *    Changelog
  *      YYMMDD    author        comment
  *      090807    J. Melman     First creation of code.
- *      100930    D. Dirkx      Modified to comply with Tudat standards
+ *      100930    D. Dirkx      Modified to comply with Tudat standards.
+ *      100930    J. Melman     Implemented namespace, minor comment
+ *                              changes.
  */
 
-
-#ifndef EIGENROUTINES_H_
-#define EIGENROUTINES_H_
+#ifndef LINEAR_ALGEBRA_H
+#define LINEAR_ALGEBRA_H
 
 // Notice that coefficient access methods in Eigen have assertions
 // checking the ranges. So if you do a lot of coefficient access, 
-// these assertion can have an important cost. If you want to 
+// these assertions can have an important cost. If you want to 
 // save cost, define EIGEN_NO_DEBUG, and it won't check assertions.
 //#ifndef EIGEN_NO_DEBUG
 //#define EIGEN_NO_DEBUG
@@ -62,9 +63,16 @@ USING_PART_OF_NAMESPACE_EIGEN
 // by in-house code.
 typedef VectorXd Vector;
 
+//! linear_algebra namespace.
+/*!
+ *  Defintion of linear_algebra namespace.
+ */
+namespace linear_algebra
+{
+
 //! Determine the cosine of the angle between two vectors.
 /*!
- * Function to determine the cosine of the angle between two vectors,
+ * Function to determine the cosine of the angle between two vectors;
  * both vectors must have non-zero norm.
  */
 double determineCosineOfAngleBetweenVectors( const Vector3d& vector0,
@@ -72,13 +80,13 @@ double determineCosineOfAngleBetweenVectors( const Vector3d& vector0,
 
 //! Determine the angle between two vectors.
 /*!
- * Function to determine the angle between two vectors,
+ * Function to determine the angle between two vectors;
  * both vectors must have non-zero norm.
  */
 double determineAngleBetweenVectors( const Vector3d& vector0,
                                      const Vector3d& vector1 );
 
-//! Determine the average of the components of a vector
+//! Determine the average of the components of a vector.
 /*!
  * Function to determine the average (arithmetic mean) of the components of a
  * vector.
@@ -90,5 +98,10 @@ double determineAverageOfVectorComponents( const VectorXd& vector0 );
  * Function to determine the standard deviation of the components of a vector.
  */
 double determineStandardDeviationOfVectorComponents( const VectorXd& vector0 );
+    
+}
 
-#endif // EIGENROUTINES_H
+#endif // LINEAR_ALGEBRA_H
+
+// End of file.
+
