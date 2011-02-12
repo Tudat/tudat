077a777df5cbeeb49bafe5ec84672fee77acf007
Jeroen Melman <hidden@hidden.com>
2011-02-12 12:25:32 +0000

    Added meshed surfaces.
    git-svn-id: file:///srv/svn/repos/tudat/trunk@76 ac7aa084-b6da-11df-bf42-7fac39f0224f

---
+++ Mathematics/GeometricShapes/unitTestLawgsSurfaceGeometry.h
@@ -0,0 +1,53 @@
+/*! \file unitTestLawgsSurfaceGeometry.h
+ *  This file contains the definition of the LawgsPartGeometry unit test.
+ *
+ *  Path              : Mathematics/Geometry/
+ *  Version           : 1
+ *  Check status      : Unchecked
+ *
+ *  Author            : Dominic Dirkx
+ *  Affiliation       : TU Delft
+ *  E-mail address    : D.Dirkx@student.tudelft.nl
+ *
+ *  Checker           : J. Melman
+ *  Affiliation       : Delft University of Technology
+ *  E-mail address    : J.C.P.Melman@tudelft.nl
+ *
+ *  Date created      : 6 February, 2011
+ *  Last modified     : 8 February, 2011
+ *
+ *  References
+ *    "A Description of the Langley Wireframe Geometry Standard (LaWGS) format",
+ *    C.B. Craidon, NASA TECHNICAL MEMORANDUM 85767.
+ *
+ *  Notes
+ *
+ *  Copyright (c) 2010 Delft University of Technology.
+ *
+ *  This software is protected by national and international copyright.
+ *  Any unauthorized use, reproduction or modification is unlawful and
+ *  will be prosecuted. Commercial and non-private application of the
+ *  software in any form is strictly prohibited unless otherwise granted
+ *  by the authors.
+ *
+ *  The code is provided without any warranty; without even the implied
+ *  warranty of merchantibility or fitness for a particular purpose.
+ *
+ *  Changelog
+ *    YYMMDD   Author               Comment
+ *    110206   D. Dirkx             First version of file.
+ *    110208   D. Dirkx             Finalized for code check.
+ */
+
+#ifndef UNITTESTLAWGSSURFACEGEOMETRY_H
+#define UNITTESTLAWGSSURFACEGEOMETRY_H
+
+#include "lawgsPartGeometry.h"
+#include "sphereSegment.h"
+
+namespace unit_tests{
+
+    bool testLawgsSurfaceGeometry( );
+}
+
+#endif // UNITTESTLAWGSSURFACEGEOMETRY_H
