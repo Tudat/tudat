b828f4363ea278cb256dc254ebd3ba81a8feb2fb
Kartik Kumar <me@kartikkumar.com>
2012-05-25 13:17:57 +0000

    Boostified spherical harmonics unit test (issue #409).
    git-svn-id: file:///srv/svn/repos/tudat/trunk@400 ac7aa084-b6da-11df-bf42-7fac39f0224f

--- Tudat/Astrodynamics/Gravitation/UnitTests/unitTestSphericalHarmonicsGravityField.cpp
+++ Tudat/Astrodynamics/Gravitation/UnitTests/unitTestSphericalHarmonicsGravityField.cpp
@@ -26,171 +26,127 @@
  *      110204    K. Kumar          Removed "vector" from naming.
  *      110310    K. Kumar          Changed naming from Laplacian to gradient tensor.
  *      120326    D. Dirkx          Changed raw pointers to shared pointers.
+ *      120521    F. Belien         Boostified unit test.
  *
  *    References
  *
  */
 
-// Temporary notes (move to class/function doxygen):
-// Test runs code and verifies result against expected value.
-// If the tested code is erroneous, the test function returns a boolean
-// true; if the code is correct, the function returns a boolean false.
-// 
-
+#define BOOST_TEST_MAIN
 
 #include <cmath>
 #include <limits>
 
+#include <boost/test/floating_point_comparison.hpp>
+#include <boost/test/unit_test.hpp>
+
 #include <Eigen/Core>
+
+#include <TudatCore/Basics/testMacros.h>
 
 #include "Tudat/Astrodynamics/Bodies/planet.h"
 #include "Tudat/Astrodynamics/Gravitation/gravityFieldModel.h"
 #include "Tudat/Astrodynamics/Gravitation/sphericalHarmonicsGravityField.h"
 
-//! Test implementation of spherical harmonics gravity field class.
-int main( )
+namespace tudat
 {
-    using std::cerr;
-    using std::endl;
-    using std::pow;
-    using std::fabs;
-    using namespace tudat;
+namespace unit_tests
+{
 
-    // Summary of tests.
-    // Test 1: Test setting and getting gravitational parameter.
-    // Test 2: Test getting gravitational parameter for predefined Earth central body gravity
-    //         field.
-    // Test 3: Test getting potential given state.
-    // Test 4: Test getting gradient of potential given state.
-    // Test 5: Test getting gradient tensor of potential given state.
+//! Test the functionality of the spherical harmonics gravity field class.
+BOOST_AUTO_TEST_SUITE( test_spherical_harmonics_gravity_field )
 
-    // Test result initialised to false.
-    bool isSphericalHarmonicsGravityFieldErroneous = false;
+//! Test setting and getting gravitational parameter.
+BOOST_AUTO_TEST_CASE( testSettingAndGettingGravitationalParameter )
+{
+    // Create gravity field for myPlanet.
+    using astrodynamics::gravitation::SphericalHarmonicsGravityField;
+    SphericalHarmonicsGravityField myPlanetGravityField;
 
-    // Set identity matrix.
-    Eigen::Matrix3d identityMatrix;
-    identityMatrix.setIdentity( 3, 3 );
+    // Set gravitational parameter of myPlanet.
+    const double gravitationalParameterOfMyPlanet = 22032.00;
+    myPlanetGravityField.setGravitationalParameter( gravitationalParameterOfMyPlanet );
+
+    // Set origin of gravity field of myPlanet with respect to geometric center.
+    myPlanetGravityField.setOrigin( Eigen::Vector3d::Zero( ) );
+
+    // Get the value for the gravitational parameter
+    const double computedResultForTest1 = myPlanetGravityField.getGravitationalParameter( );
+
+    // Determine the expected result.
+    const double expectedResultForTest1 = gravitationalParameterOfMyPlanet;
+
+    // Check that expected and computed values are equal.
+    BOOST_CHECK_EQUAL( expectedResultForTest1, computedResultForTest1 );
+}
+
+//! Test getting gravitational parameter for predefined Earth central body gravity field.
+BOOST_AUTO_TEST_CASE( testGetGravitationalParameterForPredefinedEarth )
+{
+    // Create predefined Earth central gravity field.
+    using astrodynamics::gravitation::CentralGravityField;
+    CentralGravityField predefinedEarthCentralGravityField( CentralGravityField::earth );
+
+    const double computedResultForTest2 =
+            predefinedEarthCentralGravityField.getGravitationalParameter( );
+
+    // Determine the expected result
+    const double expectedResultForTest2 = 3.9859383624e14;
+
+    // Check that expected and computed values are equal.
+    BOOST_CHECK_EQUAL ( expectedResultForTest2, computedResultForTest2 );
+}
+
+//! Test getting potential, the gradient of the potential and the gradient tensor of a given state.
+BOOST_AUTO_TEST_CASE( testGetPotential )
+{
+    // Set position with respect to geometric center.
+    const Eigen::Vector3d cartesianPosition( 5.0e6, 3.0e6, 1.0e6 );
 
     // Create gravity field for myPlanet.
     using astrodynamics::gravitation::SphericalHarmonicsGravityField;
     SphericalHarmonicsGravityField myPlanetGravityField;
 
     // Set gravitational parameter of myPlanet.
-    double gravitationalParameterOfMyPlanet = 22032.00;
+    const double gravitationalParameterOfMyPlanet = 22032.00;
     myPlanetGravityField.setGravitationalParameter( gravitationalParameterOfMyPlanet );
 
-    // Set origin of gravity field of myPlanet with respect to geometric
-    // center.
-    myPlanetGravityField.setOrigin( Eigen::Vector3d::Zero( ) );
+    // Get the potential of myPlanet
+    const double computedResultForTest3 = myPlanetGravityField.getPotential( cartesianPosition );
 
-    // Set position with respect to geometric center.
-    Eigen::Vector3d cartesianPosition;
-    cartesianPosition << 5.0e6, 3.0e6, 1.0e6;
-
-    // Create predefined Earth central gravity field.
-    using astrodynamics::gravitation::CentralGravityField;
-    CentralGravityField predefinedEarthCentralGravityField( CentralGravityField::earth );
-
-    // Expected test results.
-    double expectedResultForTest1 = gravitationalParameterOfMyPlanet;
-    double expectedResultForTest2 = 3.9859383624e+14;
+    // Generate expected result for the potential of myPlanet
     double expectedResultForTest3 = gravitationalParameterOfMyPlanet / cartesianPosition.norm( );
-    Eigen::VectorXd expectedResultForTest4 =  -gravitationalParameterOfMyPlanet
-            / pow( cartesianPosition.norm( ), 3.0 ) * cartesianPosition;
+
+    // Get the gradient of the potential of myPlanet
+    const Eigen::Vector3d computedResultForTest4 = myPlanetGravityField.getGradientOfPotential(
+                cartesianPosition );
+
+    // Determine the expected result for the gradient of the potential of myPlanet
+    const Eigen::VectorXd expectedResultForTest4 = -gravitationalParameterOfMyPlanet
+            / std::pow( cartesianPosition.norm( ), 3.0 ) * cartesianPosition;
+
+    // Get the gradient tensor of the potential of myPlanet
+    const Eigen::Matrix3d computedResultForTest5
+            = myPlanetGravityField.getGradientTensorOfPotential( cartesianPosition );
+
+    // Set identity matrix.
+    Eigen::Matrix3d identityMatrix = Eigen::Matrix3d::Identity( 3, 3 );
+
+    // Determine the expected result.
     Eigen::Matrix3d expectedResultForTest5 = gravitationalParameterOfMyPlanet
-            / pow( cartesianPosition.norm( ), 5.0 )
+            / std::pow( cartesianPosition.norm( ), 5.0 )
             * ( ( 3.0 * cartesianPosition * cartesianPosition.transpose( ) )
                 - ( cartesianPosition.squaredNorm( ) * identityMatrix ) );
 
-    // Results computed using implementation of spherical harmonics gravity
-    // field class.
-    double computedResultForTest1 = myPlanetGravityField.getGravitationalParameter( );
-    double computedResultForTest2 =
-            predefinedEarthCentralGravityField.getGravitationalParameter( );
-    double computedResultForTest3 = myPlanetGravityField.getPotential(
-                cartesianPosition );
-    Eigen::Vector3d computedResultForTest4 = myPlanetGravityField.getGradientOfPotential(
-                cartesianPosition );
-    Eigen::Matrix3d computedResultForTest5 = myPlanetGravityField.getGradientTensorOfPotential(
-                cartesianPosition );
-
-    // Compute differences between computed and expected results.
-    Eigen::VectorXd differenceBetweenResults( 5 );
-    Eigen::Vector3d differenceBetweenResultsForTest4;
-    Eigen::Matrix3d differenceBetweenResultsForTest5;
-    differenceBetweenResults( 0 ) = fabs( computedResultForTest1 - expectedResultForTest1 );
-    differenceBetweenResults( 1 ) = fabs( computedResultForTest2 - expectedResultForTest2 );
-    differenceBetweenResults( 2 ) = fabs( computedResultForTest3 - expectedResultForTest3 );
-    differenceBetweenResultsForTest4 = computedResultForTest4 - expectedResultForTest4;
-    differenceBetweenResults( 3 ) =  differenceBetweenResultsForTest4.norm( );
-    differenceBetweenResultsForTest5 = computedResultForTest5 - expectedResultForTest5;
-    differenceBetweenResults( 4 ) =  differenceBetweenResultsForTest5.norm( );
-
-    // Set test result to false if the test does not match the expected result.
-    if ( differenceBetweenResults.norm( ) > std::numeric_limits< double >::epsilon( ) )
-    {
-        // Check if the source of the error is Test 1.
-        if ( differenceBetweenResults( 0 ) > std::numeric_limits< double >::epsilon( ) )
-        {
-            cerr << "The computed value ( " << computedResultForTest1
-                 << " ) for a user-defined gravitational parameter does not "
-                 << "match the set value ( " << expectedResultForTest1
-                 << " )." << endl;
-            cerr << "The difference is: "
-                 << differenceBetweenResults( 0 ) << endl;
-        }
-
-        // Check if the source of the error is Test 2.
-        if ( differenceBetweenResults( 1 ) > std::numeric_limits< double >::epsilon( ) )
-        {
-            cerr << "The retrieved value ( " << computedResultForTest2
-                 << " ) for a predefined gravitational parameter does not "
-                 << "match the set value ( " << expectedResultForTest2
-                 << " )." << endl;
-            cerr << "The difference is: "
-                 << differenceBetweenResults( 1 ) << endl;
-        }
-
-        // Check if the source of the error is Test 3.
-        if ( differenceBetweenResults( 2 ) > std::numeric_limits< double >::epsilon( ) )
-        {
-            cerr << "The computed value ( " << computedResultForTest3
-                 << " ) for the gravitational potential does not match "
-                 << "the expected solution ( " << expectedResultForTest3
-                 << " )." << endl;
-            cerr << "The difference is: "
-                 << differenceBetweenResults( 2 ) << endl;
-        }
-
-        // Check if the source of the error is Test 4.
-        if ( differenceBetweenResults( 3 ) > std::numeric_limits< double >::epsilon( ) )
-        {
-            cerr << "The computed value ( " << computedResultForTest3
-                 << " ) for the gravitational gradient does not match "
-                 << "the expected solution  (" << expectedResultForTest3
-                 << " )." << endl;
-            cerr << "The difference is: "
-                 << differenceBetweenResults( 4 ) << endl;
-        }
-
-        // Check if the source of the error is Test 5.
-        if ( differenceBetweenResults( 4 ) > std::numeric_limits< double >::epsilon( ) )
-        {
-            cerr << "The computed value ( " << computedResultForTest5
-                 <<  ") for the gravitational gradient tensor does not match "
-                 << "the expected solution ( " << expectedResultForTest5 << " )." << endl;
-            cerr << "The difference is: " << differenceBetweenResults( 4 ) << endl;
-        }
-
-        isSphericalHarmonicsGravityFieldErroneous = true;
-    }
-
-    // Return test result.
-    // If test is successful return false; if test fails, return true.
-    if ( isSphericalHarmonicsGravityFieldErroneous )
-    {
-        cerr << "testSphericalHarmonicsGravityField failed!" << std::endl;
-    }
-
-    return isSphericalHarmonicsGravityFieldErroneous;
+    // Check that compute values match expected values.
+    BOOST_CHECK_EQUAL( expectedResultForTest3, computedResultForTest3 );
+    TUDAT_CHECK_MATRIX_CLOSE_FRACTION( expectedResultForTest4, computedResultForTest4,
+                                       std::numeric_limits< double >::epsilon( ) );
+    TUDAT_CHECK_MATRIX_CLOSE_FRACTION( expectedResultForTest5, computedResultForTest5,
+                                       std::numeric_limits< double >::epsilon( ) );
 }
+
+BOOST_AUTO_TEST_SUITE_END( )
+
+} // namespace tudat
+} // namespace unit_tests
