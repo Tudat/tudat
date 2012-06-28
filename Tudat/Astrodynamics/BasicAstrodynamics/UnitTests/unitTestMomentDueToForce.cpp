7a5efda0399ee837e7fdd84636e7065934229f65
Kartik Kumar <me@kartikkumar.com>
2012-06-28 11:50:09 +0000

    Boostified moment due to force unit test (issue #409).
    git-svn-id: file:///srv/svn/repos/tudat/trunk@410 ac7aa084-b6da-11df-bf42-7fac39f0224f

--- Tudat/Astrodynamics/BasicAstrodynamics/UnitTests/unitTestMomentDueToForce.cpp
+++ Tudat/Astrodynamics/BasicAstrodynamics/UnitTests/unitTestMomentDueToForce.cpp
@@ -29,87 +29,94 @@
  *      110824    J. Leloux         Corrected doxygen documentation.
  *      120220    D. Dirkx          Moved moment due to force test to separate file after split
  *                                  of moment model base class.
+ *      120628    M.I. Ganeff       Boostified unit test.
  *
  *    References
  *
  */
 
+#define BOOST_TEST_MAIN
+
 #include <cmath>
 #include <iostream>
 #include <limits>
 
+#include <boost/test/floating_point_comparison.hpp>
+#include <boost/test/unit_test.hpp>
+
 #include <Eigen/Core>
 #include <Eigen/Geometry>
 
+#include <TudatCore/Basics/testMacros.h>
+
 #include "Tudat/Astrodynamics/BasicAstrodynamics/momentDueToForceModel.h"
 
-//! Test implementation of moment due to force.
-int main( )
+namespace tudat
 {
-    using std::cerr;
-    using std::endl;
-    using std::fabs;
-    using namespace tudat;
-    using namespace tudat::astrodynamics::moment_models;
+namespace unit_tests
+{
 
-    bool isMomentDueToForceBroken = false;
+BOOST_AUTO_TEST_SUITE( test_MomentDueToForce )
 
-    // Initialise objects.
-    Eigen::Vector3d forceCoefficients;
-    forceCoefficients( 0 ) = 1.1;
-    forceCoefficients( 1 ) = 1.2;
-    forceCoefficients( 2 ) = 1.3;
+//! Testing moment due to force.
+BOOST_AUTO_TEST_CASE( testMomentDueToForceUsingClass )
+{
+    // Initialize input data.
+    const Eigen::Vector3d forceCoefficients( 1.1, 1.2, 1.3 );
+    const Eigen::Vector3d momentArm( -2.0, 0.0, 0.0 );
+    const double dynamicPressure = 50.0;
+    const double referenceArea = 2.2;
 
-    Eigen::Vector3d momentArm;
-    momentArm( 0 ) = -2.0;
-    momentArm( 1 ) = 0.0;
-    momentArm( 2 ) = 0.0;
-
-    double dynamicPressure = 50.0;
-    double referenceArea = 2.2;
-
-    Eigen::Vector3d intermediateExpectedForce
+    const Eigen::Vector3d intermediateExpectedForce
             = forceCoefficients * dynamicPressure * referenceArea;
 
-    // Calculate moment.
-    MomentDueToForceModel momentDueToForce = MomentDueToForceModel();
+    // Compute moment.
+    tudat::astrodynamics::moment_models::MomentDueToForceModel momentDueToForce;
     momentDueToForce.computeMoment( intermediateExpectedForce, momentArm );
-    Eigen::Vector3d moment = momentDueToForce.getMomentDueToForce( );
+    const Eigen::Vector3d moment = momentDueToForce.getMomentDueToForce( );
 
-    // Calculate expected moment.
-    Eigen::Vector3d expectedMomentDueToForce;
-    expectedMomentDueToForce( 0 ) = 0.0;
-    expectedMomentDueToForce( 1 ) = -1.0 * momentArm( 0 ) * intermediateExpectedForce( 2 );
-    expectedMomentDueToForce( 2 ) = momentArm( 0 ) * intermediateExpectedForce( 1 );
+    // Compute expected moment.
+    const Eigen::Vector3d expectedMomentDueToForce(
+                0.0, -1.0 * momentArm( 0 ) * intermediateExpectedForce( 2 ),
+                momentArm( 0 ) * intermediateExpectedForce( 1 ) );
 
-    // Error in calculation.
-    Eigen::Vector3d errorInMoment;
-    errorInMoment( 0 ) = fabs( ( expectedMomentDueToForce( 0 ) - moment( 0 ) ) / moment( 0 ) );
-    errorInMoment( 1 ) = fabs( ( expectedMomentDueToForce( 1 ) - moment( 1 ) ) / moment( 1 ) );
-    errorInMoment( 2 ) = fabs( ( expectedMomentDueToForce( 2 ) - moment( 2 ) ) / moment( 2 ) );
+    // Check if computed moment matches expected moment.
+    BOOST_CHECK_SMALL( moment( 0 ), std::numeric_limits< double >::min( ) );
 
-    if ( errorInMoment( 0 ) > std::numeric_limits< double >::epsilon( ) ||
-         errorInMoment( 1 ) > std::numeric_limits< double >::epsilon( ) ||
-         errorInMoment( 2 ) > std::numeric_limits< double >::epsilon( ) )
-    {
-        isMomentDueToForceBroken = true;
-        cerr << "Test momentDueToForce using class failed." << endl;
-    }
+    BOOST_CHECK_CLOSE_FRACTION( expectedMomentDueToForce( 1 ), moment( 1 ),
+                                std::numeric_limits< double >::epsilon( ) );
 
-    moment = computeMomentDueToForce( intermediateExpectedForce, momentArm );
-
-    // Error in calculation.
-    errorInMoment( 0 ) = fabs( ( expectedMomentDueToForce( 0 ) - moment( 0 ) ) / moment( 0 ) );
-    errorInMoment( 1 ) = fabs( ( expectedMomentDueToForce( 1 ) - moment( 1 ) ) / moment( 1 ) );
-    errorInMoment( 2 ) = fabs( ( expectedMomentDueToForce( 2 ) - moment( 2 ) ) / moment( 2 ) );
-
-    if ( errorInMoment( 0 ) > std::numeric_limits< double >::epsilon( ) ||
-         errorInMoment( 1 ) > std::numeric_limits< double >::epsilon( ) ||
-         errorInMoment( 2 ) > std::numeric_limits< double >::epsilon( ) )
-    {
-        isMomentDueToForceBroken = true;
-        cerr << "Test momentDueToForce using class failed." << endl;
-    }
-
-    return isMomentDueToForceBroken;
+    BOOST_CHECK_CLOSE_FRACTION( expectedMomentDueToForce( 2 ), moment( 2 ),
+                                std::numeric_limits< double >::epsilon( ) );
 }
+
+//! Testing moment due to force.
+BOOST_AUTO_TEST_CASE( testMomentDueToForceUsingFreeFunction )
+{
+    // Initialize input data.
+    const Eigen::Vector3d forceCoefficients( 1.1, 1.2, 1.3 );
+    const Eigen::Vector3d momentArm( -2.0, 0.0, 0.0 );
+    const double dynamicPressure = 50.0;
+    const double referenceArea = 2.2;
+
+    const Eigen::Vector3d intermediateExpectedForce
+            = forceCoefficients * dynamicPressure * referenceArea;
+
+    // Compute moment.
+    const Eigen::Vector3d moment = tudat::astrodynamics::moment_models::computeMomentDueToForce(
+                intermediateExpectedForce, momentArm );
+
+    // Compute expected moment.
+    const Eigen::Vector3d expectedMomentDueToForce(
+                0.0, -1.0 * momentArm( 0 ) * intermediateExpectedForce( 2 ),
+                momentArm( 0 ) * intermediateExpectedForce( 1 ) );
+
+    // Check if computed moment matches expected moment.
+    TUDAT_CHECK_MATRIX_CLOSE_FRACTION( expectedMomentDueToForce, moment,
+                                       std::numeric_limits< double >::epsilon( ) );
+}
+
+BOOST_AUTO_TEST_SUITE_END( )
+
+} // namespace unit_tests
+} // namespace tudat
