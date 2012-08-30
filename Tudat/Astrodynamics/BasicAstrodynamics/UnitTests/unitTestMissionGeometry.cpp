094c018423093b7b7f7a7068a1f00d68556e4e66
Kartik Kumar <me@kartikkumar.com>
2012-08-30 09:15:59 +0000

    Added shadow function computation with unit tests (issue #482).
    git-svn-id: file:///srv/svn/repos/tudat/trunk@432 ac7aa084-b6da-11df-bf42-7fac39f0224f

---
+++ Tudat/Astrodynamics/BasicAstrodynamics/UnitTests/unitTestMissionGeometry.cpp
@@ -0,0 +1,133 @@
+/*    Copyright (c) 2010-2012, Delft University of Technology
+ *    All rights reserved.
+ *
+ *    Redistribution and use in source and binary forms, with or without modification, are
+ *    permitted provided that the following conditions are met:
+ *      - Redistributions of source code must retain the above copyright notice, this list of
+ *        conditions and the following disclaimer.
+ *      - Redistributions in binary form must reproduce the above copyright notice, this list of
+ *        conditions and the following disclaimer in the documentation and/or other materials
+ *        provided with the distribution.
+ *      - Neither the name of the Delft University of Technology nor the names of its contributors
+ *        may be used to endorse or promote products derived from this software without specific
+ *        prior written permission.
+ *
+ *    THIS SOFTWARE IS PROVIDED BY THE COPYRIGHT HOLDERS AND CONTRIBUTORS "AS IS" AND ANY EXPRESS
+ *    OR IMPLIED WARRANTIES, INCLUDING, BUT NOT LIMITED TO, THE IMPLIED WARRANTIES OF
+ *    MERCHANTABILITY AND FITNESS FOR A PARTICULAR PURPOSE ARE DISCLAIMED. IN NO EVENT SHALL THE
+ *    COPYRIGHT HOLDER OR CONTRIBUTORS BE LIABLE FOR ANY DIRECT, INDIRECT, INCIDENTAL, SPECIAL,
+ *    EXEMPLARY, OR CONSEQUENTIAL DAMAGES (INCLUDING, BUT NOT LIMITED TO, PROCUREMENT OF SUBSTITUTE
+ *    GOODS OR SERVICES; LOSS OF USE, DATA, OR PROFITS; OR BUSINESS INTERRUPTION) HOWEVER CAUSED
+ *    AND ON ANY THEORY OF LIABILITY, WHETHER IN CONTRACT, STRICT LIABILITY, OR TORT (INCLUDING
+ *    NEGLIGENCE OR OTHERWISE) ARISING IN ANY WAY OUT OF THE USE OF THIS SOFTWARE, EVEN IF ADVISED
+ *    OF THE POSSIBILITY OF SUCH DAMAGE.
+ *
+ *    Changelog
+ *      YYMMDD    Author            Comment
+ *      120530    M.I. Ganeff       Creation of code.
+ *
+ *    References
+ *      Montebruck O, Gill E. Satellite Orbits, Corrected Third Printing, Springer, 2005.
+ *
+ */
+
+#define BOOST_TEST_MAIN
+
+#include <boost/test/floating_point_comparison.hpp>
+#include <boost/test/unit_test.hpp>
+
+#include <Eigen/Core>
+
+#include <Tudat/Astrodynamics/BasicAstrodynamics/missionGeometry.h>
+
+namespace tudat
+{
+namespace unit_tests
+{
+
+//! Show the functionality of the unit tests.
+BOOST_AUTO_TEST_SUITE( test_Mission_Geometry )
+
+//! Unit test for shadow function (Sun,Earth).
+BOOST_AUTO_TEST_CASE( testShadowFunctionForFullShadow )
+{
+    // Satellite totally blocked from solar radiation by the earth. Sun and satellite located on the
+    // x-axis with the Earth in between. Altitude of satellite = 1000 km.
+
+    const Eigen::Vector3d occultedBodyPosition = -149598000e3 * Eigen::Vector3d( 1.0, 0.0, 0.0 );
+    const Eigen::Vector3d occultingBodyPosition = Eigen::Vector3d::Zero( );
+    const double occultedBodyRadius = 6.96e8; // Siedelmann 1992.
+    const double occultingBodyRadius = 6378.137e3; // WGS-84.
+
+    const Eigen::Vector3d satelliteState = ( occultingBodyRadius + 1.0e6 )
+            * Eigen::Vector3d( 1.0, 0.0, 0.0 );
+
+    // Compute shadow function.
+    const double shadowFunction = mission_geometry::computeShadowFunction(
+                occultedBodyPosition,
+                occultedBodyRadius,
+                occultingBodyPosition,
+                occultingBodyRadius,
+                satelliteState );
+
+    // Test values.
+    BOOST_CHECK_EQUAL( 0.0, shadowFunction );
+}
+
+BOOST_AUTO_TEST_CASE( testShadowFunctionForFullLight )
+{
+    // Satellite totally subjected to sunlight. Satellite is located on the y-axis and the sun is
+    // located on the x-axis. Altitude of satellite = 1000 km.
+
+    const Eigen::Vector3d occultedBodyPosition = -149598000e3 * Eigen::Vector3d( 1.0, 0.0, 0.0 );
+    const Eigen::Vector3d occultingBodyPosition = Eigen::Vector3d::Zero( );
+    const double occultedBodyRadius = 6.96e8; // Siedelmann 1992.
+    const double occultingBodyRadius = 6378.137e3; // WGS-84.
+
+    const Eigen::Vector3d satelliteState = ( occultingBodyRadius + 1.0e6 )
+            * Eigen::Vector3d( 0.0, 1.0, 0.0 );
+
+    // Compute shadow function.
+    const double shadowFunction = mission_geometry::computeShadowFunction(
+                occultedBodyPosition,
+                occultedBodyRadius,
+                occultingBodyPosition,
+                occultingBodyRadius,
+                satelliteState );
+
+    // Test values
+    BOOST_CHECK_EQUAL( 1.0, shadowFunction );
+}
+
+BOOST_AUTO_TEST_CASE( testShadowFunctionForPartialShadow )
+{
+    // Satellite partially visible. Satellite is located between the locations of the previous
+    // tests in penumbra. According to analytical derivations in Matlab the shadow function should
+    // be around 0.4547. Altitude of satellite = 1000 km.
+
+    const Eigen::Vector3d occultedBodyPosition = -149598000e3 * Eigen::Vector3d( 1.0, 0.0, 0.0 );
+    const Eigen::Vector3d occultingBodyPosition = Eigen::Vector3d::Zero( );
+    const double occultedBodyRadius = 6.96e8; // Siedelmann 1992.
+    const double occultingBodyRadius = 6378.137e3; // WGS-84.
+
+    Eigen::Vector3d satelliteDirection( 0.018, 1.0, 0.0 );
+    satelliteDirection.normalize( );
+
+    const Eigen::Vector3d satelliteState = ( occultingBodyRadius + 1.0e3 ) * satelliteDirection;
+
+    // Compute shadow function
+    const double shadowFunction = mission_geometry::computeShadowFunction(
+                occultedBodyPosition,
+                occultedBodyRadius,
+                occultingBodyPosition,
+                occultingBodyRadius,
+                satelliteState );
+
+    // Test values.
+    BOOST_CHECK_CLOSE_FRACTION( 0.4547, shadowFunction, 0.001 );
+}
+
+BOOST_AUTO_TEST_SUITE_END( )
+
+} // namespace unit_tests
+} // namespace tudat
---
+++ Tudat/Astrodynamics/BasicAstrodynamics/missionGeometry.h
@@ -0,0 +1,74 @@
+/*    Copyright (c) 2010-2012, Delft University of Technology
+ *    All rights reserved.
+ *
+ *    Redistribution and use in source and binary forms, with or without modification, are
+ *    permitted provided that the following conditions are met:
+ *      - Redistributions of source code must retain the above copyright notice, this list of
+ *        conditions and the following disclaimer.
+ *      - Redistributions in binary form must reproduce the above copyright notice, this list of
+ *        conditions and the following disclaimer in the documentation and/or other materials
+ *        provided with the distribution.
+ *      - Neither the name of the Delft University of Technology nor the names of its contributors
+ *        may be used to endorse or promote products derived from this software without specific
+ *        prior written permission.
+ *
+ *    THIS SOFTWARE IS PROVIDED BY THE COPYRIGHT HOLDERS AND CONTRIBUTORS "AS IS" AND ANY EXPRESS
+ *    OR IMPLIED WARRANTIES, INCLUDING, BUT NOT LIMITED TO, THE IMPLIED WARRANTIES OF
+ *    MERCHANTABILITY AND FITNESS FOR A PARTICULAR PURPOSE ARE DISCLAIMED. IN NO EVENT SHALL THE
+ *    COPYRIGHT HOLDER OR CONTRIBUTORS BE LIABLE FOR ANY DIRECT, INDIRECT, INCIDENTAL, SPECIAL,
+ *    EXEMPLARY, OR CONSEQUENTIAL DAMAGES (INCLUDING, BUT NOT LIMITED TO, PROCUREMENT OF SUBSTITUTE
+ *    GOODS OR SERVICES; LOSS OF USE, DATA, OR PROFITS; OR BUSINESS INTERRUPTION) HOWEVER CAUSED
+ *    AND ON ANY THEORY OF LIABILITY, WHETHER IN CONTRACT, STRICT LIABILITY, OR TORT (INCLUDING
+ *    NEGLIGENCE OR OTHERWISE) ARISING IN ANY WAY OUT OF THE USE OF THIS SOFTWARE, EVEN IF ADVISED
+ *    OF THE POSSIBILITY OF SUCH DAMAGE.
+ *
+ *    Changelog
+ *      YYMMDD    Author            Comment
+ *      120530    M.I. Ganeff       Creation of code.
+ *
+ *    References
+ *      Montebruck O, Gill E. Satellite Orbits, Corrected Third Printing, Springer, 2005.
+ *
+ *    Notes
+ *      In future, it might make sense to create a new MissionGeometry sub-directory if a lot more
+ *      funtionality is added.
+ *
+ */
+
+#ifndef TUDAT_MISSION_GEOMETRY_H
+#define TUDAT_MISSION_GEOMETRY_H
+
+#include <Eigen/Core>
+
+namespace tudat
+{
+namespace mission_geometry
+{
+
+//! Compute the shadow function.
+/*!
+* Returns the value of of the shadow function. Returns 0 if the satellite is in umbra, 1 if the
+* satellite is fully exposed and a value between 0 and 1 if the satellite is in penumbra.
+*
+* The point of view is from the satellite. The occulting body (for example the Earth) is the body
+* that blocks the light from the occulting body (for example the Sun).
+*
+* Reference: Section 3.4 from ( Montebruck O, Gill E., 2005).
+*
+* \param occultedBodyPosition Vector containing Cartesian coordinates of the occulted body.
+* \param occultedBodyRadius Mean radius of occulted body.
+* \param occultingBodyPosition Vector containing Cartesian coordinates of the occulting body.
+* \param occultingBodyRadius Mean radius of occulting body.
+* \param satelliteState Vector containing Cartesian coordinates of the satellite.
+* \return Shadow function value.
+*/
+double computeShadowFunction( const Eigen::VectorXd occultedBodyPosition,
+                              const double occultedBodyRadius,
+                              const Eigen::VectorXd occultingBodyPosition,
+                              const double occultingBodyRadius,
+                              const Eigen::VectorXd satelliteState );
+
+} // namespace mission_geometry
+} // namespace tudat
+
+#endif // TUDAT_MISSION_GEOMETRY_H
