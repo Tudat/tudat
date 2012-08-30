931f6c76f9b427c81fff4346b589ff8fa8b38585
Kartik Kumar <me@kartikkumar.com>
2012-08-30 08:43:39 +0000

    Added apparent acceleration model with unit tests (issue #519).
    git-svn-id: file:///srv/svn/repos/tudat/trunk@431 ac7aa084-b6da-11df-bf42-7fac39f0224f

---
+++ Tudat/Astrodynamics/ReferenceFrames/UnitTests/unitTestApparentAccelerationModel.cpp
@@ -0,0 +1,197 @@
+/*    Copyright (c) 2010-2012, Delft University of Technology
+ *    All rights reserved.
+ *
+ *    Redistribution and use in source and binary forms, with or without modification, are
+ *    permitted provided that the following conditions are met:
+ *      - Redistributions of source code must retain the above copyright notice, this list of
+ *        conditions and the following disclaimer.
+ *      - Redistributions in binary form must reproduce the above copyright notice, this list of
+ *        conditions and the following disclaimer in the documentation and/or other materials
+ *        provided with the distribution.
+ *      - Neither the name of the Delft University of Technology nor the names of its contributors
+ *        may be used to endorse or promote products derived from this software without specific
+ *        prior written permission.
+ *
+ *    THIS SOFTWARE IS PROVIDED BY THE COPYRIGHT HOLDERS AND CONTRIBUTORS "AS IS" AND ANY EXPRESS
+ *    OR IMPLIED WARRANTIES, INCLUDING, BUT NOT LIMITED TO, THE IMPLIED WARRANTIES OF
+ *    MERCHANTABILITY AND FITNESS FOR A PARTICULAR PURPOSE ARE DISCLAIMED. IN NO EVENT SHALL THE
+ *    COPYRIGHT HOLDER OR CONTRIBUTORS BE LIABLE FOR ANY DIRECT, INDIRECT, INCIDENTAL, SPECIAL,
+ *    EXEMPLARY, OR CONSEQUENTIAL DAMAGES (INCLUDING, BUT NOT LIMITED TO, PROCUREMENT OF SUBSTITUTE
+ *    GOODS OR SERVICES; LOSS OF USE, DATA, OR PROFITS; OR BUSINESS INTERRUPTION) HOWEVER CAUSED
+ *    AND ON ANY THEORY OF LIABILITY, WHETHER IN CONTRACT, STRICT LIABILITY, OR TORT (INCLUDING
+ *    NEGLIGENCE OR OTHERWISE) ARISING IN ANY WAY OUT OF THE USE OF THIS SOFTWARE, EVEN IF ADVISED
+ *    OF THE POSSIBILITY OF SUCH DAMAGE.
+ *
+ *    Changelog
+ *      YYMMDD    Author            Comment
+ *      120716    A. Ronse          Creation of code.
+ *
+ *    References
+ *      [1] J.R. Clynch, Introduction to Earth Gravity, March 2006,
+ *          http://www.gmat.unsw.edu.au/snap/gps/clynch_pdfs/intgrav.pdf
+ *      [2] WolframAlpha: computational knowledge engine, coriolis calculator
+ *          http://www.wolframalpha.com/input/?i=coriolis+effect&a=*C.coriolis+effect-_*Formula-
+ *
+ *    Notes
+ *
+ */
+
+#define BOOST_TEST_MAIN
+
+#include <boost/function.hpp>
+#include <boost/lambda/lambda.hpp>
+#include <boost/make_shared.hpp>
+#include <boost/shared_ptr.hpp>
+#include <boost/test/unit_test.hpp>
+
+#include <Eigen/Core>
+
+#include <TudatCore/Basics/testMacros.h>
+
+#include "Tudat/Astrodynamics/ReferenceFrames/apparentAccelerationModel.h"
+
+namespace tudat
+{
+namespace unit_tests
+{
+
+using tudat::acceleration_models::computeApparentAcceleration;
+using tudat::acceleration_models::ApparentAccelerationModel;
+
+BOOST_AUTO_TEST_SUITE( test_reference_frame_transformations )
+
+// Summary of tests.
+// Test 1: Check apparent acceleration of static object at Earth equator (=only centrifugal).
+// Test 2: Check apparent acceleration of moving object at Earth pole (=only Coriolis).
+// Test 3: Check apparent acceleration of moving object in an accelerating, rotational reference
+//         frame.
+// Test 4: Check apparent acceleration of moving object in inertial reference frame (=0).
+// Test 5: Test class implementation.
+
+//! Test apparent acceleration of static object at Earth equator (=only centrifugal).
+// Reference data from [1] (page 5), recomputed using Excel for higher precision.
+BOOST_AUTO_TEST_CASE( testCentrifugalAccelerationEarth )
+{
+    // Specify reference frame dynamics.
+    const Eigen::Vector3d referenceFrameAcceleration = Eigen::Vector3d::Zero( );
+    const Eigen::Vector3d rotationRate( 0.0, 0.0, 7.29212351699e-5 );
+    const Eigen::Vector3d rotationAcceleration = Eigen::Vector3d::Zero( );
+
+    // Specify state.
+    const Eigen::Vector3d position( 6378000.0, 0.0, 0.0 );
+    const Eigen::Vector3d velocity = Eigen::Vector3d::Zero( );
+
+    // Compute apparent acceleration.
+    const Eigen::Vector3d apparentAcceleration = computeApparentAcceleration(
+            referenceFrameAcceleration, rotationRate, rotationAcceleration, position, velocity );
+    const Eigen::Vector3d expectedAcceleration( 0.0339150567038567, 0.0, 0.0 );
+
+    // Check that resulting acceleration matches expected one.
+    TUDAT_CHECK_MATRIX_CLOSE_FRACTION( apparentAcceleration, expectedAcceleration, 1.0e-12 );
+}
+
+//! Test apparent acceleration of moving object at Earth pole (=only Coriolis).
+// Reference data from pen/paper analysis, checked using [2].
+BOOST_AUTO_TEST_CASE( testCoriolisAccelerationAtEarthPoles )
+{
+    // Specify reference frame dynamics.
+    const Eigen::Vector3d referenceFrameAcceleration = Eigen::Vector3d::Zero( );
+    const Eigen::Vector3d rotationRate( 0.0, 0.0, 7.29212351699e-5 );
+    const Eigen::Vector3d rotationAcceleration = Eigen::Vector3d::Zero( );
+
+    // Specify state.
+    const Eigen::Vector3d position( 0.0, 0.0, 6378000.0 );
+    const Eigen::Vector3d velocity( 10.0, 0.0, 0.0 );
+
+    // Compute apparent acceleration.
+    const Eigen::Vector3d apparentAcceleration = computeApparentAcceleration(
+            referenceFrameAcceleration, rotationRate, rotationAcceleration, position, velocity );
+    const Eigen::Vector3d expectedAcceleration( 0.0, -0.0014584247033981, 0.0 );
+
+    // Check that resulting acceleration matches expected one.
+    TUDAT_CHECK_MATRIX_CLOSE_FRACTION( apparentAcceleration, expectedAcceleration, 1.0e-13 );
+}
+
+//! Test apparent acceleration of moving object in a general non-inertial reference frame.
+// Checked with computations performed in MATLAB.
+BOOST_AUTO_TEST_CASE( testApparentAccelerationGeneral )
+{
+    // Specify reference frame dynamics.
+    const Eigen::Vector3d referenceFrameAcceleration( 1.0, 2.0, 3.0 );
+    const Eigen::Vector3d rotationRate( 0.1, 0.2, 0.3 );
+    const Eigen::Vector3d rotationAcceleration( 0.03, 0.02, 0.01 );
+
+    // Specify state.
+    const Eigen::Vector3d position( 100.0, 300.0, 500.0 );
+    const Eigen::Vector3d velocity( 10.0, 40.0, 25.0 );
+
+    // Compute apparent acceleration.
+    const Eigen::Vector3d apparentAcceleration = computeApparentAcceleration(
+            referenceFrameAcceleration, rotationRate, rotationAcceleration, position, velocity );
+    const Eigen::Vector3d expectedAcceleration( -2.0, 9.0, -10.0 );
+
+    // Check that resulting acceleration matches expected one.
+    TUDAT_CHECK_MATRIX_CLOSE_FRACTION( apparentAcceleration, expectedAcceleration, 1.0e-14 );
+}
+
+//! Test apparent acceleration of moving object in an inertial reference frame (=0).
+BOOST_AUTO_TEST_CASE( testApparentAccelerationStatic )
+{
+    // Specify reference frame dynamics.
+    const Eigen::Vector3d referenceFrameAcceleration = Eigen::Vector3d::Zero( );
+    const Eigen::Vector3d rotationRate = Eigen::Vector3d::Zero( );
+    const Eigen::Vector3d rotationAcceleration = Eigen::Vector3d::Zero( );
+
+    // Specify state.
+    const Eigen::Vector3d position( 100.0, 300.0, 500.0 );
+    const Eigen::Vector3d velocity( 10.0, 40.0, 25.0 );
+
+    // Compute apparent acceleration.
+    const Eigen::Vector3d apparentAcceleration = computeApparentAcceleration(
+            referenceFrameAcceleration, rotationRate, rotationAcceleration, position, velocity );
+    const Eigen::Vector3d expectedAcceleration( 0.0, 0.0, 0.0 );
+
+    // Check that resulting acceleration matches expected one.
+    TUDAT_CHECK_MATRIX_BASE( apparentAcceleration, expectedAcceleration );
+}
+
+//! Test class implementation of apparent acceleration model.
+BOOST_AUTO_TEST_CASE( testApparentAccelerationClass )
+{
+    // Specify reference frame dynamics.
+    const Eigen::Vector3d referenceFrameAcceleration( 1.0, 2.0, 3.0 );
+    const Eigen::Vector3d rotationRate( 0.1, 0.2, 0.3 );
+    const Eigen::Vector3d rotationAcceleration( 0.03, 0.02, 0.01 );
+    const Eigen::Vector3d position( 100.0, 300.0, 500.0 );
+    const Eigen::Vector3d velocity( 10.0, 40.0, 25.0 );
+
+    // Create functions returning the parameters defined above.
+    boost::function< Eigen::Vector3d( ) > accelerationFunction =
+            boost::lambda::constant( referenceFrameAcceleration );
+    boost::function< Eigen::Vector3d( ) > rotationRateFunction =
+            boost::lambda::constant( rotationRate );
+    boost::function< Eigen::Vector3d( ) > rotationAccelerationFunction =
+            boost::lambda::constant( rotationAcceleration );
+    boost::function< Eigen::Vector3d( ) > positionFunction =
+            boost::lambda::constant( position );
+    boost::function< Eigen::Vector3d( ) > velocityFunction =
+            boost::lambda::constant( velocity );
+
+    // Create object of apparentAcceleraitonModel class; pass functions in constructor
+    ApparentAccelerationModel apparentAccelerationModel(
+           accelerationFunction, rotationRateFunction, rotationAccelerationFunction,
+           positionFunction, velocityFunction );
+    const Eigen::Vector3d expectedAcceleration( -2.0, 9.0, -10.0 );
+
+    // Compute apparent acceleration.
+    const Eigen::Vector3d computedAcceleration = apparentAccelerationModel.getAcceleration( );
+
+    // Check that resulting acceleration matches expected one.
+    TUDAT_CHECK_MATRIX_CLOSE_FRACTION( computedAcceleration,
+                                       expectedAcceleration, 1.0e-14 );
+}
+
+BOOST_AUTO_TEST_SUITE_END( )
+
+} // namespace unit_tests
+} // namespace tudat
---
+++ Tudat/Astrodynamics/ReferenceFrames/apparentAccelerationModel.h
@@ -0,0 +1,310 @@
+/*    Copyright (c) 2010-2012, Delft University of Technology
+ *    All rights reserved.
+ *
+ *    Redistribution and use in source and binary forms, with or without modification, are
+ *    permitted provided that the following conditions are met:
+ *      - Redistributions of source code must retain the above copyright notice, this list of
+ *        conditions and the following disclaimer.
+ *      - Redistributions in binary form must reproduce the above copyright notice, this list of
+ *        conditions and the following disclaimer in the documentation and/or other materials
+ *        provided with the distribution.
+ *      - Neither the name of the Delft University of Technology nor the names of its contributors
+ *        may be used to endorse or promote products derived from this software without specific
+ *        prior written permission.
+ *
+ *    THIS SOFTWARE IS PROVIDED BY THE COPYRIGHT HOLDERS AND CONTRIBUTORS "AS IS" AND ANY EXPRESS
+ *    OR IMPLIED WARRANTIES, INCLUDING, BUT NOT LIMITED TO, THE IMPLIED WARRANTIES OF
+ *    MERCHANTABILITY AND FITNESS FOR A PARTICULAR PURPOSE ARE DISCLAIMED. IN NO EVENT SHALL THE
+ *    COPYRIGHT HOLDER OR CONTRIBUTORS BE LIABLE FOR ANY DIRECT, INDIRECT, INCIDENTAL, SPECIAL,
+ *    EXEMPLARY, OR CONSEQUENTIAL DAMAGES (INCLUDING, BUT NOT LIMITED TO, PROCUREMENT OF SUBSTITUTE
+ *    GOODS OR SERVICES; LOSS OF USE, DATA, OR PROFITS; OR BUSINESS INTERRUPTION) HOWEVER CAUSED
+ *    AND ON ANY THEORY OF LIABILITY, WHETHER IN CONTRACT, STRICT LIABILITY, OR TORT (INCLUDING
+ *    NEGLIGENCE OR OTHERWISE) ARISING IN ANY WAY OUT OF THE USE OF THIS SOFTWARE, EVEN IF ADVISED
+ *    OF THE POSSIBILITY OF SUCH DAMAGE.
+ *
+ *    Changelog
+ *      YYMMDD    Author            Comment
+ *      120716    A. Ronse          First creation of code.
+ *      120718    D. Dirkx          Splitted up functions into Coriolis, centripetal and Euler.
+ *
+ *    References
+ *      J.S. Torok, Analytical Mechanics, Wiley-Interscience, 2000.
+ *
+ *    Notes
+ *
+ */
+
+#ifndef TUDAT_APPARENT_ACCELERATION_MODEL_H
+#define TUDAT_APPARENT_ACCELERATION_MODEL_H
+
+#include <boost/function.hpp>
+
+#include <Eigen/Dense>
+
+#include "Tudat/Astrodynamics/BasicAstrodynamics/accelerationModel.h"
+
+namespace tudat
+{
+namespace acceleration_models
+{
+
+//! Compute apparent acceleration due to non-inertiality of reference frame.
+/*!
+ * Computes apparent acceleration experienced by an object, of which the position and velocity
+ * are provided in a non-inertial (i.e. rotating and/or accelerating) reference frame.
+ * The apparent acceleration of a particle due to the non-inertiality of the reference frame in
+ * which its state is given, is given by following equation:
+ * \f[
+ *      \boldsymbol{a}_{apparent} =
+ *      -\boldsymbol{a}_B - \dot{\boldsymbol{\omega}} \times \boldsymbol{r}_{ni} -
+ *      \boldsymbol{\omega} \times (\boldsymbol{\omega} \times \boldsymbol{r}_{ni}) -
+ *      2(\boldsymbol{\omega} \times \boldsymbol{v}_{ni})
+ * \f]
+ * where \f$\boldsymbol{a}_B\f$ is the acceleration of the non-inertial frame with respect to
+ * an inertial reference frame, \f$\boldsymbol{\omega}\f$ is the respective rotation rate
+ * and \f$\boldsymbol{r}_{ni}\f$ and \f$\boldsymbol{v}_{ni}\f$ are the object's position and
+ * velocity vector in the non-inertial frame in which the apparent acceleration is computed.
+ * \param accelerationOfNonInertialReferenceFrame Acceleration vector of the non-inertial
+ *          frame with respect to an inertial reference frame [m s^-2].
+ * \param angularVelocityOfNonInertialReferenceFrame Angular velocity vector of the non-inertial
+ *          frame with respect to an inertial reference frame [rad s^-1].
+ * \param angularAccelerationOfNonInertialReferenceFrame Angular acceleration vector of the
+ *          non-inertial frame with respect to an inertial reference frame [rad s^-1].
+ * \param positionOfBodyInNonInertialReferenceFrame Position vector of body in the non-inertial
+ *          frame of reference in which the apparent acceleration is computed [m].
+ * \param velocityOfBodyInNonInertialReferenceFrame Velocity vector of body in the non-inertial
+ *          frame of reference in which the apparent acceleration is computed [m s^-1].
+ * \return Apparent acceleration as seen by an observer in the rotating and accelerating frame.
+ */
+Eigen::Vector3d computeApparentAcceleration(
+        const Eigen::Vector3d& accelerationOfNonInertialReferenceFrame,
+        const Eigen::Vector3d& angularVelocityOfNonInertialReferenceFrame,
+        const Eigen::Vector3d& angularAccelerationOfNonInertialReferenceFrame,
+        const Eigen::Vector3d& positionOfBodyInNonInertialReferenceFrame,
+        const Eigen::Vector3d& velocityOfBodyInNonInertialReferenceFrame );
+
+//! Compute centripetal acceleration due to non-inertiality of reference frame.
+/*!
+  * Computes centripetal acceleration experienced by an object, of which the position is
+  * provided in a rotating reference frame.
+  * The centripetal acceleration of a particle due to the non-inertiality of the reference frame in
+  * which its state is given, is given by following equation:
+  * \f[
+  *      \boldsymbol{a}_{centripetal} =
+  *      -\boldsymbol{\omega} \times (\boldsymbol{\omega} \times \boldsymbol{r}_{ni})
+  * \f]
+  * where \f$\boldsymbol{\omega}\f$ is the rotation rate of the non-inertial frame with respect to
+  * an inertial reference frame  and \f$\boldsymbol{r}_{ni}\f$ is the object's position in the
+  * non-inertial frame in which the apparent acceleration is computed.
+  * \param angularVelocityOfNonInertialReferenceFrame Angular velocity vector of the non-inertial
+  *          frame with respect to an inertial reference frame [rad s^-1].
+  * \param positionOfBodyInNonInertialReferenceFrame Position vector of body in the non-inertial
+  *          frame of reference in which the apparent acceleration is computed [m].
+  * \return Centripetal acceleration as seen by an observer in the rotating frame.
+  */
+Eigen::Vector3d computeCentripetalAcceleration(
+        const Eigen::Vector3d& angularVelocityOfNonInertialReferenceFrame,
+        const Eigen::Vector3d& positionOfBodyInNonInertialReferenceFrame );
+
+//! Compute Coriolis acceleration due to non-inertiality of reference frame.
+/*!
+  * Computes Coriolis acceleration experienced by an object, of which the position is
+  * provided in a rotating reference frame.
+  * The Coriolis acceleration of a particle due to the non-inertiality of the reference frame in
+  * which its state is given, is given by following equation:
+  * \f[
+  *      \boldsymbol{a}_{Coriolis} =
+  *      -2(\boldsymbol{\omega} \times \boldsymbol{v}_{ni})
+  * \f]
+  * where \f$\boldsymbol{\omega}\f$ is the rotation rate of the non-inertial frame with respect to
+  * an inertial reference frame and \f$\boldsymbol{v}_{ni}\f$ is the object's velocity in the
+  * non-inertial frame in which the apparent acceleration is computed.
+  * \param angularVelocityOfNonInertialReferenceFrame Angular velocity vector of the non-inertial
+  *          frame with respect to an inertial reference frame [rad s^-1].
+  * \param velocityOfBodyInNonInertialReferenceFrame Velocity vector of body in the non-inertial
+  *          frame of reference in which the apparent acceleration is computed [m s^-1].
+  * \return Coriolis acceleration as seen by an observer in the rotating frame.
+  */
+Eigen::Vector3d computeCoriolisAcceleration(
+        const Eigen::Vector3d& angularVelocityOfNonInertialReferenceFrame,
+        const Eigen::Vector3d& velocityOfBodyInNonInertialReferenceFrame );
+
+//! Compute Euler acceleration due to non-inertiality of reference frame.
+/*!
+  * Computes Euler acceleration experienced by an object, of which the position is
+  * provided in a rotationally accelerating reference frame.
+  * The Euler acceleration of a particle due to the non-inertiality of the reference frame in
+  * which its state is given, is given by following equation:
+  * \f[
+  *      \boldsymbol{a}_{Euler} =
+  *      - \dot{\boldsymbol{\omega}} \times \boldsymbol{r}_{ni}
+  * \f]
+  * where \f$\dot{\boldsymbol{\omega}}\f$ is the rate of change of the rotation rate of the
+  * non-inertial frame with respect to an inertial reference frame and \f$\boldsymbol{r}_{ni}\f$
+  * is the object's position in the non-inertial frame in which the apparent acceleration is
+  * computed.
+  * \param angularAccelerationOfNonInertialReferenceFrame Angular acceleration vector of the
+  *          non-inertial frame with respect to an inertial reference frame [rad s^-1].
+  * \param positionOfBodyInNonInertialReferenceFrame Position vector of body in the non-inertial
+  *          frame of reference in which the apparent acceleration is computed [m].
+  * \return Euler acceleration as seen by an observer in the rotationally accelerating frame.
+  */
+Eigen::Vector3d computeEulerAcceleration(
+        const Eigen::Vector3d& angularAccelerationOfNonInertialReferenceFrame,
+        const Eigen::Vector3d& positionOfBodyInNonInertialReferenceFrame );
+
+//! Apparent acceleration model class.
+/*!
+ * Implementation of apparent acceleration due to non-inertiality of a reference system in which
+ * the equations of motion are evaluated. It evaluates the total apparent acceleration due to the
+ * acceleration of the reference frame, as well as the Coriolis, centripetal and Euler
+ * accelerations.
+ */
+class ApparentAccelerationModel
+        : public basic_astrodynamics::acceleration_models::AccelerationModel < 3, double >
+{
+private:
+
+    //! Typedef for Eigen::Vector3d returning function.
+    typedef boost::function< Eigen::Vector3d( ) > Vector3dReturningFunction;
+
+public:
+
+    //! Class constructor.
+    /*!
+     * Constructor for apparent acceleration model.
+     * \param accelerationOfNonInertialReferenceFrame Pointer to a function returning
+     *         the acceleration vector of the non-intertial reference frame w.r.t. an
+     *         inertial frame of reference.
+     * \param angularVelocityOfNonInertialReferenceFrame Pointer to a function returning
+     *         the angular velocity vector of the non-intertial reference frame w.r.t. an
+     *         inertial frame of reference.
+     * \param angularAccelerationOfNonInertialReferenceFrame Pointer to a function returning
+     *         the angular acceleration vector of the non-inertial frame with respect to an
+     *         inertial reference frame.
+     * \param positionOfBodyInNonInertialReferenceFrame Pointer to a function returning
+     *         the position vector in the non-inertial frame of reference in which the apparent
+     *         acceleration is computed.
+     * \param velocityOfBodyInNonInertialReferenceFrame Pointer to a function returning
+     *          the velocity vector in the non-inertial frame of reference in which the apparent
+     *          acceleration is computed.
+     */
+    ApparentAccelerationModel(
+            Vector3dReturningFunction accelerationOfNonInertialReferenceFrameFunction,
+            Vector3dReturningFunction angularVelocityOfNonInertialReferenceFrameFunction,
+            Vector3dReturningFunction angularAccelerationOfNonInertialReferenceFrameFunction,
+            Vector3dReturningFunction positionOfBodyInNonInertialReferenceFrameFunction,
+            Vector3dReturningFunction velocityOfBodyInNonInertialReferenceFrameFunction )
+        : accelerationOfNonInertialReferenceFrameFunction_(
+              accelerationOfNonInertialReferenceFrameFunction ),
+          angularVelocityOfNonInertialReferenceFrameFunction_(
+              angularVelocityOfNonInertialReferenceFrameFunction ),
+          angularAccelerationOfNonInertialReferenceFrameFunction_(
+              angularAccelerationOfNonInertialReferenceFrameFunction ),
+          positionOfBodyInNonInertialReferenceFrameFunction_(
+              positionOfBodyInNonInertialReferenceFrameFunction ),
+          velocityOfBodyInNonInertialReferenceFrameFunction_(
+              velocityOfBodyInNonInertialReferenceFrameFunction )
+    {
+        updateMembers( );
+    }
+
+    //! Get apparent acceleration.
+    /*!
+     * Computes and returns the acceleration.
+     * \return Vector of the apparent acceleration in the non-inertial Cartesian frame in which the
+     *          object's state is defined.
+     */
+    Eigen::Vector3d getAcceleration( )
+    {
+        return computeApparentAcceleration( currentAccelerationOfNonInertialReferenceFrame_,
+                                            currentAngularVelocityOfNonInertialReferenceFrame_,
+                                            currentAngularAccelerationOfNonInertialReferenceFrame_,
+                                            currentPositionOfBodyInNonInertialReferenceFrame_,
+                                            currentVelocityOfBodyInNonInertialReferenceFrame_ );
+    }
+
+    //! Function to update member variables used by this acceleration model.
+    /*!
+     * Function to update member variables used by this acceleration model. The variables
+     * that are required as input for the evaluation of the accelerations are retrieved from
+     * boost::functions that may or may not give constant return values, depending on the
+     * user input. This function sets the member variables using these functions.
+     * \return True if update is successful.
+     */
+    bool updateMembers( );
+
+protected:
+
+private:
+
+    //! Function returning translational acceleration vector of non-inertial reference frame.
+    /*!
+     * Function returning translational acceleration vector of non-inertial reference frame.
+     */
+    const Vector3dReturningFunction accelerationOfNonInertialReferenceFrameFunction_;
+
+    //! Function returning angular velocity vector of non-inertial reference frame.
+    /*!
+     * Function returning angular velocity vector of non-inertial reference frame.
+     */
+    const Vector3dReturningFunction angularVelocityOfNonInertialReferenceFrameFunction_;
+
+    //! Function returning angular acceleration vector of non-inertial reference frame.
+    /*!
+     * Function returning angular acceleration vector of non-inertial reference frame.
+     */
+    const Vector3dReturningFunction angularAccelerationOfNonInertialReferenceFrameFunction_;
+
+    //! Function returning position vector in non-inertial reference frame.
+    /*!
+     * Function returning position vector in non-inertial reference frame.
+     */
+    const Vector3dReturningFunction positionOfBodyInNonInertialReferenceFrameFunction_;
+
+    //! Function returning velocity vector in non-inertial reference frame.
+    /*!
+     * Function returning velocity vector in non-inertial reference frame.
+     */
+    const Vector3dReturningFunction velocityOfBodyInNonInertialReferenceFrameFunction_;
+
+    //! Current translational acceleration vector of non-inertial reference frame.
+    /*!
+     * Current translational acceleration vector of non-inertial reference frame. This vector
+     * is updated by the updateMembers( ) function.
+     */
+    Eigen::Vector3d currentAccelerationOfNonInertialReferenceFrame_;
+
+    //! Current angular velocity vector of non-inertial reference frame.
+    /*!
+     * Current angular velocity vector of non-inertial reference frame. This vector
+     * is updated by the updateMembers( ) function.
+     */
+    Eigen::Vector3d currentAngularVelocityOfNonInertialReferenceFrame_;
+
+    //! Current angular acceleration vector of non-inertial reference frame.
+    /*!
+     * Current angular acceleration vector of non-inertial reference frame. This vector
+     * is updated by the updateMembers( ) function.
+     */
+    Eigen::Vector3d currentAngularAccelerationOfNonInertialReferenceFrame_;
+
+    //! Current position vector in non-inertial reference frame.
+    /*!
+     * Current position vector in non-inertial reference frame. This vector
+     * is updated by the updateMembers( ) function.
+     */
+    Eigen::Vector3d currentPositionOfBodyInNonInertialReferenceFrame_;
+
+    //! Current velocity vector in non-inertial reference frame.
+    /*!
+     * Current velocity vector in non-inertial reference frame. This vector
+     * is updated by the updateMembers( ) function.
+     */
+    Eigen::Vector3d currentVelocityOfBodyInNonInertialReferenceFrame_;
+};
+
+}   // namespace acceleration_models
+}   // namespace tudat
+
+#endif // TUDAT_APPARENT_ACCELERATION_MODEL_H
