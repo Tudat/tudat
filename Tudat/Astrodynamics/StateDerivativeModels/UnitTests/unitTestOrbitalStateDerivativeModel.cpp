027a37a4480bc11beabbfbeb7a7a2b768e06c48c
Dominic Dirkx <D.Dirkx@tudelft.nl>
2014-11-05 12:14:34 +0000

    Adding core of OrbitalStateDerivativeModel (Issue #703)
    git-svn-id: file:///srv/svn/repos/tudat/trunk@558 ac7aa084-b6da-11df-bf42-7fac39f0224f

---
+++ Tudat/Astrodynamics/StateDerivativeModels/UnitTests/unitTestOrbitalStateDerivativeModel.cpp
@@ -0,0 +1,338 @@
+/*    Copyright (c) 2010-2013, Delft University of Technology
+ *    All rights reserved.
+ *
+ *    Redistribution and use in source and binary forms, with or without modification, are
+ *    permitted provided that the following conditions are met:
+ *      - Redistributions of source code must retain the above copyright notice, this list of
+ *        conditions and the following disclaimer.
+ *      - Redistributions in binary form must reproduce the above copyright notice, this list of
+ *        conditions and the following disclaimer in the documentation and/or other materials
+ *        provided with the distribution.
+ *      - Neither the name of the Delft University of Technology nor the names of its contributors
+ *        may be used to endorse or promote products derived from this software without specific
+ *        prior written permission.
+ *
+ *    THIS SOFTWARE IS PROVIDED BY THE COPYRIGHT HOLDERS AND CONTRIBUTORS "AS IS" AND ANY EXPRESS
+ *    OR IMPLIED WARRANTIES, INCLUDING, BUT NOT LIMITED TO, THE IMPLIED WARRANTIES OF
+ *    MERCHANTABILITY AND FITNESS FOR A PARTICULAR PURPOSE ARE DISCLAIMED. IN NO EVENT SHALL THE
+ *    COPYRIGHT HOLDER OR CONTRIBUTORS BE LIABLE FOR ANY DIRECT, INDIRECT, INCIDENTAL, SPECIAL,
+ *    EXEMPLARY, OR CONSEQUENTIAL DAMAGES (INCLUDING, BUT NOT LIMITED TO, PROCUREMENT OF SUBSTITUTE
+ *    GOODS OR SERVICES; LOSS OF USE, DATA, OR PROFITS; OR BUSINESS INTERRUPTION) HOWEVER CAUSED
+ *    AND ON ANY THEORY OF LIABILITY, WHETHER IN CONTRACT, STRICT LIABILITY, OR TORT (INCLUDING
+ *    NEGLIGENCE OR OTHERWISE) ARISING IN ANY WAY OUT OF THE USE OF THIS SOFTWARE, EVEN IF ADVISED
+ *    OF THE POSSIBILITY OF SUCH DAMAGE.
+ *
+ *    Changelog
+ *      YYMMDD    Author            Comment
+ *      140106    J. Geul           File creatad, copied from unitTestOrbitalStateDerivativeModel.
+ *
+ *
+ *    References
+ *
+ *    Notes:
+ *      Test tolerance was set at 5.0e-15 instead of epsilon due to rounding errors in Eigen types
+ *      with entries over a number of orders of magnitude, presumably causing the observed larger
+ *      than epsilon relative differences between expected and computed values.
+ *
+ */
+
+#define BOOST_TEST_MAIN
+
+#include <boost/assign/list_of.hpp>
+#include <boost/bind.hpp>
+#include <boost/function.hpp>
+#include <boost/make_shared.hpp>
+#include <boost/shared_ptr.hpp>
+#include <boost/test/unit_test.hpp>
+#include <boost/test/floating_point_comparison.hpp>
+
+#include <Eigen/Core>
+#include <Eigen/Geometry>
+
+#include <TudatCore/Basics/testMacros.h>
+
+#include "Tudat/Astrodynamics/BasicAstrodynamics/UnitTests/testAccelerationModels.h"
+#include "Tudat/Astrodynamics/BasicAstrodynamics/UnitTests/testBody.h"
+#include "Tudat/Astrodynamics/StateDerivativeModels/orbitalStateDerivativeModel.h"
+
+#include "Tudat/Mathematics/BasicMathematics/linearAlgebraTypes.h"
+
+namespace tudat
+{
+namespace unit_tests
+{
+
+using boost::assign::list_of;
+using basic_mathematics::Vector6d;
+
+//! Rotate vector over arbitrary angles.
+/*!
+ * This function computes a composite rotation of the input vector over arbitrary angles about the
+ * unit x-, y-, and z-axes. This is used in conjunction with rotateOverOtherArbitraryAngles() to
+ * test the frame transformation features provided by the orbitalStateDerivativeModel.
+ * \param inputVector Input vector before rotation.
+ * \return Rotated vector.
+ * \sa orbitalStateDerivativeModel, rotateOverOtherArbitraryAngles().
+ */
+Eigen::Vector3d rotateOverArbitraryAngles( const Eigen::Vector3d& inputVector )
+{
+    // Declare rotation matrix.
+    Eigen::Matrix3d rotationMatrix;
+    rotationMatrix = Eigen::AngleAxisd( -1.15, Eigen::Vector3d::UnitX( ) )
+            * Eigen::AngleAxisd( 0.23, Eigen::Vector3d::UnitY( ) )
+            * Eigen::AngleAxisd( 2.56, Eigen::Vector3d::UnitZ( ) );
+
+    // Compute rotated matrix and return.
+    return  rotationMatrix * inputVector;
+}
+
+//! Rotate vector over other arbitrary angles.
+/*!
+ * This function computes another composite rotation of the input vector over different arbitrary
+ * angles (compared to the rotateOverArbitraryAngles() function) about the unit x-, y-, and z-axes.
+ * This is used in conjunction with rotateOverArbitraryAngles() to test the frame transformation
+ * features provided by the OrbitalStateDerivativeModel.
+ * \param inputVector Input vector before rotation.
+ * \return Rotated vector.
+ * \sa orbitalStateDerivativeModel, rotateOverArbitraryAngles().
+ */
+Eigen::Vector3d rotateOverOtherArbitraryAngles( const Eigen::Vector3d& inputVector )
+{
+    // Declare rotation matrix.
+    Eigen::Matrix3d rotationMatrix;
+    rotationMatrix = Eigen::AngleAxisd( 0.24, Eigen::Vector3d::UnitX( ) )
+            * Eigen::AngleAxisd( -1.55, Eigen::Vector3d::UnitY( ) )
+            * Eigen::AngleAxisd( 2.13, Eigen::Vector3d::UnitZ( ) );
+
+    // Compute rotated matrix and return.
+    return  rotationMatrix * inputVector;
+}
+
+//! Perform Mapping from Acceleration to Cartesian State Derivative
+/*!
+ * This function is implemented in order to not use any external
+ * mapping functions. The function was build completely from pieces of
+ * code of the old cartesianStateDerivativeModel in order to use
+ * completely different, but also verified methods of constructing a
+ * stateDerivative from the total accelerations.
+ * \param currentState The current state.
+ * \param accelerations Total sum of accelerations.
+ * \return stateDerivative
+ */
+Vector6d mapCartesian( const Vector6d& cartesianState, const Eigen::Vector3d& acceleration )
+{
+    
+    // {{{ Snippets from cartesianStateDerivativeModel.h
+    typedef Vector6d CartesianStateDerivativeType;
+
+    // Declare Cartesian state derivative size.
+    unsigned int stateDerivativeSize = cartesianState.rows( );
+
+    // Declare Cartesian state derivative of the same size as Cartesian state.
+    CartesianStateDerivativeType cartesianStateDerivative
+	= CartesianStateDerivativeType::Zero( stateDerivativeSize );
+
+    // Set derivative of position components to current Cartesian velocity.
+    cartesianStateDerivative.segment( 0, stateDerivativeSize / 2 )
+	= cartesianState.segment( stateDerivativeSize / 2, stateDerivativeSize / 2 );
+
+    // Add transformed acceleration to state derivative.
+    cartesianStateDerivative.segment( stateDerivativeSize / 2, stateDerivativeSize / 2 )
+	+= acceleration;
+
+    // Return assembled state derivative.
+    return cartesianStateDerivative;
+
+    // }}} End Snippets from cartesianStateDerivativeModel.h
+}
+
+BOOST_AUTO_TEST_SUITE( test_orbital_state_derivative_model )
+
+//! Test whether 6D Cartesian state derivative model works correctly without frame transformations.
+BOOST_AUTO_TEST_CASE( test_OrbitalStateDerivativeModelWithoutFrameTransformations )
+{
+    using basic_astrodynamics::AccelerationModel3dPointer;
+    using state_derivative_models::OrbitalStateDerivativeModelType;
+    using state_derivative_models::OrbitalStateDerivativeModelPointer;
+
+    // Shortcuts.
+    typedef TestBody< 3, double > TestBody3d;
+    typedef boost::shared_ptr< TestBody3d > TestBody3dPointer;
+    typedef DerivedAccelerationModel< > DerivedAccelerationModel3d;
+    typedef AnotherDerivedAccelerationModel< > AnotherDerivedAccelerationModel3d;
+
+    // Set current state.
+    const Vector6d currentState = ( Eigen::VectorXd( 6 )
+                                    << Eigen::Vector3d( -1.1, 2.2, -3.3 ),
+                                    Eigen::Vector3d( 0.23, 1.67, -0.11 ) ).finished( );
+
+    // Set current time.
+    const double currentTime = 5.6;
+
+    // Set current position.
+    const Eigen::Vector3d currentPosition = currentState.segment( 0, 3 );
+
+    // Set current velocity.
+    const Eigen::Vector3d currentVelocity = currentState.segment( 3, 3 );
+
+    // Create body with zombie time and state.
+    TestBody3dPointer body = boost::make_shared< TestBody3d >( Eigen::VectorXd::Zero( 6 ), 0.0 );
+
+    // Create acceleration models.
+    AccelerationModel3dPointer firstAccelerationModel3d
+            = boost::make_shared< DerivedAccelerationModel3d >(
+                boost::bind( &TestBody3d::getCurrentPosition, body ),
+                boost::bind( &TestBody3d::getCurrentTime, body ) );
+
+    AccelerationModel3dPointer secondAccelerationModel3d
+            = boost::make_shared< AnotherDerivedAccelerationModel3d >(
+                boost::bind( &TestBody3d::getCurrentPosition, body ),
+                boost::bind( &TestBody3d::getCurrentVelocity, body ),
+                boost::bind( &TestBody3d::getCurrentTime, body ) );
+
+    // Create list of acceleration models to provide to state derivative model.
+    OrbitalStateDerivativeModelType::AccelerationModelPointerVector listOfAccelerations
+            = list_of( firstAccelerationModel3d )( secondAccelerationModel3d );
+
+    // Declare Cartesian state derivative model.
+    OrbitalStateDerivativeModelPointer cartesianStateDerivativeModel
+            = boost::make_shared< OrbitalStateDerivativeModelType >(
+                listOfAccelerations,
+                boost::bind( &TestBody3d::setCurrentTimeAndState, body, _1, _2 ),
+	        boost::bind( &mapCartesian, _1, _2 ) );
+
+    // // Set Earth gravitational parameter [m^3 s^-2].
+    // const double earthGravitationalParameter = 3.986004415e14;
+
+    // // Declare Keplerian state derivative model.
+    // OrbitalStateDerivativeModelPointer keplerianStateDerivativeModel
+    //         = boost::make_shared< OrbitalStateDerivativeModelType >(
+    //             listOfAccelerations,
+    //             boost::bind( &TestBody3d::setCurrentTimeAndState, body, _1, _2 ),
+    // 	        boost::bind( &stateDerivativeMapKeplerian, _1, _2, 
+    // 		    earthGravitationalParameter ) );
+
+    // // Declare Modified Equinoctial state derivative model.
+    // OrbitalStateDerivativeModelPointer modifiedEquinoctialStateDerivativeModel
+    //         = boost::make_shared< OrbitalStateDerivativeModelType >(
+    //             listOfAccelerations,
+    //             boost::bind( &TestBody3d::setCurrentTimeAndState, body, _1, _2 ),
+    // 	        boost::bind( &stateDerivativeMapModifiedEquinoctial, _1, _2, 
+    // 		    earthGravitationalParameter ) );
+
+    // Set expected accelerations.
+    const Eigen::Vector3d expectedAccelerationFirstModel
+            = currentPosition / ( currentTime * currentTime );
+    const Eigen::Vector3d expectedAccelerationSecondModel
+            = 0.5 * currentPosition / ( 3.2 * ( currentTime + 3.4 ) * currentTime )
+            + currentVelocity / currentTime;
+
+    // Set expected (cumulative) Cartesian state derivative.
+    const Vector6d expectedCartesianStateDerivative
+            = ( Eigen::VectorXd( 6 ) << currentVelocity,
+                expectedAccelerationFirstModel + expectedAccelerationSecondModel ).finished( );
+
+    // Compute Cartesian state derivative.
+    const Vector6d computedCartesianStateDerivative
+            = cartesianStateDerivativeModel->computeStateDerivative( currentTime, currentState );
+
+    // Check that computed Cartesian state derivative matches expected values.
+    TUDAT_CHECK_MATRIX_BASE( computedCartesianStateDerivative, expectedCartesianStateDerivative )
+            BOOST_CHECK_SMALL( computedCartesianStateDerivative.coeff( row, col ) -
+                               expectedCartesianStateDerivative.coeff( row, col ),
+                               5.0e-15 );
+}
+
+//! Test whether 6D Cartesian state derivative model works correctly with frame transformations.
+BOOST_AUTO_TEST_CASE( test_OrbitalStateDerivativeModelWithFrameTransformations )
+{
+    using basic_astrodynamics::AccelerationModel3dPointer;
+    using state_derivative_models::OrbitalStateDerivativeModelType;
+    using state_derivative_models::OrbitalStateDerivativeModelPointer;
+
+    // Shortcuts.
+    typedef TestBody< 3, double > TestBody3d;
+    typedef boost::shared_ptr< TestBody3d > TestBody3dPointer;
+    typedef DerivedAccelerationModel< > DerivedAccelerationModel3d;
+    typedef AnotherDerivedAccelerationModel< > AnotherDerivedAccelerationModel3d;
+
+    // Set current state.
+    const Vector6d currentState = ( Eigen::VectorXd( 6 )
+                                    << Eigen::Vector3d( -1.1, 2.2, -3.3 ),
+                                    Eigen::Vector3d( 0.23, 1.67, -0.11 ) ).finished( );
+
+    // Set current time.
+    const double currentTime = 5.6;
+
+    // Set current position.
+    const Eigen::Vector3d currentPosition = currentState.segment( 0, 3 );
+
+    // Set current velocity.
+    const Eigen::Vector3d currentVelocity = currentState.segment( 3, 3 );
+
+    // Create body with zombie time and state.
+    TestBody3dPointer body = boost::make_shared< TestBody3d >( Eigen::VectorXd::Zero( 6 ), 0.0 );
+
+    // Create acceleration models.
+    AccelerationModel3dPointer firstAccelerationModel3d
+            = boost::make_shared< DerivedAccelerationModel3d >(
+                boost::bind( &TestBody3d::getCurrentPosition, body ),
+                boost::bind( &TestBody3d::getCurrentTime, body ) );
+
+    AccelerationModel3dPointer secondAccelerationModel3d
+            = boost::make_shared< AnotherDerivedAccelerationModel3d >(
+                boost::bind( &TestBody3d::getCurrentPosition, body ),
+                boost::bind( &TestBody3d::getCurrentVelocity, body ),
+                boost::bind( &TestBody3d::getCurrentTime, body ) );
+
+    // Create list of reference frame transformations for first acceleration model.
+    // NB: The order of this list is VERY important! The order of transformations executed is from
+    // the beginning of the vector to the end sequentially.
+    OrbitalStateDerivativeModelType::ListOfReferenceFrameTransformations listOfFrameTransformations
+            = list_of( &rotateOverArbitraryAngles )( &rotateOverOtherArbitraryAngles );
+
+    // Create list to pass to constructor of acceleration model/frame transformation list pairs.
+    // In this case, there are two frame transformations executed for the first acceleration model,
+    // and none for the second.
+    OrbitalStateDerivativeModelType::ListOfAccelerationFrameTransformationPairs
+            listOfAccelerationFrameTransformations
+            = list_of( std::make_pair( firstAccelerationModel3d, listOfFrameTransformations ) )
+            ( std::make_pair( secondAccelerationModel3d,
+                              OrbitalStateDerivativeModelType::
+                              ListOfReferenceFrameTransformations( ) ) );
+
+    // Declare Cartesian state derivative model.
+    OrbitalStateDerivativeModelPointer stateDerivativeModel
+            = boost::make_shared< OrbitalStateDerivativeModelType >(
+                listOfAccelerationFrameTransformations,
+                boost::bind( &TestBody3d::setCurrentTimeAndState, body, _1, _2 ),
+		boost::bind( &mapCartesian, _1, _2 ) );
+
+    // Set expected accelerations.
+    const Eigen::Vector3d expectedAccelerationFirstModel
+            = rotateOverOtherArbitraryAngles(
+                rotateOverArbitraryAngles( currentPosition / ( currentTime * currentTime ) ) );
+    const Eigen::Vector3d expectedAccelerationSecondModel
+            = 0.5 * currentPosition / ( 3.2 * ( currentTime + 3.4 ) * currentTime )
+            + currentVelocity / currentTime;
+
+    // Set expected (cumulative) Cartesian state derivative.
+    const Vector6d expectedCartesianStateDerivative
+            = ( Eigen::VectorXd( 6 ) << currentVelocity,
+                expectedAccelerationFirstModel + expectedAccelerationSecondModel ).finished( );
+
+    // Compute Cartesian state derivative.
+    const Vector6d computedCartesianStateDerivative
+            = stateDerivativeModel->computeStateDerivative( currentTime, currentState );
+
+    // Check that computed Cartesian state derivative matches expected values.
+    TUDAT_CHECK_MATRIX_BASE( computedCartesianStateDerivative, expectedCartesianStateDerivative )
+            BOOST_CHECK_CLOSE_FRACTION( computedCartesianStateDerivative.coeff( row, col ),
+                                        expectedCartesianStateDerivative.coeff( row, col ),
+                                        5.0e-15 );
+}
+
+BOOST_AUTO_TEST_SUITE_END( )
+
+} // namespace unit_tests
+} // namespace tudat
