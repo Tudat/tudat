661711d6d839caf465a307635f384330408cafa7
Dominic Dirkx <D.Dirkx@tudelft.nl>
2015-02-08 10:31:40 +0000

    Adding modified FindSpice script (Issue #775)
    git-svn-id: file:///srv/svn/repos/tudat/trunk@568 ac7aa084-b6da-11df-bf42-7fac39f0224f

--- Tudat/External/CMake/FindSpice.cmake
+++ Tudat/External/CMake/FindSpice.cmake
@@ -28,6 +28,7 @@
  #      12xxxx    P. Musegaas
  #      12xxxx    D. Dirkx          Adapted to detect the SPICE library.
  #      140127    D. Dirkx          Adapted for custom Spice kernel folder.
+ #      150206    J. Geul           Automatic library find (/w/wo lib prefix) 
  #
  #
  #    References
@@ -44,25 +45,45 @@
  #      FindEigen3.cmake states that redistribution and use is allowed according to the terms of
  #      the 2-clause BSD license.
 
+# If the path has not been set previously or manually, try to autodetect the path
 if(NOT SPICE_BASE_PATH)
     find_path(SPICE_BASE_PATH NAMES SpiceUsr.h
         PATHS
             ${PROJECT_SOURCE_DIR}/External
+	    ${PROJECT_SOURCE_DIR}/..
+	    ${PROJECT_SOURCE_DIR}/../..
+	    ${PROJECT_SOURCE_DIR}/../../..
+	    ${PROJECT_SOURCE_DIR}/../../../..
         PATH_SUFFIXES cspice/include
 )
 endif(NOT SPICE_BASE_PATH)
 
+# If the path is still not set, then autodetection went wrong
 if(NOT SPICE_BASE_PATH)
-  message(STATUS "WARNING: SPICE not found! Make sure SPICE is installed or set USE_CSPICE to 'false'.")
+
+  # Throw a warning and disable SPICE
+  message(WARNING "WARNING: SPICE not found! USE_CSPICE flag has been disabled.")
+  SET(USE_CSPICE false)
+
+else(NOT SPICE_BASE_PATH)
+
+  # Good, path has been set/found, now set important variables and find libraries.
+  set(SPICE_BASE_PATH ${SPICE_BASE_PATH}/..)
+  set(SPICE_INCLUDE_DIR ${SPICE_BASE_PATH}/..)
+  set(SPICE_LIBRARIES_DIR ${SPICE_BASE_PATH}/lib)
+
+  find_library(SPICE_LIBRARIES
+	NAMES libcspice.a libcspice.lib cspice.a cspice.lib
+	PATHS ${SPICE_LIBRARIES_DIR})
+
+  # Let user know which SPICE library was found.
+  message(STATUS "SPICE_LIBRARIES: ${SPICE_LIBRARIES}")
+
+  link_directories(${SPICE_LIBRARIES_DIR})
+
+  include(FindPackageHandleStandardArgs)
+  find_package_handle_standard_args(SPICE DEFAULT_MSG SPICE_INCLUDE_DIR)
+
+  mark_as_advanced(SPICE_INCLUDE_DIR)
+
 endif(NOT SPICE_BASE_PATH)
-
-set(SPICE_BASE_PATH ${SPICE_BASE_PATH}/..)
-set(SPICE_INCLUDE_DIR ${SPICE_BASE_PATH}/..)
-set(SPICE_LIBRARIES_DIR ${SPICE_BASE_PATH}/lib)
-set(SPICE_LIBRARIES "cspice")
-link_directories(${SPICE_LIBRARIES_DIR})
-
-include(FindPackageHandleStandardArgs)
-find_package_handle_standard_args(SPICE DEFAULT_MSG SPICE_INCLUDE_DIR)
-
-mark_as_advanced(SPICE_INCLUDE_DIR)
