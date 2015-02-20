4994325b33c69e160a93f22ae8dd887750c1be6d
Dominic Dirkx <D.Dirkx@tudelft.nl>
2015-02-20 15:45:08 +0000

    Updating Tudat version (new bundle)
    git-svn-id: file:///srv/svn/repos/tudat/trunk@576 ac7aa084-b6da-11df-bf42-7fac39f0224f

--- Tudat/tudatVersion.h
+++ Tudat/tudatVersion.h
@@ -1,3 +1,3 @@
 // This file solely exists for CMake to be able to find the Tudat library.
-#define TUDAT_VERSION_MAJOR 2 
-#define TUDAT_VERSION_MINOR 2 
+#define TUDAT_VERSION_MAJOR 3 
+#define TUDAT_VERSION_MINOR 0 
