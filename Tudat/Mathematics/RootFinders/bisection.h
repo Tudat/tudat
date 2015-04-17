9f089cb33e29ebe7d495bcbf6e562dc03708bb67
Dominic Dirkx <D.Dirkx@tudelft.nl>
2015-04-17 14:22:04 +0000

    Added missing correction to Issue #783 (minor error in previous commit)
    git-svn-id: file:///srv/svn/repos/tudat/trunk@582 ac7aa084-b6da-11df-bf42-7fac39f0224f

--- Tudat/Mathematics/RootFinders/bisection.h
+++ Tudat/Mathematics/RootFinders/bisection.h
@@ -1,4 +1,4 @@
-/*    Copyright (c) 2010-2014, Delft University of Technology
+/*    Copyright (c) 2010-2015, Delft University of Technology
  *    All rights reserved.
  *
  *    Redistribution and use in source and binary forms, with or without modification, are
@@ -51,8 +51,7 @@
 #include <boost/make_shared.hpp>
 #include <boost/shared_ptr.hpp>
 
-#include <TudatCore/Basics/utilityMacros.h>
-
+#include "Tudat/Basics/utilityMacros.h"
 #include "Tudat/Mathematics/RootFinders/rootFinder.h"
 #include "Tudat/Mathematics/RootFinders/terminationConditions.h"
 
