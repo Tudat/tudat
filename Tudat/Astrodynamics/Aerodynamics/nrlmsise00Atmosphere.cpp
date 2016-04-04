1bfbf1cae45cc11ea0d71c1aa940b151b3e900f0
Dominic Dirkx <d.dirkx@tudelft.nl>
2016-04-04 17:43:02 +0200

    With additional comments and style fixes

--- Tudat/Astrodynamics/Aerodynamics/nrlmsise00Atmosphere.cpp
+++ Tudat/Astrodynamics/Aerodynamics/nrlmsise00Atmosphere.cpp
@@ -39,56 +39,69 @@ namespace tudat
 namespace aerodynamics
 {
 
+//! Compute the local atmospheric properties.
 void NRLMSISE00Atmosphere::computeProperties(double altitude, double longitude,
                                              double latitude, double time) {
     // Compute the hash key
     size_t hashKey = hashFunc(altitude, longitude, latitude, time);
 
     // If hash key is same do nothing
-    if (hashKey == hashKey_) {
+    if (hashKey == hashKey_)
+    {
       return;
     }
     hashKey_ = hashKey;
 
+    // Retrieve input data.
     NRLMSISE00Input inputData = nrlmsise00InputFunction_(
-        altitude, longitude, latitude, time);
+        altitude, longitude, latitude, time );
+    std::copy( inputData.apVector.begin( ), inputData.apVector.end( ), aph_.a );
+    std::copy( inputData.switches.begin( ), inputData.switches.end(), flags_.switches);
 
-    std::copy(inputData.apVector.begin(),
-              inputData.apVector.end(), aph_.a);
-    std::copy(inputData.switches.begin(),
-              inputData.switches.end(), flags_.switches);
-
-    input_.g_lat  = latitude;
+    // Set environment properties.
+    input_.g_lat = latitude;
     input_.g_long = longitude;
-    input_.alt    = altitude;
-    input_.year   = inputData.year;
-    input_.doy    = inputData.dayOfTheYear;
-    input_.sec    = inputData.secondOfTheDay;
-    input_.lst    = inputData.localSolarTime;
-    input_.f107   = inputData.f107;
-    input_.f107A  = inputData.f107a;
-    input_.ap     = inputData.apDaily;
-    input_.ap_a   = &aph_;
+    input_.alt = altitude;
+    input_.year = inputData.year;
+    input_.doy = inputData.dayOfTheYear;
+    input_.sec = inputData.secondOfTheDay;
+    input_.lst = inputData.localSolarTime;
+    input_.f107 = inputData.f107;
+    input_.f107A = inputData.f107a;
+    input_.ap = inputData.apDaily;
+    input_.ap_a = &aph_;
 
-    gtd7(&input_, &flags_, &output_);
+    // Call Neutral Atmosphere Empircial Model from the surface to lower exosphere from NRLMSISE00.
+    gtd7( &input_, &flags_, &output_ );
 
-    density_ = output_.d[5]*1000.0;
-    temperature_ = output_.t[1];
-    pressure_ = TUDAT_NAN;
+    // Compute current density and temperature.
+    density_ = output_.d[ 5 ] * 1000.0;
+    temperature_ = output_.t[ 1 ];
+    if( useIdealGasLaw_ )
+    {
+        pressure_ = density_ * physical_constants::SPECIFIC_GAS_CONSTANT_AIR * temperature_;
+    }
+    else
+    {
+        pressure_ = TUDAT_NAN;
+    }
+
 }
 
-std::pair< std::vector< double >, std::vector< double >>
-    NRLMSISE00Atmosphere::getFullOutput(
-        double altitude, double longitude,
-        double latitude, double time) {
+//! Get the full model output
+std::pair< std::vector< double >, std::vector< double > >
+NRLMSISE00Atmosphere::getFullOutput( const double altitude, const double longitude,
+                                     const double latitude, const double time )
+{
     // Compute the properties
     computeProperties(altitude, longitude, latitude, time);
     std::pair< std::vector< double >, std::vector< double >> output;
+
     // Copy array members of struct to vectors on the pair.
-    output.first  = std::vector< double >(output_.d,
-        output_.d + sizeof output_.d / sizeof output_.d[0]);
-    output.second = std::vector< double >(output_.t,
-        output_.t + sizeof output_.t / sizeof output_.t[0]);
+    output.first = std::vector< double >(
+                output_.d, output_.d + sizeof output_.d / sizeof output_.d[ 0 ] );
+    output.second = std::vector< double >(
+                output_.t, output_.t + sizeof output_.t / sizeof output_.t[ 0 ] );
     return output;
 }
 
