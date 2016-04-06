1debe0a73dac71a3e57a151b200b2287601ec6f1
mvandenbroeck <m.m.h.vandenbroeck@student.tudelft.nl>
2016-04-06 18:45:20 +0200

    new header file to support the unifiedStateModelElementConversions.cpp file

---
+++ Tudat/Astrodynamics/BasicAstrodynamics/unifiedStateModelElementConversions.h
@@ -0,0 +1,71 @@
+/*    Copyright (c) 2010-2016, Delft University of Technology
+ *    All rights reserved.
+ *
+ *    Redistribution and use in source and binary forms, with or without modification, are
+ *    permitted provided that the following conditions are met:
+ *      - Redistributions of source code must retain the above copyright notice, this list of
+ *        conditions and the following disclaimer.
+ *      - Redistributions in binary form must reproduce the above copyright notice, this list of
+ *        conditions and the following disclaimer in the documentation and/or other materials
+ *        provided with the distribution.
+ *      - Neither the name of the Delft University of Technology nor the names of its contributors
+ *        may be used to endorse or promote products derived from this software without specific
+ *        prior written permission.
+ *
+ *    THIS SOFTWARE IS PROVIDED BY THE COPYRIGHT HOLDERS AND CONTRIBUTORS "AS IS" AND ANY EXPRESS
+ *    OR IMPLIED WARRANTIES, INCLUDING, BUT NOT LIMITED TO, THE IMPLIED WARRANTIES OF
+ *    MERCHANTABILITY AND FITNESS FOR A PARTICULAR PURPOSE ARE DISCLAIMED. IN NO EVENT SHALL THE
+ *    COPYRIGHT HOLDER OR CONTRIBUTORS BE LIABLE FOR ANY DIRECT, INDIRECT, INCIDENTAL, SPECIAL,
+ *    EXEMPLARY, OR CONSEQUENTIAL DAMAGES (INCLUDING, BUT NOT LIMITED TO, PROCUREMENT OF SUBSTITUTE
+ *    GOODS OR SERVICES; LOSS OF USE, DATA, OR PROFITS; OR BUSINESS INTERRUPTION) HOWEVER CAUSED
+ *    AND ON ANY THEORY OF LIABILITY, WHETHER IN CONTRACT, STRICT LIABILITY, OR TORT (INCLUDING
+ *    NEGLIGENCE OR OTHERWISE) ARISING IN ANY WAY OUT OF THE USE OF THIS SOFTWARE, EVEN IF ADVISED
+ *    OF THE POSSIBILITY OF SUCH DAMAGE.
+ *
+ *    Changelog
+ *      YYMMDD    Author            Comment
+ *      160406    M. Van den Broeck File created.
+ *
+ *    References
+ *      The Unified State Model: Derivation and Applications in Astrodynamics and Navigation,
+ *          Vivek Vittaldevs; M.Sc. Thesis TU Delft (2010). Available on repository.tudelft.nl
+ *
+ *    Notes
+ *
+ */
+
+#ifndef TUDAT_UNIFIED_STATE_MODEL_ELEMENT_CONVERSIONS_H
+#define TUDAT_UNIFIED_STATE_MODEL_ELEMENT_CONVERSIONS_H
+
+#include "Tudat/Mathematics/BasicMathematics/linearAlgebraTypes.h"
+
+namespace tudat
+{
+
+namespace orbital_element_conversions
+{
+
+//! Convert Keplerian to Unified State Model elements.
+/*!
+ * Converts Keplerian to Unified State Model elements.
+ * \param keplerianElements Vector containing Keplerian elements. Order of elements is important!
+ *         keplerianElements( 0 ) = semi-major axis,                                            [m]
+ *         keplerianElements( 1 ) = eccentricity,                                               [-]
+ *         keplerianElements( 2 ) = inclination (in range [0,PI]),                            [rad]
+ *         keplerianElements( 3 ) = argument of periapsis,                                    [rad]
+ *         keplerianElements( 4 ) = longitude of ascending node,                              [rad]
+ *         keplerianElements( 5 ) = true anomaly.                                             [rad]
+ * \return Converted state in Unified State Model elements. The order of elements is fixed!
+ *         unifiedStateModelElements( 0 ) = C hodograph element,                              [m/s]
+ *         unifiedStateModelElements( 1 ) = Rf1 hodograph element,                            [m/s]
+ *         unifiedStateModelElements( 2 ) = Rf1 hodograph element,                            [m/s]
+ *         unifiedStateModelElements( 3 ) = epsilon1 quaternion,                                [-]
+ *         unifiedStateModelElements( 4 ) = epsilon2 quaternion,                                [-]
+ *         unifiedStateModelElements( 5 ) = epsilon3 quaternion,                                [-]
+ *         unifiedStateModelElements( 6 ) = eta quaternion.                                     [-]
+ */
+basic_mathematics::Vector6d convertKeplerianToUnifiedStateModelElements(
+        const basic_mathematics::Vector6d& keplerianElements );
+
+}
+}
