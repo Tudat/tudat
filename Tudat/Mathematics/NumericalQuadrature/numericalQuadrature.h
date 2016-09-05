93c433bbf8d8b4a39f65dc23d85308378bd18df9
Dominic Dirkx <d.dirkx@tudelft.nl>
2016-09-05 17:29:23 +0200

    Adding Numerical Quadrature modifications

--- Tudat/Mathematics/NumericalQuadrature/numericalQuadrature.h
+++ Tudat/Mathematics/NumericalQuadrature/numericalQuadrature.h
@@ -1,41 +1,18 @@
-/*    Copyright (c) 2010-2015, Delft University of Technology
- *    All rights reserved.
+/*    Copyright (c) 2010-2016, Delft University of Technology
+ *    All rigths reserved
  *
- *    Redistribution and use in source and binary forms, with or without modification, are
- *    permitted provided that the following conditions are met:
- *      - Redistributions of source code must retain the above copyright notice, this list of
- *        conditions and the following disclaimer.
- *      - Redistributions in binary form must reproduce the above copyright notice, this list of
- *        conditions and the following disclaimer in the documentation and/or other materials
- *        provided with the distribution.
- *      - Neither the name of the Delft University of Technology nor the names of its contributors
- *        may be used to endorse or promote products derived from this software without specific
- *        prior written permission.
- *
- *    THIS SOFTWARE IS PROVIDED BY THE COPYRIGHT HOLDERS AND CONTRIBUTORS "AS IS" AND ANY EXPRESS
- *    OR IMPLIED WARRANTIES, INCLUDING, BUT NOT LIMITED TO, THE IMPLIED WARRANTIES OF
- *    MERCHANTABILITY AND FITNESS FOR A PARTICULAR PURPOSE ARE DISCLAIMED. IN NO EVENT SHALL THE
- *    COPYRIGHT HOLDER OR CONTRIBUTORS BE LIABLE FOR ANY DIRECT, INDIRECT, INCIDENTAL, SPECIAL,
- *    EXEMPLARY, OR CONSEQUENTIAL DAMAGES (INCLUDING, BUT NOT LIMITED TO, PROCUREMENT OF SUBSTITUTE
- *    GOODS OR SERVICES; LOSS OF USE, DATA, OR PROFITS; OR BUSINESS INTERRUPTION) HOWEVER CAUSED
- *    AND ON ANY THEORY OF LIABILITY, WHETHER IN CONTRACT, STRICT LIABILITY, OR TORT (INCLUDING
- *    NEGLIGENCE OR OTHERWISE) ARISING IN ANY WAY OUT OF THE USE OF THIS SOFTWARE, EVEN IF ADVISED
- *    OF THE POSSIBILITY OF SUCH DAMAGE.
- *
- *    Changelog
- *      YYMMDD    Author            Comment
- *      120716    D. Dirkx          Creation of file.
- *
- *    References
- *
- *    Notes
- *
+ *    This file is part of the Tudat. Redistribution and use in source and
+ *    binary forms, with or without modification, are permitted exclusively
+ *    under the terms of the Modified BSD license. You should have received
+ *    a copy of the license with this file. If not, please or visit:
+ *    http://tudat.tudelft.nl/LICENSE.
  */
 
 #ifndef TUDAT_NUMERICAL_QUADRATURE_H
 #define TUDAT_NUMERICAL_QUADRATURE_H
 
 #include <vector>
+
 #include <Eigen/Core>
 
 namespace tudat
@@ -46,11 +23,7 @@ namespace numerical_quadrature
 //! Base class for numerical quadrature.
 /*!
  * Base class for the numerical quadrature methods included in Tudat, the dependent and independent variable
- * types are specified as user parameters, as are the number of dimensions. The number of
- * dimensions is chosen as a template parameter, so that a boost multi_array of this
- * dimension can be used as a member variable of derived classes.
- * \tparam IndependentVariableType Type of independent variable(s).
- * \tparam IndependentVariableType Type of dependent variable.
+ * types are specified as template parameters.
  */
 template< typename IndependentVariableType, typename DependentVariableType >
 class NumericalQuadrature
@@ -58,34 +31,24 @@ class NumericalQuadrature
 public:
 
     //! Destructor.
-    /*!
-     * Destructor.
-     */
     virtual ~NumericalQuadrature( ) { }
 
-    //! Integrate.
+    //! This function returns the value of the numerical integration performed by quadrature.
     /*!
-     * This function performs the integration. It must be implemented in derived classes.
+     * This function returns the value of the numerical integration. It must be implemented in derived classes.
      * \return Integral of the data.
      */
-    virtual DependentVariableType integrate(  ) = 0;
+    virtual DependentVariableType getQuadrature( ) = 0;
 
-
-    void resetData( const std::vector< IndependentVariableType >& independentVariables,
-                    const std::vector< DependentVariableType >& dependentVariables)
-    {
-        independentVariables_ = independentVariables;
-        dependentVariables_ = dependentVariables;
-    }
 
 protected:
 
-
-    //! Independent variables.
-    std::vector< IndependentVariableType > independentVariables_;
-
-    //! Dependent variables.
-    std::vector< DependentVariableType > dependentVariables_;
+    //! Function that is called to perform the numerical quadrature
+    /*!
+     * Function that is called to perform the numerical quadrature. It must be implemented in derived classes. Any
+     * implementation of performQuadrature must be made consistent with that of getQuadrature.
+     */
+    virtual void performQuadrature( ) = 0;
 
 private:
 
@@ -93,6 +56,7 @@ private:
 };
 
 } // namespace numerical_quadrature
+
 } // namespace tudat
 
 #endif // TUDAT_NUMERICAL_QUADRATURE_H
