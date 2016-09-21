ebffebc8780a02efd610b1e120fef2887553fda7
Dominic Dirkx <d.dirkx@tudelft.nl>
2016-09-21 14:14:41 +0200

    Added missing file headers, etc.

--- Tudat/SimulationSetup/tudatSimulationHeader.h
+++ Tudat/SimulationSetup/tudatSimulationHeader.h
@@ -1,5 +1,16 @@
-#ifndef TUDATAPI_H
-#define TUDATAPI_H
+/*    Copyright (c) 2010-2016, Delft University of Technology
+ *    All rigths reserved
+ *
+ *    This file is part of the Tudat. Redistribution and use in source and
+ *    binary forms, with or without modification, are permitted exclusively
+ *    under the terms of the Modified BSD license. You should have received
+ *    a copy of the license with this file. If not, please or visit:
+ *    http://tudat.tudelft.nl/LICENSE.
+ */
+
+
+#ifndef TUDATSIMULATIONHEADER_H
+#define TUDATSIMULATIONHEADER_H
 
 #include "Tudat/Astrodynamics/BasicAstrodynamics/physicalConstants.h"
 #include "Tudat/Astrodynamics/BasicAstrodynamics/timeConversions.h"
@@ -36,4 +47,4 @@
 #include "Tudat/SimulationSetup/PropagationSetup/createNumericalSimulator.h"
 #include "Tudat/SimulationSetup/PropagationSetup/thrustSettings.h"
 
-#endif // TUDATAPI_H
+#endif // TUDATSIMULATIONHEADER_H
