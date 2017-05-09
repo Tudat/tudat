9a4f749b860bb601c4f3065cb98622f07a4d847d
Aleix Pinardell <J.A.PinardellPons@student.tudelft.nl>
2017-05-09 10:44:44 +0200

    Added periapsis altitude dependent variable

--- Tudat/SimulationSetup/PropagationSetup/propagationOutput.cpp
+++ Tudat/SimulationSetup/PropagationSetup/propagationOutput.cpp
@@ -205,6 +205,9 @@ int getDependentVariableSize(
     case lvlh_to_inertial_frame_rotation_dependent_variable:
         variableSize = 9;
         break;
+    case periapsis_altitude_dependent_variable:
+        variableSize = 1;
+        break;
     default:
         std::string errorMessage = "Error, did not recognize dependent variable size of type: " +
                 boost::lexical_cast< std::string >( dependentVariableSettings );
