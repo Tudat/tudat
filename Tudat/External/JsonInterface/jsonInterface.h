a643bccc7771758dc0bb18d6118b50124f5397dd
Aleix Pinardell <J.A.PinardellPons@student.tudelft.nl>
2017-07-06 17:08:00 +0200

    Basic JSON file parsing

--- Tudat/External/JsonInterface/jsonInterface.h
+++ Tudat/External/JsonInterface/jsonInterface.h
@@ -11,6 +11,9 @@
 #ifndef TUDAT_JSONINTERFACE_H
 #define TUDAT_JSONINTERFACE_H
 
+#include <boost/filesystem.hpp>
+
+#include "json/src/json.hpp"
 
 #include "integratorSettings.h"
 
@@ -21,7 +24,40 @@ namespace tudat
 namespace json_interface
 {
 
+using json = nlohmann::json;
+typedef boost::filesystem::path path;
 
+json parseInputFile( std::string inputFile )
+{
+    // Add .json extension if necessary
+    std::string inputFileWithExtension = inputFile;
+    if ( inputFileWithExtension.find(".json") == std::string::npos )
+    {
+        inputFileWithExtension += ".json";
+    }
+
+    // Determine the absolute path of the directory where the input file is located
+    path inputDirectory = boost::filesystem::canonical( inputFileWithExtension ).parent_path();
+
+    // Determine the filename (without file extension) of the input file
+    // path inputFilename = path( inputFile ).stem();
+
+    // Build absolute path
+    path inputPath = inputDirectory / path( inputFileWithExtension ).filename();
+
+    // Check that input file exists
+    if ( ! boost::filesystem::exists( inputPath ) )
+    {
+        std::cerr << "The input file \"" << inputPath << "\" does not exist." << std::endl;
+        std::terminate();
+    }
+
+    // Read file
+    std::ifstream stream( inputPath.string() );
+    // std::string contents( ( std::istreambuf_iterator<char>(ifs) ), ( std::istreambuf_iterator<char>() ) );
+
+    return json::parse( stream );
+}
 
 } // namespace json_interfaces
 
