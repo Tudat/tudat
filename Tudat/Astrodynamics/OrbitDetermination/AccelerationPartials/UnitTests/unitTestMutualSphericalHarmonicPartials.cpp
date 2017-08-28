cabc580c3f5ee2b014965515665a900fd0359004
Dominic Dirkx <d.dirkx@tudelft.nl>
2017-08-28 16:18:31 +0200

    Modifying Mars/Jupiter system kernels

--- Tudat/Astrodynamics/OrbitDetermination/AccelerationPartials/UnitTests/unitTestMutualSphericalHarmonicPartials.cpp
+++ Tudat/Astrodynamics/OrbitDetermination/AccelerationPartials/UnitTests/unitTestMutualSphericalHarmonicPartials.cpp
@@ -91,9 +91,7 @@ BOOST_AUTO_TEST_CASE( testMutualSphericalHarmonicGravityPartials )
 
     spice_interface::loadSpiceKernelInTudat( kernelsPath + "pck00009.tpc");
     spice_interface::loadSpiceKernelInTudat( kernelsPath + "de-403-masses.tpc");
-    spice_interface::loadSpiceKernelInTudat( kernelsPath + "de421.bsp");
-    spice_interface::loadSpiceKernelInTudat( kernelsPath + "mar097short.bsp");
-    spice_interface::loadSpiceKernelInTudat( kernelsPath + "naif0009.tls");
+    spice_interface::loadSpiceKernelInTudat( kernelsPath + "planetaryOrbitKernel.tm" );
 
 
     std::vector< std::string > bodyList;
