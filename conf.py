e8e41ab4bfe74eab947597fd24a713641831bb4d
Frank Hogervorst <f.c.hogervorst@student.tudelft.nl>
2017-10-02 18:04:54 +0200

    added Graphvix extension for diagrams

--- conf.py
+++ conf.py
@@ -32,6 +32,7 @@
 # ones.
 extensions = ['sphinx.ext.imgmath']
 extensions = ['sphinx.ext.mathjax']
+extensions = ['sphinx.ext.graphviz']
 
 # Add any paths that contain templates here, relative to this directory.
 templates_path = ['_templates']
