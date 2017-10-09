a4a35e939619fe5ff33733bdf8ebc4eb3c41948c
Frank Hogervorst <f.c.hogervorst@student.tudelft.nl>
2017-10-09 14:42:47 +0200

    svg format for diagrams

--- conf.py
+++ conf.py
@@ -34,6 +34,8 @@ extensions = ['sphinx.ext.imgmath']
 extensions = ['sphinx.ext.mathjax']
 extensions = ['sphinx.ext.graphviz']
 
+graphviz_output_format = 'svg'
+
 # Add any paths that contain templates here, relative to this directory.
 templates_path = ['_templates']
 
