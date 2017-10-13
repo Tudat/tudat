d80e1a7f71a4fbb9b9d10c9cb518bd44bd558d3c
Dominic Dirkx <d.dirkx@tudelft.nl>
2017-10-13 20:17:19 +0200

    Tidal dissipation in satellite seems to be working. Should check formulation with VL.

--- Tudat/Astrodynamics/Gravitation/UnitTests/unitTestDirectTidalDissipation.cpp
+++ Tudat/Astrodynamics/Gravitation/UnitTests/unitTestDirectTidalDissipation.cpp
@@ -14,6 +14,7 @@
 
 #include "Tudat/SimulationSetup/tudatSimulationHeader.h"
 #include "Tudat/Mathematics/BasicMathematics/leastSquaresEstimation.h"
+#include "Tudat/InputOutput/basicInputOutput.h"
 
 namespace tudat
 {
@@ -46,7 +47,7 @@ BOOST_AUTO_TEST_CASE( testMutualSphericalHarmonicGravity )
 
     // Specify initial time
     double initialTime = 0.0;
-    double finalTime = 10.0 * physical_constants::JULIAN_YEAR;
+    double finalTime = 2.0 * physical_constants::JULIAN_YEAR;
 
     // Get body settings.
     std::map< std::string, boost::shared_ptr< BodySettings > > bodySettings =
@@ -68,7 +69,7 @@ BOOST_AUTO_TEST_CASE( testMutualSphericalHarmonicGravity )
               cosineCoefficients, sineCoefficients, "IAU_Io" );
 
     bodySettings[ "Io" ]->ephemerisSettings = boost::make_shared< KeplerEphemerisSettings >(
-                ( Eigen::Vector6d( )<< 421.8E6, 0.004, 0.0, 0.0, 0.0, 0.0 ).finished( ), 0.0,
+                ( Eigen::Vector6d( )<< 1.0 * 421.8E6, 1.0 * 0.004, 0.0, 0.0, 0.0, 0.0 ).finished( ), 0.0,
                 getBodyGravitationalParameter( "Jupiter" ) + getBodyGravitationalParameter( "Io" ), "Jupiter", "ECLIPJ2000" );
     bodySettings[ "Europa" ]->ephemerisSettings = boost::make_shared< KeplerEphemerisSettings >(
                 ( Eigen::Vector6d( )<< 671.1E6, 0.009, 0.0, 0.0, 0.0, 0.0 ).finished( ), 0.0,
@@ -90,76 +91,76 @@ BOOST_AUTO_TEST_CASE( testMutualSphericalHarmonicGravity )
     double jupiterLoveNumber = 0.1;
     double jupiterTimeLag = 100.0;
 
-    const double fixedStepSize = 1800.0;
+    const double fixedStepSize = 450.0;
     boost::shared_ptr< IntegratorSettings< > > integratorSettings =
             boost::make_shared< RungeKuttaVariableStepSizeSettings< > >
             ( rungeKuttaVariableStepSize, 0.0, fixedStepSize,
-              RungeKuttaCoefficients::rungeKuttaFehlberg78, 900.0, 900.0, 1.0, 1.0);
+              RungeKuttaCoefficients::rungeKuttaFehlberg78, fixedStepSize, fixedStepSize, 1.0, 1.0);
 
-    //    std::map< double, Eigen::VectorXd > integrationResultWithDissipation;
-    //    std::map< double, Eigen::VectorXd > integrationResultWithDissipationKepler;
+    std::map< double, Eigen::VectorXd > integrationResultWithDissipation;
+    std::map< double, Eigen::VectorXd > integrationResultWithDissipationKepler;
 
-    //    std::map< double, double > semiMajorAxes, eccentricities;
-    //    {
-    //        std::map< std::string, std::vector< boost::shared_ptr< AccelerationSettings > > > accelerationsOfIo;
-    //        accelerationsOfIo[ "Jupiter" ].push_back( boost::make_shared< AccelerationSettings >(
-    //                                                      basic_astrodynamics::central_gravity ) );
-    //        accelerationsOfIo[ "Jupiter" ].push_back( boost::make_shared< DirectTidalDissipationAccelerationSettings >(
-    //                                                      jupiterLoveNumber, jupiterTimeLag, false ) );
-    //        accelerationMap[ "Io" ] = accelerationsOfIo;
-    //        basic_astrodynamics::AccelerationMap accelerationModelMap = createAccelerationModelsMap(
-    //                    bodyMap, accelerationMap, bodiesToPropagate, centralBodies );
+    std::map< double, double > semiMajorAxes, eccentricities;
+    {
+        std::map< std::string, std::vector< boost::shared_ptr< AccelerationSettings > > > accelerationsOfIo;
+        accelerationsOfIo[ "Jupiter" ].push_back( boost::make_shared< AccelerationSettings >(
+                                                      basic_astrodynamics::central_gravity ) );
+        accelerationsOfIo[ "Jupiter" ].push_back( boost::make_shared< DirectTidalDissipationAccelerationSettings >(
+                                                      jupiterLoveNumber, jupiterTimeLag, false ) );
+        accelerationMap[ "Io" ] = accelerationsOfIo;
+        basic_astrodynamics::AccelerationMap accelerationModelMap = createAccelerationModelsMap(
+                    bodyMap, accelerationMap, bodiesToPropagate, centralBodies );
 
-    //        boost::shared_ptr< TranslationalStatePropagatorSettings< double > > propagatorSettings =
-    //                boost::make_shared< TranslationalStatePropagatorSettings< double > >
-    //                ( centralBodies, accelerationModelMap, bodiesToPropagate, getInitialStatesOfBodies(
-    //                      bodiesToPropagate, centralBodies, bodyMap, initialTime ), finalTime );
+        boost::shared_ptr< TranslationalStatePropagatorSettings< double > > propagatorSettings =
+                boost::make_shared< TranslationalStatePropagatorSettings< double > >
+                ( centralBodies, accelerationModelMap, bodiesToPropagate, getInitialStatesOfBodies(
+                      bodiesToPropagate, centralBodies, bodyMap, initialTime ), finalTime );
 
 
-    //        ///////////////////////////////////////////////////////////////////////////////////////////////////////////////////////
-    //        ///////////////////////             PROPAGATE ORBIT            ////////////////////////////////////////////////////////
-    //        ///////////////////////////////////////////////////////////////////////////////////////////////////////////////////////
-    //        // Create simulation object and propagate dynamics.
-    //        SingleArcDynamicsSimulator< > dynamicsSimulator(
-    //                    bodyMap, integratorSettings, propagatorSettings, true, false, false );
-    //        integrationResultWithDissipation = dynamicsSimulator.getEquationsOfMotionNumericalSolution( );
+        ///////////////////////////////////////////////////////////////////////////////////////////////////////////////////////
+        ///////////////////////             PROPAGATE ORBIT            ////////////////////////////////////////////////////////
+        ///////////////////////////////////////////////////////////////////////////////////////////////////////////////////////
+        // Create simulation object and propagate dynamics.
+        SingleArcDynamicsSimulator< > dynamicsSimulator(
+                    bodyMap, integratorSettings, propagatorSettings, true, false, false );
+        integrationResultWithDissipation = dynamicsSimulator.getEquationsOfMotionNumericalSolution( );
 
-    //        for( std::map< double, Eigen::VectorXd >::const_iterator mapIterator = integrationResultWithDissipation.begin( );
-    //             mapIterator != integrationResultWithDissipation.end( ); mapIterator++ )
-    //        {
-    //            integrationResultWithDissipationKepler[ mapIterator->first ] =
-    //                    orbital_element_conversions::convertCartesianToKeplerianElements< double >(
-    //                        mapIterator->second, getBodyGravitationalParameter( "Jupiter" ) +
-    //                        getBodyGravitationalParameter( "Io" ) );
-    //            semiMajorAxes[ mapIterator->first ] = integrationResultWithDissipationKepler[ mapIterator->first ]( 0 ) -
-    //                    integrationResultWithDissipationKepler.begin( )->second( 0 );
-    //            eccentricities[ mapIterator->first ] = integrationResultWithDissipationKepler[ mapIterator->first ]( 1 ) -
-    //                    integrationResultWithDissipationKepler.begin( )->second( 1 );
+        for( std::map< double, Eigen::VectorXd >::const_iterator mapIterator = integrationResultWithDissipation.begin( );
+             mapIterator != integrationResultWithDissipation.end( ); mapIterator++ )
+        {
+            integrationResultWithDissipationKepler[ mapIterator->first ] =
+                    orbital_element_conversions::convertCartesianToKeplerianElements< double >(
+                        mapIterator->second, getBodyGravitationalParameter( "Jupiter" ) +
+                        getBodyGravitationalParameter( "Io" ) );
+            semiMajorAxes[ mapIterator->first ] = integrationResultWithDissipationKepler[ mapIterator->first ]( 0 ) -
+                    integrationResultWithDissipationKepler.begin( )->second( 0 );
+            eccentricities[ mapIterator->first ] = integrationResultWithDissipationKepler[ mapIterator->first ]( 1 ) -
+                    integrationResultWithDissipationKepler.begin( )->second( 1 );
 
-    //        }
-    //    }
+        }
+    }
 
-    //    std::vector< double > semiMajorAxisFit = linear_algebra::getLeastSquaresPolynomialFit(
-    //                semiMajorAxes, boost::assign::list_of( 0 )( 1 ) );
-    //    std::vector< double > eccentricityFit = linear_algebra::getLeastSquaresPolynomialFit(
-    //                eccentricities, boost::assign::list_of( 0 )( 1 ) );
+    std::vector< double > semiMajorAxisFit = linear_algebra::getLeastSquaresPolynomialFit(
+                semiMajorAxes, boost::assign::list_of( 0 )( 1 ) );
+    std::vector< double > eccentricityFit = linear_algebra::getLeastSquaresPolynomialFit(
+                eccentricities, boost::assign::list_of( 0 )( 1 ) );
 
-    //    Eigen::Vector6d intialKeplerElements = integrationResultWithDissipationKepler.begin( )->second;
-    //    double meanMotion = basic_astrodynamics::computeKeplerMeanMotion(
-    //                intialKeplerElements( 0 ), getBodyGravitationalParameter( "Jupiter" ) + getBodyGravitationalParameter( "Io" ) );
-    //    double orbitalPeriod = 2.0 * mathematical_constants::PI / meanMotion;
-    //    double jupiterForcingTime = 9.925 * 3600.0 * orbitalPeriod / ( 2.0 * std::fabs( orbitalPeriod - 9.925 * 3600.0 ) );
-    //    double jupiterQualityFactor = 1.0 / std::sin( 2.0 * mathematical_constants::PI * jupiterTimeLag / ( jupiterForcingTime ) );
+    Eigen::Vector6d intialKeplerElements = integrationResultWithDissipationKepler.begin( )->second;
+    double meanMotion = basic_astrodynamics::computeKeplerMeanMotion(
+                intialKeplerElements( 0 ), getBodyGravitationalParameter( "Jupiter" ) + getBodyGravitationalParameter( "Io" ) );
+    double orbitalPeriod = 2.0 * mathematical_constants::PI / meanMotion;
+    double jupiterForcingTime = 9.925 * 3600.0 * orbitalPeriod / ( 2.0 * std::fabs( orbitalPeriod - 9.925 * 3600.0 ) );
+    double jupiterQualityFactor = 1.0 / std::sin( 2.0 * mathematical_constants::PI * jupiterTimeLag / ( jupiterForcingTime ) );
 
-    //    double theoreticalSemiMajorAxisRateFromJupiterTide =  3.0 * jupiterLoveNumber / jupiterQualityFactor * getBodyGravitationalParameter( "Io" ) /
-    //            getBodyGravitationalParameter( "Jupiter" ) * std::pow( getAverageRadius( "Jupiter" ) / intialKeplerElements( 0 ), 5.0 ) *
-    //            intialKeplerElements( 0 ) * meanMotion;
-    //    double theoreticaEccentricityRateFromJupiterTide =  57.0 / 8.0 * jupiterLoveNumber / jupiterQualityFactor * getBodyGravitationalParameter( "Io" ) /
-    //            getBodyGravitationalParameter( "Jupiter" ) * std::pow( getAverageRadius( "Jupiter" ) / intialKeplerElements( 0 ), 5.0 ) *
-    //            intialKeplerElements( 1 ) * meanMotion;
+    double theoreticalSemiMajorAxisRateFromJupiterTide =  3.0 * jupiterLoveNumber / jupiterQualityFactor * getBodyGravitationalParameter( "Io" ) /
+            getBodyGravitationalParameter( "Jupiter" ) * std::pow( getAverageRadius( "Jupiter" ) / intialKeplerElements( 0 ), 5.0 ) *
+            intialKeplerElements( 0 ) * meanMotion;
+    double theoreticaEccentricityRateFromJupiterTide =  57.0 / 8.0 * jupiterLoveNumber / jupiterQualityFactor * getBodyGravitationalParameter( "Io" ) /
+            getBodyGravitationalParameter( "Jupiter" ) * std::pow( getAverageRadius( "Jupiter" ) / intialKeplerElements( 0 ), 5.0 ) *
+            intialKeplerElements( 1 ) * meanMotion;
 
-    //    BOOST_CHECK_CLOSE_FRACTION( semiMajorAxisFit.at( 1 ), theoreticalSemiMajorAxisRateFromJupiterTide, 2.0E-3 );
-    //    BOOST_CHECK_CLOSE_FRACTION( eccentricityFit.at( 1 ), theoreticaEccentricityRateFromJupiterTide, 1.0E-1 );
+    BOOST_CHECK_CLOSE_FRACTION( semiMajorAxisFit.at( 1 ), theoreticalSemiMajorAxisRateFromJupiterTide, 2.0E-3 );
+    BOOST_CHECK_CLOSE_FRACTION( eccentricityFit.at( 1 ), theoreticaEccentricityRateFromJupiterTide, 1.0E-1 );
 
 
     std::map< double, Eigen::VectorXd > integrationResultWithDissipationInIo;
@@ -193,7 +194,7 @@ BOOST_AUTO_TEST_CASE( testMutualSphericalHarmonicGravity )
                     bodyMap, integratorSettings, propagatorSettings, true, false, false );
         integrationResultWithDissipationInIo = dynamicsSimulator.getEquationsOfMotionNumericalSolution( );
 
-        for( std::map< double, Eigen::VectorXd >::const_iterator mapIterator = integrationResultWithDissipationInIo.begin( );
+        for( std::map< double, Eigen::VectorXd >::const_iterator mapIterator = ( integrationResultWithDissipationInIo.begin( )++ );
              mapIterator != integrationResultWithDissipationInIo.end( ); mapIterator++ )
         {
             integrationResultWithDissipationInIoKepler[ mapIterator->first ] =
@@ -210,19 +211,28 @@ BOOST_AUTO_TEST_CASE( testMutualSphericalHarmonicGravity )
 
 
 
-    Eigen::Vector6d intialKeplerElements = integrationResultWithDissipationInIoKepler.begin( )->second;
-    double meanMotion = basic_astrodynamics::computeKeplerMeanMotion(
+    intialKeplerElements = integrationResultWithDissipationInIoKepler.begin( )->second;
+    meanMotion = basic_astrodynamics::computeKeplerMeanMotion(
                 intialKeplerElements( 0 ), getBodyGravitationalParameter( "Jupiter" ) + getBodyGravitationalParameter( "Io" ) );
-    double orbitalPeriod = 2.0 * mathematical_constants::PI / meanMotion;
-//    double jupiterForcingTime = 9.925 * 3600.0 * orbitalPeriod / ( 2.0 * std::fabs( orbitalPeriod - 9.925 * 3600.0 ) );
-//    double jupiterQualityFactor = 1.0 / std::sin( 2.0 * mathematical_constants::PI * jupiterTimeLag / ( jupiterForcingTime ) );
+    orbitalPeriod = 2.0 * mathematical_constants::PI / meanMotion;
+    //    double jupiterForcingTime = 9.925 * 3600.0 * orbitalPeriod / ( 2.0 * std::fabs( orbitalPeriod - 9.925 * 3600.0 ) );
+    //    double jupiterQualityFactor = 1.0 / std::sin( 2.0 * mathematical_constants::PI * jupiterTimeLag / ( jupiterForcingTime ) );
 
-    double ioQualityFactor = 1.0 / std::sin( 2.0 * mathematical_constants::PI * ( ioTimeLag / 2.0 ) / orbitalPeriod );
+    double ioQualityFactor = 1.0 / std::sin( 2.0 * mathematical_constants::PI * ioTimeLag / orbitalPeriod );
 
     std::vector< double > semiMajorAxisFitIoTide = linear_algebra::getLeastSquaresPolynomialFit(
                 semiMajorAxesIoTide, boost::assign::list_of( 0 )( 1 ) );
     std::vector< double > eccentricityFitIoTide = linear_algebra::getLeastSquaresPolynomialFit(
                 eccentricitiesIoTide, boost::assign::list_of( 0 )( 1 ) );
+
+    tudat::input_output::writeMatrixToFile( tudat::utilities::convertStlVectorToEigenVector(
+                                                tudat::utilities::createVectorFromMapValues( semiMajorAxesIoTide ) ), "ioSemiMajorAxis.dat" );
+    tudat::input_output::writeMatrixToFile( tudat::utilities::convertStlVectorToEigenVector(
+                                                tudat::utilities::createVectorFromMapValues( eccentricitiesIoTide ) ), "ioEccentricity.dat" );
+    tudat::input_output::writeMatrixToFile( tudat::utilities::convertStlVectorToEigenVector(
+                                                tudat::utilities::createVectorFromMapKeys( semiMajorAxesIoTide ) ), "ioTimes.dat" );
+
+    std::cout<<"Io values: "<<ioLoveNumber<<" "<< ioLoveNumber / ioQualityFactor<<std::endl;
 
     double theoreticalSemiMajorAxisRateFromIoTide =  -21.0 * ioLoveNumber / ioQualityFactor * getBodyGravitationalParameter( "Jupiter" ) /
             getBodyGravitationalParameter( "Io" ) * std::pow( getAverageRadius( "Io" ) / intialKeplerElements( 0 ), 5.0 ) *
@@ -231,6 +241,9 @@ BOOST_AUTO_TEST_CASE( testMutualSphericalHarmonicGravity )
             getBodyGravitationalParameter( "Io" ) * std::pow( getAverageRadius( "Io" ) / intialKeplerElements( 0 ), 5.0 ) *
             intialKeplerElements( 1 ) * meanMotion;
 
+
+    BOOST_CHECK_CLOSE_FRACTION( semiMajorAxisFitIoTide.at( 1 ), theoreticalSemiMajorAxisRateFromIoTide, 1.0E-3 );
+    BOOST_CHECK_CLOSE_FRACTION( eccentricityFitIoTide.at( 1 ), theoreticaEccentricityRateFromIoTide, 1.0E-3 );
 
     std::cout<<"THEORETICAL VALUE "<<theoreticalSemiMajorAxisRateFromIoTide<<" "<<theoreticaEccentricityRateFromIoTide<<" "<<std::endl<<
                ioQualityFactor<<" "<<getAverageRadius( "Jupiter" ) / intialKeplerElements( 0 )<<" "<<getBodyGravitationalParameter( "Io" ) /
