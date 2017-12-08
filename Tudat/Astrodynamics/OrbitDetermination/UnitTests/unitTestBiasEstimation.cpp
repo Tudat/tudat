9bb33ba5fd31ef81b5e6674c1d4ebfdc412ed4bb
Dominic Dirkx <d.dirkx@tudelft.nl>
2017-12-08 13:59:34 +0100

    Bias estimation updated and tested

---
+++ Tudat/Astrodynamics/OrbitDetermination/UnitTests/unitTestBiasEstimation.cpp
@@ -0,0 +1,85 @@
+/*    Copyright (c) 2010-2017, Delft University of Technology
+ *    All rigths reserved
+ *
+ *    This file is part of the Tudat. Redistribution and use in source and
+ *    binary forms, with or without modification, are permitted exclusively
+ *    under the terms of the Modified BSD license. You should have received
+ *    a copy of the license with this file. If not, please or visit:
+ *    http://tudat.tudelft.nl/LICENSE.
+ */
+
+#define BOOST_TEST_MAIN
+
+
+#include <limits>
+
+#include <boost/test/unit_test.hpp>
+
+#include "Tudat/Astrodynamics/OrbitDetermination/UnitTests/orbitDeterminationTestCases.h"
+
+
+namespace tudat
+{
+namespace unit_tests
+{
+BOOST_AUTO_TEST_SUITE( test_estimation_from_positions )
+
+
+
+//! This test checks, for double states/observables and double time, if the orbit determination correctly converges
+//! when simulating data, perturbing the dynamical parameters, and then retrieving the original parameters
+BOOST_AUTO_TEST_CASE( test_EstimationFromPosition )
+{
+
+    for( int estimateRangeBiases = 0; estimateRangeBiases < 2; estimateRangeBiases++ )
+    {
+        for( int estimateTwoWayBiases = 0; estimateTwoWayBiases < 2; estimateTwoWayBiases++ )
+        {
+            for( int useSingleBiasModel = 0; useSingleBiasModel < 2; useSingleBiasModel++ )
+            {
+                for( int estimateAbsoluteBiases = 0; estimateAbsoluteBiases < 2; estimateAbsoluteBiases++ )
+                {
+                    std::cout << "=========== Running Case: " << estimateRangeBiases <<" "<<estimateTwoWayBiases<<" "<<useSingleBiasModel<<" "<<estimateAbsoluteBiases<<std::endl;
+
+                    // Simulate estimated parameter error.
+                    Eigen::VectorXd totalError = executeEarthOrbiterBiasEstimation< double, double >(
+                                estimateRangeBiases, estimateTwoWayBiases, useSingleBiasModel, estimateAbsoluteBiases );
+
+                    for( unsigned int j = 0; j < 3; j++ )
+                    {
+                        BOOST_CHECK_SMALL( std::fabs( totalError( j ) ), 1.0E-6 );
+                        BOOST_CHECK_SMALL( std::fabs( totalError( j + 3 ) ), 1.0E-9 );
+                    }
+
+                    for( unsigned int j = 6; j < totalError.rows( ); j++ )
+                    {
+                        if( estimateAbsoluteBiases )
+                        {
+                            if( estimateRangeBiases )
+                            {
+                                BOOST_CHECK_SMALL( std::fabs( totalError( j ) ), 1.0E-7);
+                            }
+                            else
+                            {
+                                BOOST_CHECK_SMALL( std::fabs( totalError( j ) ), 1.0E-18 );
+                            }
+                        }
+                        else
+                        {
+                            BOOST_CHECK_SMALL( std::fabs( totalError( j ) ), 1.0E-14 );
+                        }
+                    }
+
+                }
+            }
+        }
+    }
+}
+
+BOOST_AUTO_TEST_SUITE_END( )
+
+}
+
+}
+
+
