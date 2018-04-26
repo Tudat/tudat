d8d1dc6c609cd4d27c1d0672d597011300bfad9a
mfacchinelli <michele.facchinelli@yahoo.it>
2018-04-26 15:03:05 +0200

    Latest changes in tabulated atmosphere: Created yet another function for (N-dimensional) interpolation, changes input file type to map, various other corrections

--- Tudat/SimulationSetup/EnvironmentSetup/defaultBodies.cpp
+++ Tudat/SimulationSetup/EnvironmentSetup/defaultBodies.cpp
@@ -31,8 +31,8 @@ boost::shared_ptr< AtmosphereSettings > getDefaultAtmosphereModelSettings(
     // A default atmosphere is only implemented for Earth.
     if( bodyName == "Earth" )
     {
-        std::vector< std::string > tabulatedAtmosphereFiles = { input_output::getAtmosphereTablesPath( ) +
-                                                                "USSA1976Until100kmPer100mUntil1000kmPer1000m.dat" };
+        std::map< int, std::string > tabulatedAtmosphereFiles = { { 0, input_output::getAtmosphereTablesPath( ) +
+                                                                    "USSA1976Until100kmPer100mUntil1000kmPer1000m.dat" } };
         atmosphereSettings = boost::make_shared< TabulatedAtmosphereSettings >( tabulatedAtmosphereFiles );
     }
 
