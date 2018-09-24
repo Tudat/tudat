03a17336ebdbd53eb5160f558dc679a5ebaffd84
Dominic Dirkx <d.dirkx@tudelft.nl>
2018-09-24 12:34:39 +0200

    Added some CMake arguments to allow compilation of subsets of functionality. Definition of extern template classes still need to be put inside #if statements.

--- Tudat/SimulationSetup/PropagationSetup/propagationSettings.cpp
+++ Tudat/SimulationSetup/PropagationSetup/propagationSettings.cpp
@@ -7,30 +7,26 @@ namespace propagators
 {
 
 template class PropagatorSettings< double >;
-template class PropagatorSettings< long double >;
-
 template class SingleArcPropagatorSettings< double >;
-template class SingleArcPropagatorSettings< long double >;
-
 template class MultiArcPropagatorSettings< double >;
-template class MultiArcPropagatorSettings< long double >;
-
 template class TranslationalStatePropagatorSettings< double >;
-template class TranslationalStatePropagatorSettings< long double >;
-
 template class RotationalStatePropagatorSettings< double >;
-template class RotationalStatePropagatorSettings< long double >;
-
 template class MassPropagatorSettings< double >;
-template class MassPropagatorSettings< long double >;
-
 template class CustomStatePropagatorSettings< double >;
+template class MultiTypePropagatorSettings< double >;
+
+#if( BUILD_EXTENDED_PRECISION_PROPAGATION_TOOLS )
+template class PropagatorSettings< long double >;
+template class SingleArcPropagatorSettings< long double >;
+template class MultiArcPropagatorSettings< long double >;
+template class TranslationalStatePropagatorSettings< long double >;
+template class RotationalStatePropagatorSettings< long double >;
+template class MassPropagatorSettings< long double >;
+template class MultiTypePropagatorSettings< long double >;
 template class CustomStatePropagatorSettings< long double >;
 template class CustomStatePropagatorSettings< double, Time >;
 template class CustomStatePropagatorSettings< long double, Time >;
-
-template class MultiTypePropagatorSettings< double >;
-template class MultiTypePropagatorSettings< long double >;
+#endif
 
 template std::map< IntegratedStateType, std::vector< std::pair< std::string, std::string > > > getIntegratedTypeAndBodyList< double >(
         const std::shared_ptr< SingleArcPropagatorSettings< double > > propagatorSettings );
