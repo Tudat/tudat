1d0384b1fbde9fbf799c06e85c19bda236db1557
Dominic Dirkx <d.dirkx@tudelft.nl>
2019-01-18 15:10:03 +0100

    Small modifications for links between approximate and full numerical propagation

---
+++ Tudat/SimulationSetup/PropagationSetup/propagationPatchedConicFullProblem.h
@@ -0,0 +1,42 @@
+/*    Copyright (c) 2010-2017, Delft University of Technology
+ *    All rigths reserved
+ *
+ *    This file is part of the Tudat. Redistribution and use in source and
+ *    binary forms, with or without modification, are permitted exclusively
+ *    under the terms of the Modified BSD license. You should have received
+ *    a copy of the license with this file. If not, please or visit:
+ *    http://tudat.tudelft.nl/LICENSE.
+ */
+
+#ifndef TUDAT_PROPAGATION_PATCHED_CONIC_FULL
+#define TUDAT_PROPAGATION_PATCHED_CONIC_FULL
+
+#include "Tudat/SimulationSetup/tudatSimulationHeader.h"
+#include "Tudat/Astrodynamics/TrajectoryDesign/trajectory.h"
+
+namespace tudat
+{
+
+namespace propagators
+{
+
+transfer_trajectories::Trajectory createTransferTrajectoryObject(
+        const simulation_setup::NamedBodyMap& bodyMap,
+        const std::vector< std::string >& transferBodyOrder,
+        const std::string& centralBody,
+        const std::vector< transfer_trajectories::TransferLegType >& transferLegTypes,
+        const std::vector< double > trajectoryIndependentVariables,
+        const std::vector< double > minimumPericenterRadii,
+        const bool includeDepartureDeltaV,
+        const double departureSemiMajorAxis,
+        const double departureEccentricity,
+        const bool includeArrivalDeltaV,
+        const double arrivalSemiMajorAxis,
+        const double arrivalEccentricity );
+
+}
+
+}
+
+
+#endif // TUDAT_PROPAGATION_PATCHED_CONIC_FULL
