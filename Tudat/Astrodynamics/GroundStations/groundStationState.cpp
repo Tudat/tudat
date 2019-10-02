2beb44f120322f3b0b50d3144ba8b2d426a2b62b
Dominic Dirkx <d.dirkx@tudelft.nl>
2019-10-02 14:34:45 +0200

    Update to license statement

--- Tudat/Astrodynamics/GroundStations/groundStationState.cpp
+++ Tudat/Astrodynamics/GroundStations/groundStationState.cpp
@@ -1,7 +1,7 @@
 #include "Tudat/Mathematics/BasicMathematics/coordinateConversions.h"
 #include "Tudat/Mathematics/BasicMathematics/mathematicalConstants.h"
 
-/*    Copyright (c) 2010-2018, Delft University of Technology
+/*    Copyright (c) 2010-2019, Delft University of Technology
  *    All rigths reserved
  *
  *    This file is part of the Tudat. Redistribution and use in source and
--- Tudat/Mathematics/BasicMathematics/UnitTests/unitTestRotationPartials.cpp
+++ Tudat/Mathematics/BasicMathematics/UnitTests/unitTestRotationPartials.cpp
@@ -1,18 +1,11 @@
-/*    Copyright (c) 2010-2012 Delft University of Technology.
+/*    Copyright (c) 2010-2019, Delft University of Technology
+ *    All rigths reserved
  *
- *    This software is protected by national and international copyright.
- *    Any unauthorized use, reproduction or modification is unlawful and
- *    will be prosecuted. Commercial and non-private application of the
- *    software in any form is strictly prohibited unless otherwise granted
- *    by the authors.
- *
- *    The code is provided without any warranty; without even the implied
- *    warranty of merchantibility or fitness for a particular purpose.
- *
- *    Changelog
- *      YYMMDD    Author            Comment
- *
- *    References
+ *    This file is part of the Tudat. Redistribution and use in source and
+ *    binary forms, with or without modification, are permitted exclusively
+ *    under the terms of the Modified BSD license. You should have received
+ *    a copy of the license with this file. If not, please or visit:
+ *    http://tudat.tudelft.nl/LICENSE.
  *
  */
 
