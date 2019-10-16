3a003849b66251923fd9a96decb842f470a0f544
mchambe <m.s.chambe@student.tudelft.nl>
2019-10-16 23:17:08 +0200

    Reorganised low-thrust trajectory module (+ some minor updates).

---
+++ Tudat/Astrodynamics/LowThrustTrajectories/ShapeBasedMethods/createBaseFunctionHodographicShaping.h
@@ -0,0 +1,167 @@
+/*    Copyright (c) 2010-2018, Delft University of Technology
+ *    All rigths reserved
+ *
+ *    This file is part of the Tudat. Redistribution and use in source and
+ *    binary forms, with or without modification, are permitted exclusively
+ *    under the terms of the Modified BSD license. You should have received
+ *    a copy of the license with this file. If not, please or visit:
+ *    http://tudat.tudelft.nl/LICENSE.
+ *
+ */
+
+#ifndef TUDAT_CREATE_BASE_FUNCTION_HODOGRAPHIC_SHAPING_H
+#define TUDAT_CREATE_BASE_FUNCTION_HODOGRAPHIC_SHAPING_H
+
+#include <math.h>
+#include <boost/make_shared.hpp>
+#include "Tudat/Astrodynamics/LowThrustTrajectories/ShapeBasedMethods/baseFunctionsHodographicShaping.h"
+
+namespace tudat
+{
+namespace shape_based_methods
+{
+
+//! Settings class for hodographic shaping base functions.
+class BaseFunctionHodographicShapingSettings
+{
+public:
+
+    //! Default, empty constructor.
+    BaseFunctionHodographicShapingSettings( ){ }
+
+    //! Destructor.
+    virtual ~BaseFunctionHodographicShapingSettings( ){ }
+
+};
+
+
+//! Settings class for hodographic shaping trigonometric function (either sine or cosine).
+class TrigonometricFunctionHodographicShapingSettings: public BaseFunctionHodographicShapingSettings
+{
+public:
+
+    //! Constructor to set frequency for a trigonometric function (either sine or cosine).
+    /*!
+     *  Constructor to set frequency for a trigonometric function (either sine or cosine).
+     *  \param frequency Frequency of the trigonometric function.
+     */
+    TrigonometricFunctionHodographicShapingSettings( const double frequency ) :
+        frequency_( frequency ){ }
+
+    //! Frequency of the trigonometric function.
+    double frequency_;
+};
+
+
+//! Settings class for hodographic shaping exponential function.
+class ExponentialFunctionHodographicShapingSettings: public BaseFunctionHodographicShapingSettings
+{
+public:
+
+    //! Constructor to set exponent, and possibly scaling factor for the exponential function.
+    /*!
+     *  Constructor to set exponent, and possibly scaling factor for the exponential function.
+     *  \param exponent Exponent of the exponential function.
+     *  \param scaleFactor Scaling factor for the exponential function.
+     */
+    ExponentialFunctionHodographicShapingSettings( const double exponent,
+                                                   const double scaleFactor = 1.0 ) :
+        exponent_( exponent ), scaleFactor_( scaleFactor ){ }
+
+    //! Exponent of the exponential function.
+    double exponent_;
+
+    //! Scale factor, to be used for a scaled exponential function.
+    double scaleFactor_;
+};
+
+
+//! Settings class for hodographic shaping exponential times trigonometric function.
+class ExponentialTimesTrigonometricFunctionHodographicShapingSettings: public BaseFunctionHodographicShapingSettings
+{
+public:
+
+    //! Constructor to set exponent, frequency, and possibly scaling factor for the exponential times sine or cosine function.
+    /*!
+     *  Constructor to set exponent, frequency, and possibly scaling factor for the exponential times sine or cosine function.
+     *  \param exponent Exponent of the exponential function.
+     *  \param frequency Frequency of the trigonometric function.
+     *  \param scaleFactor Scaling factor for the exponential times trigonometric function.
+     */
+    ExponentialTimesTrigonometricFunctionHodographicShapingSettings( const double exponent,
+                                                                     const double frequency,
+                                                                     const double scaleFactor = 1.0 ) :
+        exponent_( exponent ), frequency_( frequency ), scaleFactor_( scaleFactor ){ }
+
+    //! Exponent of the exponential function.
+    double exponent_;
+
+    //! Frequency of the trigonometric function.
+    double frequency_;
+
+    //! Scale factor, to be used for a scaled exponential times trigonometric function.
+    double scaleFactor_;
+};
+
+
+//! Settings class for hodographic shaping power function.
+class PowerFunctionHodographicShapingSettings: public BaseFunctionHodographicShapingSettings
+{
+public:
+
+    //! Constructor to set exponent, and possibly scaling factor for the power function.
+    /*!
+     *  Constructor to set exponent, and possibly scaling factor for the power function.
+     *  \param exponent Exponent of the power function.
+     *  \param scaleFactor Scaling factor for the power function.
+     */
+    PowerFunctionHodographicShapingSettings( const double exponent,
+                                             const double scaleFactor = 1.0 ) :
+        exponent_( exponent ), scaleFactor_( scaleFactor ){ }
+
+    //! Exponent of the power function.
+    double exponent_;
+
+    //! Scale factor, to be used for a scaled power function.
+    double scaleFactor_;
+};
+
+
+//! Settings class for hodographic shaping power times trigonometric function.
+class PowerTimesTrigonometricFunctionHodographicShapingSettings: public BaseFunctionHodographicShapingSettings
+{
+public:
+
+    //! Constructor to set exponent, frequency, and possibly scaling factor for the power times sine or cosine function.
+    /*!
+     *  Constructor to set exponent, frequency, and possibly scaling factor for the power times sine or cosine function.
+     *  \param exponent Exponent of the power function.
+     *  \param frequency Frequency of the trigonometric function.
+     *  \param scaleFactor Scaling factor for the power times trigonometric function.
+     */
+    PowerTimesTrigonometricFunctionHodographicShapingSettings( const double exponent,
+                                                               const double frequency,
+                                                               const double scaleFactor = 1.0 ) :
+        exponent_( exponent ), frequency_( frequency ), scaleFactor_( scaleFactor ){ }
+
+    //! Exponent of the power function.
+    double exponent_;
+
+    //! Frequency of the trigonometric function.
+    double frequency_;
+
+    //! Scale factor, to be used for a scaled power times trigonometric function.
+    double scaleFactor_;
+};
+
+
+//! Function to create a base function for hodographic shaping.
+std::shared_ptr< shape_based_methods::BaseFunctionHodographicShaping > createBaseFunctionHodographicShaping(
+        const shape_based_methods::baseFunctionHodographicShapingType baseFunctionType,
+        std::shared_ptr< shape_based_methods::BaseFunctionHodographicShapingSettings > baseFunctionSettings );
+
+
+} // namespace shape_based_methods
+} // namespace tudat
+
+#endif // TUDAT_CREATE_BASE_FUNCTION_HODOGRAPHIC_SHAPING_H
---
+++ Tudat/Astrodynamics/LowThrustTrajectories/simsFlanagan.h
@@ -0,0 +1,254 @@
+/*    Copyright (c) 2010-2018, Delft University of Technology
+ *    All rigths reserved
+ *
+ *    This file is part of the Tudat. Redistribution and use in source and
+ *    binary forms, with or without modification, are permitted exclusively
+ *    under the terms of the Modified BSD license. You should have received
+ *    a copy of the license with this file. If not, please or visit:
+ *    http://tudat.tudelft.nl/LICENSE.
+ *
+ */
+
+#ifndef TUDAT_SIMS_FLANAGAN_H
+#define TUDAT_SIMS_FLANAGAN_H
+
+#include <Tudat/SimulationSetup/tudatSimulationHeader.h>
+#include <math.h>
+#include <vector>
+#include <Eigen/Dense>
+#include <map>
+#include "Tudat/Astrodynamics/LowThrustTrajectories/simsFlanaganLeg.h"
+#include "Tudat/SimulationSetup/optimisationSettings.h" //LowThrustDirectMethods/optimisationSettings.h"
+#include "Tudat/Astrodynamics/LowThrustTrajectories/ShapeBasedMethods/shapeBasedMethodLeg.h"
+
+namespace tudat
+{
+namespace low_thrust_trajectories
+{
+
+
+//! Transform thrust model as a function of time into Sims Flanagan thrust model.
+std::vector< Eigen::Vector3d > convertToSimsFlanaganThrustModel( std::function< Eigen::Vector3d( const double ) > thrustModelWrtTime,
+                                                                 const double maximumThrust,
+                                                                 const double timeOfFlight, const int numberSegmentsForwardPropagation,
+                                                                 const int numberSegmentsBackwardPropagation );
+
+//! Convert a shape-based thrust profile into a possible initial guess for Sims-Flanagan.
+std::function< Eigen::Vector3d( const double ) > getInitialGuessFunctionFromShaping(
+        std::shared_ptr< shape_based_methods::ShapeBasedMethodLeg > shapeBasedLeg,
+        const int numberSegmentsSimsFlanagan,
+        const double timeOfFlight,
+        std::function< double( const double ) > specificImpulseFunction,
+        std::shared_ptr< numerical_integrators::IntegratorSettings< double > > integratorSettings );
+
+
+
+class SimsFlanagan : public low_thrust_trajectories::LowThrustLeg
+{
+public:
+
+    //! Constructor.
+    SimsFlanagan(
+            const Eigen::Vector6d& stateAtDeparture,
+            const Eigen::Vector6d& stateAtArrival,
+            const double maximumThrust,
+            const std::function< double ( const double ) > specificImpulseFunction,
+            const int numberSegments,
+            const double timeOfFlight,
+            simulation_setup::NamedBodyMap bodyMap,
+            const std::string bodyToPropagate,
+            const std::string centralBody,
+            std::shared_ptr< simulation_setup::OptimisationSettings > optimisationSettings ) :
+        LowThrustLeg( stateAtDeparture, stateAtArrival, timeOfFlight, bodyMap, bodyToPropagate, centralBody ),
+        maximumThrust_( maximumThrust ),
+        specificImpulseFunction_( specificImpulseFunction ),
+        numberSegments_( numberSegments ),
+        optimisationSettings_( optimisationSettings )
+    {
+
+        // Store initial spacecraft mass.
+        initialSpacecraftMass_ = bodyMap_[ bodyToPropagate_ ]->getBodyMass();
+
+        // Calculate number of segments for both the forward propagation (from departure to match point)
+        // and the backward propagation (from arrival to match point).
+        numberSegmentsForwardPropagation_ = ( numberSegments_ + 1 ) / 2;
+        numberSegmentsBackwardPropagation_ = numberSegments_ / 2;
+
+        // Convert the thrust model proposed as initial guess into simplified thrust model adapted to the Sims-Flanagan method.
+        if ( optimisationSettings_->initialGuessThrustModel_.first != nullptr )
+        {
+            initialGuessThrustModel_.first = convertToSimsFlanaganThrustModel( optimisationSettings_->initialGuessThrustModel_.first ,
+                                                                               maximumThrust_, timeOfFlight_, numberSegmentsForwardPropagation_,
+                                                                               numberSegmentsBackwardPropagation_ );
+        }
+        else
+        {
+            initialGuessThrustModel_.first = std::vector< Eigen::Vector3d >( );
+        }
+        initialGuessThrustModel_.second = optimisationSettings_->initialGuessThrustModel_.second;
+
+        // Perform optimisation
+        std::pair< std::vector< double >, std::vector< double > > bestIndividual = performOptimisation( );
+        championFitness_ = bestIndividual.first;
+        championDesignVariables_ = bestIndividual.second;
+
+        // Transform best design variables into vector of throttles.
+        std::vector< Eigen::Vector3d > throttles;
+        for ( int i = 0 ; i < numberSegments ; i++ )
+        {
+            throttles.push_back( ( Eigen::Vector3d( ) << championDesignVariables_[ i * 3 ], championDesignVariables_[ i * 3 + 1 ],
+                    championDesignVariables_[ i * 3 + 2 ] ).finished( ) );
+        }
+
+        bodyMap_[ bodyToPropagate_ ]->setConstantBodyMass( initialSpacecraftMass_ );
+
+        // Create Sims-Flanagan leg from the best optimisation individual.
+        simsFlanaganLeg_ = std::make_shared< SimsFlanaganLeg >( stateAtDeparture_, stateAtArrival_, maximumThrust_, specificImpulseFunction_,
+                                            timeOfFlight_, bodyMap_, throttles, bodyToPropagate_, centralBody_ );
+
+    }
+
+    //! Default destructor.
+    ~SimsFlanagan( ) { }
+
+    //! Convert time to independent variable.
+    double convertTimeToIndependentVariable( const double time )
+    {
+        return time;
+    }
+
+    //! Convert independent variable to time.
+    double convertIndependentVariableToTime( const double independentVariable )
+    {
+        return independentVariable;
+    }
+
+    //! Perform optimisation.
+    std::pair< std::vector< double >, std::vector< double > > performOptimisation( );
+
+    //! Compute DeltaV.
+    double computeDeltaV( )
+    {
+        return simsFlanaganLeg_->getTotalDeltaV( );
+    }
+
+    //! Compute current cartesian state.
+    Eigen::Vector6d computeCurrentStateVector( const double currentTime );
+
+    //! Compute state history.
+    void getTrajectory(
+            std::vector< double >& epochsVector,
+            std::map< double, Eigen::Vector6d >& propagatedTrajectory )
+    {
+        simsFlanaganLeg_->propagateTrajectory( epochsVector, propagatedTrajectory );
+    }
+
+    Eigen::Vector3d computeCurrentThrust( double time,
+                                          std::function< double ( const double ) > specificImpulseFunction,
+                                          std::shared_ptr<numerical_integrators::IntegratorSettings< double > > integratorSettings );
+
+    //! Compute direction thrust acceleration in cartesian coordinates.
+    Eigen::Vector3d computeCurrentThrustAccelerationDirection(
+            double currentTime, std::function< double ( const double ) > specificImpulseFunction,
+            std::shared_ptr<numerical_integrators::IntegratorSettings< double > > integratorSettings );
+
+    //! Compute magnitude thrust acceleration.
+    double computeCurrentThrustAccelerationMagnitude(
+            double currentTime, std::function< double ( const double ) > specificImpulseFunction,
+            std::shared_ptr<numerical_integrators::IntegratorSettings< double > > integratorSettings );
+
+    //! Return thrust acceleration profile.
+    void getThrustAccelerationProfile(
+            std::vector< double >& epochsVector,
+            std::map< double, Eigen::VectorXd >& thrustAccelerationProfile,
+            std::function< double ( const double ) > specificImpulseFunction,
+            std::shared_ptr<numerical_integrators::IntegratorSettings< double > > integratorSettings );
+
+    //! Return best individual.
+    std::vector< double > getBestIndividual( )
+    {
+        return championDesignVariables_;
+    }
+
+    //! Return fitness of best individual.
+    std::vector< double > getBestIndividualFitness( )
+    {
+        return championFitness_;
+    }
+
+
+    //! Return best Sims-Flanagan leg after optimisation.
+    std::shared_ptr< SimsFlanaganLeg > getOptimalSimsFlanaganLeg( )
+    {
+        return simsFlanaganLeg_;
+    }
+
+
+    //! Retrieve acceleration map (thrust and central gravity accelerations).
+    basic_astrodynamics::AccelerationMap retrieveLowThrustAccelerationMap(
+            std::function< double ( const double ) > specificImpulseFunction,
+            std::shared_ptr< numerical_integrators::IntegratorSettings< double > > integratorSettings );
+
+
+    //! Define appropriate translational state propagator settings for the full propagation.
+    std::pair< std::shared_ptr< propagators::TranslationalStatePropagatorSettings< double > >,
+    std::shared_ptr< propagators::TranslationalStatePropagatorSettings< double > > > createLowThrustTranslationalStatePropagatorSettings(
+            basic_astrodynamics::AccelerationMap accelerationModelMap,
+            std::shared_ptr< propagators::DependentVariableSaveSettings > dependentVariablesToSave );
+
+
+
+protected:
+
+    int convertTimeToLegSegment( double currentTime )
+    {
+        return simsFlanaganLeg_->convertTimeToLegSegment( currentTime );
+    }
+
+private:
+
+    //! Maximum allowed thrust.
+    double maximumThrust_;
+
+    //! Specific impulse function.
+    std::function< double ( const double ) > specificImpulseFunction_;
+
+    //! Number of segments into which the leg is subdivided.
+    int numberSegments_;
+
+    //! Initial guess for the optimisation.
+    //! The first element contains the thrust throttles corresponding to the initial guess for the thrust model.
+    //! The second element defines the bounds around the initial time (in percentage).
+    std::pair< std::vector< Eigen::Vector3d >, double > initialGuessThrustModel_;
+
+    //! Optimisation settings.
+    std::shared_ptr< simulation_setup::OptimisationSettings > optimisationSettings_;
+
+    //! Fitness vector of the optimisation best individual.
+    std::vector< double > championFitness_;
+
+    //! Design variables vector corresponding to the optimisation best individual.
+    std::vector< double > championDesignVariables_;
+
+    //! Initial mass of the spacecraft.
+    double initialSpacecraftMass_;
+
+    //! Number of segments for the forward propagation from departure to match point.
+    int numberSegmentsForwardPropagation_;
+
+    //! Number of segments for the backward propagation from arrival to match point.
+    int numberSegmentsBackwardPropagation_;
+
+    //! DeltaV corresponding to best trajectory.
+    double deltaV_;
+
+    //! Sims-Flanagan leg corresponding to the best optimisation output.
+    std::shared_ptr< SimsFlanaganLeg > simsFlanaganLeg_;
+
+};
+
+
+} // namespace low_thrust_trajectories
+} // namespace tudat
+
+#endif // TUDAT_SIMS_FLANAGAN_H
