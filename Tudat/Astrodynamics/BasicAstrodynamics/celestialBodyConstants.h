33215e6fd90a0dd1fcec1ae6d7bf4a8982bd58ae
Dominic Dirkx <d.dirkx@tudelft.nl>
2020-02-10 11:29:57 +0100

    Removed maps giving compiler errors

--- Tudat/Astrodynamics/BasicAstrodynamics/celestialBodyConstants.h
+++ Tudat/Astrodynamics/BasicAstrodynamics/celestialBodyConstants.h
@@ -124,33 +124,33 @@ const static double NEPTUNE_GRAVITATIONAL_PARAMETER = SUN_GRAVITATIONAL_PARAMETE
  */
 const static double PLUTO_GRAVITATIONAL_PARAMETER = SUN_GRAVITATIONAL_PARAMETER / 1.35e8;
 
-static std::map< int, std::string > planetNames =
-{
-    { 0, "Sun" },
-    { 1, "Mercury" },
-    { 2, "Venus" },
-    { 3, "Earth" },
-    { 4, "Mars" },
-    { 5, "Jupiter" },
-    { 6, "Saturn" },
-    { 7, "Uranus" },
-    { 8, "Neptune" },
-    { 9, "Pluto" }
-};
+//static std::map< int, std::string > planetNames =
+//{
+//    { 0, "Sun" },
+//    { 1, "Mercury" },
+//    { 2, "Venus" },
+//    { 3, "Earth" },
+//    { 4, "Mars" },
+//    { 5, "Jupiter" },
+//    { 6, "Saturn" },
+//    { 7, "Uranus" },
+//    { 8, "Neptune" },
+//    { 9, "Pluto" }
+//};
 
-static std::map< std::string, int > planetIdNumbers =
-{
-    { "Sun", 0 },
-    { "Mercury", 1 },
-    { "Venus", 2 },
-    { "Earth", 3 },
-    { "Mars", 4 },
-    { "Jupiter", 5 },
-    { "Saturn", 6 },
-    { "Uranus", 7 },
-    { "Neptune", 8 },
-    { "Pluto", 9 }
-};
+//static std::map< std::string, int > planetIdNumbers =
+//{
+//    { "Sun", 0 },
+//    { "Mercury", 1 },
+//    { "Venus", 2 },
+//    { "Earth", 3 },
+//    { "Mars", 4 },
+//    { "Jupiter", 5 },
+//    { "Saturn", 6 },
+//    { "Uranus", 7 },
+//    { "Neptune", 8 },
+//    { "Pluto", 9 }
+//};
 
 } // namespace celestial_body_constants
 
