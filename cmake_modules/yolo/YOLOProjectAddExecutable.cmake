f09a07200b99258f615787b85313e8bc9d8aba26
Geoffrey Garrett <g.h.garrett13@gmail.com>
2020-06-02 21:19:01 +0200

    feat(tudat_bundle) Achievement unlocked: Ballmer Peak

--- cmake_modules/yolo/YOLOProjectAddExecutable.cmake
+++ cmake_modules/yolo/YOLOProjectAddExecutable.cmake
@@ -18,7 +18,7 @@ function(TUDAT_ADD_EXECUTABLE arg1 arg2)
             )
 
     target_include_directories("${target_name}"
-            SYSTEM PRIVATE "${EIGEN3_INCLUDE_DIRS}" "${Boost_INCLUDE_DIRS}"
+            SYSTEM PRIVATE "${EIGEN3_INCLUDE_DIRS}" "${Boost_INCLUDE_DIRS}" "${CSpice_INCLUDE_DIRS}" "${Sofa_INCLUDE_DIRS}"
             )
 
     target_link_libraries("${target_name}"
