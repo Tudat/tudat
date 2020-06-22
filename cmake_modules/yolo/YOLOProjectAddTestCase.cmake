45785c4de0b6eeb39227adbc9b781139d20dbda8
Geoffrey Garrett <g.h.garrett13@gmail.com>
2020-06-22 19:36:58 +0200

    Modified resource management

--- cmake_modules/yolo/YOLOProjectAddTestCase.cmake
+++ cmake_modules/yolo/YOLOProjectAddTestCase.cmake
@@ -1,48 +1,76 @@
 include(CMakeParseArguments)
 
 set(TESTS_REQUIRING_TEST_DATA
+        # The mysterious failures
+        test_basic_astro_EmpiricalAcceleration
+        test_earth_orientation_EarthOrientationCalculator
+        test_earth_orientation_EopReader
+        test_earth_orientation_PolarMotionCalculator
+        test_earth_orientation_TimeScaleConverter
+        test_earth_orientation_ShortPeriodEopCorrections
+        test_electromagnetism_PanelledRadiationPressure
+
+        # SPICE(FILENAMETOOLONG) --
+        #
+        #  Input file name </home/ggarrett/miniconda3/conda-bld/tudat_1592845536964/_h_env
+        #  _placehold_placehold_placehold_placehold_placehold_placehold_placehold_placehold
+        #  _placehold_placehold_placehold_placehold_placehold_placehold_placehold_placehold
+        #  _placehold_placehold_placehold_pl/resource/spice_kernels/pck00010.tpc> has
+        #  length 291 characters. The limit on the length of file names stored by FURNSH
+        #  is 255 characters.
+        #
+        #  A traceback follows.  The name of the highest level module is first.
+        #  furnsh_c --> FURNSH
+        test_aerodynamics_AerodynamicMomentAndAerodynamicForce
+        test_aerodynamics_ControlSurfaceIncrements
+        test_aerodynamics_AerodynamicCoefficientsFromFile
+        test_aerodynamics_WindModel
+        test_spice_SpiceInterface
+        test_simulation_EnvironmentModelSetup
+        test_simulation_AccelerationModelSetup
+
         # Fails remotely
-#        test_aerodynamics_AerodynamicMomentAndAerodynamicForce
-#        test_aerodynamics_TabulatedAtmosphere
-#        test_aerodynamics_ControlSurfaceIncrements
-#        test_aerodynamics_WindModel
-#        test_ephemerides_ApproximatePlanetPositions
-#        test_ephemerides_TabulatedEphemeris
-#        test_spice_SpiceInterface
-#        test_simulation_EnvironmentModelSetup
-#        test_simulation_AccelerationModelSetup
-#        test_io_BasicInputOutput
-#        # Fails locally
-#        test_aerodynamics_AerodynamicCoefficientsFromFile
-#        test_basic_astro_EmpiricalAcceleration
-#        test_earth_orientation_EarthOrientationCalculator
-#        test_earth_orientation_EopReader
-#        test_earth_orientation_PolarMotionCalculator
-#        test_earth_orientation_TimeScaleConverter
-#        test_earth_orientation_ShortPeriodEopCorrections
-#        test_electromagnetism_PanelledRadiationPressure
-#        test_interpolators_CubicSplineInterpolator
-#        test_interpolators_LinearInterpolator
-#        test_interpolators_MultiLinearInterpolator
-#        test_integrators_EulerIntegrator
-#        test_integrators_RungeKutta4Integrator
-#        test_integrators_RungeKuttaFehlberg45Integrator
-#        test_integrators_RungeKuttaFehlberg78Integrator
-#        test_integrators_RungeKutta87DormandPrinceIntegrator
-#        test_quadrature_GaussianQuadrature
-#        test_io_MapTextFileReader
-#        test_io_MatrixTextFileReader
+        #        test_aerodynamics_AerodynamicMomentAndAerodynamicForce
+        #        test_aerodynamics_TabulatedAtmosphere
+        #        test_aerodynamics_ControlSurfaceIncrements
+        #        test_aerodynamics_WindModel
+        #        test_ephemerides_ApproximatePlanetPositions
+        #        test_ephemerides_TabulatedEphemeris
+        #        test_spice_SpiceInterface
+        #        test_simulation_EnvironmentModelSetup
+        #        test_simulation_AccelerationModelSetup
+        #        test_io_BasicInputOutput
+        #        # Fails locally
+        #        test_aerodynamics_AerodynamicCoefficientsFromFile
+        #        test_basic_astro_EmpiricalAcceleration
+        #        test_earth_orientation_EarthOrientationCalculator
+        #        test_earth_orientation_EopReader
+        #        test_earth_orientation_PolarMotionCalculator
+        #        test_earth_orientation_TimeScaleConverter
+        #        test_earth_orientation_ShortPeriodEopCorrections
+        #        test_electromagnetism_PanelledRadiationPressure
+        #        test_interpolators_CubicSplineInterpolator
+        #        test_interpolators_LinearInterpolator
+        #        test_interpolators_MultiLinearInterpolator
+        #        test_integrators_EulerIntegrator
+        #        test_integrators_RungeKutta4Integrator
+        #        test_integrators_RungeKuttaFehlberg45Integrator
+        #        test_integrators_RungeKuttaFehlberg78Integrator
+        #        test_integrators_RungeKutta87DormandPrinceIntegrator
+        #        test_quadrature_GaussianQuadrature
+        #        test_io_MapTextFileReader
+        #        test_io_MatrixTextFileReader
 
         # HERE
-#        test_io_TwoLineElementsTextFileReader
-#        test_io_MissileDatcomReader
-#        test_io_MissileDatcomData
-#        test_io_DictionaryInputSystem
-#        test_io_BasicInputOutput
+        #        test_io_TwoLineElementsTextFileReader
+        #        test_io_MissileDatcomReader
+        #        test_io_MissileDatcomData
+        #        test_io_DictionaryInputSystem
+        #        test_io_BasicInputOutput
 
-#        test_io_MultiArrayReader
-#        test_io_MultiArrayWriter
-#        test_io_AerodynamicCoefficientReader
+        #        test_io_MultiArrayReader
+        #        test_io_MultiArrayWriter
+        #        test_io_AerodynamicCoefficientReader
         )
 
 function("TUDAT_ADD_TEST_CASE" arg1)
@@ -74,7 +102,6 @@ function("TUDAT_ADD_TEST_CASE" arg1)
         # are included first, so that if there is already a pagmo installation
         # in the prefix path we don't risk including the headers from that
         # one instead.
-        message(${PROJECT_BINARY_DIR}/tests/include)
         target_include_directories("${target_name}" PUBLIC
                 $<BUILD_INTERFACE:${PROJECT_BINARY_DIR}/include>  # Configured test headers
                 $<BUILD_INTERFACE:${PROJECT_SOURCE_DIR}/tests/include>  # Test specific headers
@@ -110,15 +137,17 @@ function("TUDAT_ADD_TEST_CASE" arg1)
         set_property(TARGET ${target_name} PROPERTY CXX_EXTENSIONS NO)
         add_test(${target_name} "${PROJECT_BINARY_DIR}/tests/${target_name}")
 
-        #==========================================================================
-        # INSTALL-TREE.
-        #==========================================================================
-        install(TARGETS "${target_name}"
-                EXPORT tudat_export
-                LIBRARY DESTINATION "${INSTALL_LIB_DIR}"
-                ARCHIVE DESTINATION "${INSTALL_LIB_DIR}"
-                RUNTIME DESTINATION "${INSTALL_BIN_DIR}/tudat/tests"
-                )
+        if (TUDAT_INSTALL_TESTS)
+            #==========================================================================
+            # INSTALL-TREE.
+            #==========================================================================
+            install(TARGETS "${target_name}"
+                    EXPORT tudat_export
+                    LIBRARY DESTINATION "${INSTALL_LIB_DIR}"
+                    ARCHIVE DESTINATION "${INSTALL_LIB_DIR}"
+                    RUNTIME DESTINATION "${INSTALL_BIN_DIR}/tudat/tests"
+                    )
+        endif (TUDAT_INSTALL_TESTS)
 
         # Clean up set variables.
         unset(target_name)
