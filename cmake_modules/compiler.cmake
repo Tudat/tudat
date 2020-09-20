8983a9bf94b8360f4bcb071a22c96a28ee732693
Geoffrey H. Garrett <g.h.garrett13@gmail.com>
2020-09-20 14:34:17 +0200

    Update compiler.cmake

--- cmake_modules/compiler.cmake
+++ cmake_modules/compiler.cmake
@@ -90,7 +90,7 @@
      message(STATUS "Using clang compiler.")
 
      # set default cmake c flags for clang
-     set(CMAKE_C_FLAGS "-Wall -std=c11")
+     set(CMAKE_C_FLAGS "-Wall -std=c11 ${CFLAGS}")
      set(CMAKE_C_FLAGS_DEBUG "-g")
      set(CMAKE_C_FLAGS_MINSIZEREL "-Os -DNDEBUG")
      set(CMAKE_C_FLAGS_RELEASE "-O3 -DNDEBUG")
