6a3028777ce75ad54fd80ae167e65fb8b3e08d19
Dominic Dirkx <d.dirkx@tudelft.nl>
2022-11-15 20:33:07 +0100

    Merged

--- include/tudat/simulation/propagation_setup/propagationOutputSettings.h
+++ include/tudat/simulation/propagation_setup/propagationOutputSettings.h
@@ -1203,11 +1203,10 @@ inline std::shared_ptr< AccelerationPartialWrtStateSaveSettings > accelerationPa
 
 inline std::shared_ptr< TotalAccelerationPartialWrtStateSaveSettings > totalAccelerationPartialWrtBodyTranslationalStateDependentVariable(
         const std::string& bodyUndergoingAcceleration,
-        const std::string& bodyExertingAcceleration,
-        const std::string derivativeWrtBody )
+        const std::string& derivativeWrtBody )
 {
     return std::make_shared< TotalAccelerationPartialWrtStateSaveSettings >(
-            bodyUndergoingAcceleration, bodyExertingAcceleration, derivativeWrtBody );
+            bodyUndergoingAcceleration, derivativeWrtBody );
 }
 
 
