7e4b4ed5854fe008bcd36b35bf5ac57d5854537e
Dominic Dirkx <d.dirkx@tudelft.nl>
2022-11-18 12:56:59 +0100

    Corrected unit test tolerance for Mac OS test

--- tests/src/astro/propagators/unitTestVariationalEquations.cpp
+++ tests/src/astro/propagators/unitTestVariationalEquations.cpp
@@ -1238,7 +1238,7 @@ BOOST_AUTO_TEST_CASE( testMassRateVariationalEquations )
             }
             Eigen::VectorXd numericalStatePartialWrtMass = ( ( upPerturbedInitialState - downPerturbedInitialState ) / ( 2.0 * massPerturbation ) ).block( 0, 0, 6, 1 );
             Eigen::VectorXd analyticalStatePartialWrtMass = finalStateTransitionCoupled.block( 0, 6, 6, 1 );
-            TUDAT_CHECK_MATRIX_CLOSE_FRACTION( numericalStatePartialWrtMass, analyticalStatePartialWrtMass, 1.0E-5 );
+            TUDAT_CHECK_MATRIX_CLOSE_FRACTION( numericalStatePartialWrtMass, analyticalStatePartialWrtMass, 1.0E-4 );
         }
     }
 
