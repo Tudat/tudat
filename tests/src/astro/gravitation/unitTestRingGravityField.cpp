8d97ef862bd8337a465e90ea94186a2b76fd669f
MiguelAvillez <mpignatelli.avillez@gmail.com>
2023-03-30 09:23:04 +0200

    Testing ring gravity field

--- tests/src/astro/gravitation/unitTestRingGravityField.cpp
+++ tests/src/astro/gravitation/unitTestRingGravityField.cpp
@@ -126,135 +126,56 @@ BOOST_AUTO_TEST_CASE( testGravityComputation )
     const double gravitationalParameter = 2.39e21 * physical_constants::GRAVITATIONAL_CONSTANT;
     const double ringRadius = 2.7 * physical_constants::ASTRONOMICAL_UNIT;
 
-    for ( bool ellipticIntegralSFromDAndB : { true } )
+    for ( bool ellipticIntegralSFromDAndB : { true, false } )
     {
 
         gravitation::RingGravityField gravityField = gravitation::RingGravityField(
-            gravitationalParameter, ringRadius, ellipticIntegralSFromDAndB);
+            gravitationalParameter, ringRadius, ellipticIntegralSFromDAndB );
 
         Eigen::Vector3d bodyFixedPosition;
         double expectedPotential;
         Eigen::Vector3d expectedGradient;
 
+        // Point at the origin
         (bodyFixedPosition << 0.0, 0.0, 0.0).finished();
         expectedPotential = gravitationalParameter / ringRadius;
         (expectedGradient << 0.0, 0.0, 0.0).finished();
 
-//        BOOST_CHECK_CLOSE_FRACTION(
-//                expectedPotential, gravityField.getGravitationalPotential( bodyFixedPosition ), tolerance );
-//        TUDAT_CHECK_MATRIX_CLOSE_FRACTION(
-//                expectedGradient, gravityField.getGradientOfPotential( bodyFixedPosition ), tolerance );
+        BOOST_CHECK_CLOSE_FRACTION(
+                expectedPotential, gravityField.getGravitationalPotential( bodyFixedPosition ), tolerance );
+        TUDAT_CHECK_MATRIX_CLOSE_FRACTION(
+                expectedGradient, gravityField.getGradientOfPotential( bodyFixedPosition ), tolerance );
 
-//        for ( unsigned int positionId : { 0 } )
-//        {
-//            // Select expected values
-//            if ( positionId == 0 )
-//            {
-//                (bodyFixedPosition << 0.0, 0.0, 0.0).finished();
-//                expectedPotential = 3.19403761604211e-5;
-//                (expectedGradient << 2.31329148957265e-6, 1.91973919943187e-6, 1.91973919943187e-6).finished();
-//            }
-//        }
+        // Point in x=y=0 line. Check potential and direction of acceleration
+        (bodyFixedPosition << 0.0, 0.0, 500.0e3).finished();
+        expectedPotential = gravitationalParameter / std::sqrt( std::pow(ringRadius, 2) + std::pow( bodyFixedPosition(2), 2 ) );
+        (expectedGradient << 0.0, 0.0, TUDAT_NAN).finished();
+
+        BOOST_CHECK_CLOSE_FRACTION(
+                expectedPotential, gravityField.getGravitationalPotential( bodyFixedPosition ), tolerance );
+        TUDAT_CHECK_MATRIX_CLOSE_FRACTION(
+                expectedGradient.segment(0, 2), gravityField.getGradientOfPotential( bodyFixedPosition ).segment(0, 2), tolerance );
+        // Check if z component of acceleration is negative
+        BOOST_CHECK_EQUAL(
+                true, std::signbit( gravityField.getGradientOfPotential( bodyFixedPosition )(2) ) );
+
+        // Point in z=0 plane inside ring. Check direction of acceleration
+        (bodyFixedPosition << 100.0e3, 500.0e3, 0.0).finished();
+        Eigen::Vector3d computedGradient = gravityField.getGradientOfPotential( bodyFixedPosition );
+        BOOST_CHECK_CLOSE_FRACTION(
+                bodyFixedPosition(0) / bodyFixedPosition(1),
+                computedGradient(0) / computedGradient(1), tolerance );
+
+        // Point in z=0 plane outside ring. Check direction of acceleration
+        (bodyFixedPosition << - 2 * ringRadius, 1.3 * ringRadius, 0.0).finished();
+        computedGradient = gravityField.getGradientOfPotential( bodyFixedPosition );
+        BOOST_CHECK_CLOSE_FRACTION(
+                bodyFixedPosition(0) / bodyFixedPosition(1),
+                computedGradient(0) / computedGradient(1), tolerance );
+        BOOST_CHECK_EQUAL(
+                std::signbit( - bodyFixedPosition(0) ), std::signbit( computedGradient(0) ) );
     }
 
-    // Compute potential, gradient, laplacian and hessian, and compare with results from D'Urso (2014)
-
-//    Eigen::Vector3d bodyFixedPosition;
-//    double expectedPotential, computedPotential;
-//    Eigen::Vector3d expectedGradient, computedGradient;
-//    double expectedLaplacian, computedLaplacian;
-//    Eigen::Matrix3d expectedHessian, computedHessian;
-//
-//    for (unsigned int positionId : {0,1,2,3,4,5})
-//    {
-//        bool testPotential = true;
-//        bool testGradient = true;
-//        bool testLaplacian = true;
-//        bool testHessian = true;
-//
-//        // Select expected values
-//        if ( positionId == 0 )
-//        {
-//            (bodyFixedPosition << 0.0, 0.0, 0.0).finished();
-//            expectedPotential = 3.19403761604211e-5;
-//            (expectedGradient << 2.31329148957265e-6, 1.91973919943187e-6, 1.91973919943187e-6).finished();
-//            expectedLaplacian = - 0.5 * mathematical_constants::PI * gravitationalConstant * density;
-//            testHessian = false;
-//        }
-//        else if ( positionId == 1 )
-//        {
-//            (bodyFixedPosition << 5.0, 0.0, 0.0).finished();
-//            expectedPotential = 3.99993558939122e-5;
-//            (expectedGradient << 9.90115534890074e-7, 3.24128042248715e-6, 3.24128042248715e-6).finished();
-//            expectedLaplacian = - 1.0 * mathematical_constants::PI * gravitationalConstant * density;
-//            testHessian = false;
-//        }
-//        else if ( positionId == 2 )
-//        {
-//            (bodyFixedPosition << 0.0, 3.0, 2.0).finished();
-//            expectedPotential = 4.03528375471853e-5;
-//            (expectedGradient << 4.73368592565013e-6, 9.68164362892554e-7, 1.59674500375495e-6).finished();
-//            expectedLaplacian = - 2.0 * mathematical_constants::PI * gravitationalConstant * density;
-//            (expectedHessian <<
-//                -4.02204713784183E-08, 1.87140408935899E-07, 3.51261972418670E-07,
-//                1.87140408935899E-07, -5.01781942367494E-07, 8.58712984897779E-08,
-//                3.51261972418670E-07, 8.58712984897779E-08, -5.77398275537941E-07).finished();
-//        }
-//        else if ( positionId == 3 )
-//        {
-//            (bodyFixedPosition << -5.0, 5.0, 5.0).finished();
-//            expectedLaplacian = 0.0;
-//            testPotential = false;
-//            testGradient = false;
-//            testHessian = false;
-//        }
-//        else if ( positionId == 4 )
-//        {
-//            (bodyFixedPosition << 10.0, 5.0, 5.0).finished();
-//            expectedLaplacian = - 4.0 * mathematical_constants::PI * gravitationalConstant * density;
-//            testPotential = false;
-//            testGradient = false;
-//            testHessian = false;
-//        }
-//        else
-//        {
-//            (bodyFixedPosition << 10.0, 5.0, 10.0 + 1e-10).finished();
-//            expectedLaplacian = 0.0;
-//            testPotential = false;
-//            testGradient = false;
-//            testHessian = false;
-//        }
-//
-//        // Compute values and compare with expected ones
-//        if ( testPotential )
-//        {
-//            computedPotential = gravityField.getGravitationalPotential( bodyFixedPosition );
-//            BOOST_CHECK_CLOSE_FRACTION( expectedPotential, computedPotential, tolerance );
-//        }
-//        if ( testGradient )
-//        {
-//            computedGradient = gravityField.getGradientOfPotential( bodyFixedPosition );
-//            TUDAT_CHECK_MATRIX_CLOSE_FRACTION( expectedGradient, computedGradient, tolerance );
-//        }
-//        if ( testLaplacian )
-//        {
-//            computedLaplacian = gravityField.getLaplacianOfPotential( bodyFixedPosition );
-//            // Expected value is 0 for point 3, so add a constant to the laplacian
-//            if ( positionId == 3 || positionId == 5 )
-//            {
-//                computedLaplacian += 0.1;
-//                expectedLaplacian += 0.1;
-//            }
-//            BOOST_CHECK_CLOSE_FRACTION( expectedLaplacian, computedLaplacian, tolerance );
-//
-//        }
-//        if ( testHessian )
-//        {
-//            computedHessian = gravityField.getHessianOfPotential( bodyFixedPosition );
-//            TUDAT_CHECK_MATRIX_CLOSE_FRACTION( expectedHessian, computedHessian, tolerance );
-//        }
-//
-//    }
 }
 
 BOOST_AUTO_TEST_SUITE_END( )
