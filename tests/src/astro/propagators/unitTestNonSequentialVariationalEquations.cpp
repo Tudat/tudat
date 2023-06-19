a1ef9a0e8d2f009d9516d253d166b255fd065586
MarieFayolle <m.s.fayolle-chambe@tudelft.nl>
2023-06-19 15:50:26 +0200

    added test for non-sequential hybrid arc variational equations solver.

--- tests/src/astro/propagators/unitTestNonSequentialVariationalEquations.cpp
+++ tests/src/astro/propagators/unitTestNonSequentialVariationalEquations.cpp
@@ -409,6 +409,253 @@ BOOST_AUTO_TEST_CASE( testNonSequentialMultiArcVariationalEquations )
 
 }
 
+BOOST_AUTO_TEST_CASE( testNonSequentialHybridArcVariationalEquations )
+{
+    //Load spice kernels.
+    spice_interface::loadStandardSpiceKernels( );
+
+    std::vector< std::string > bodyNames;
+    bodyNames.push_back( "Sun" );
+    bodyNames.push_back( "Jupiter" );
+    bodyNames.push_back( "Saturn" );
+    bodyNames.push_back( "Io" );
+    bodyNames.push_back( "Europa" );
+    bodyNames.push_back( "Ganymede" );
+    bodyNames.push_back( "Callisto" );
+
+    // Specify initial times
+    double initialEpoch = 0.0;
+    double finalEpoch = 86400.0;
+    double midSingleArc = ( finalEpoch + initialEpoch ) / 2.0;
+    std::vector< double > arcStartTimes = { 0.0, 8.0 * 3600.0, 16.0 * 3600.0 };
+    std::vector< double > arcEndTimes = { 7.0 * 3600.0, 15.0 * 3600.0, 86400.0 };
+
+    std::vector< double > midArcTimes;
+    for ( unsigned int i = 0 ; i < arcStartTimes.size( ) ; i++ )
+    {
+        midArcTimes.push_back( ( arcStartTimes.at( i ) + arcEndTimes.at( i ) ) / 2.0 );
+    }
+
+    unsigned int nbArcs = arcStartTimes.size( );
+
+    std::string globalFrameOrientation = "ECLIPJ2000";
+    std::string globalFrameOrigin = "Sun";
+
+    // Create bodies.
+    BodyListSettings bodySettings = getDefaultBodySettings( bodyNames, initialEpoch - 86400.0, finalEpoch + 86400.0 );
+    bodySettings.at( "Jupiter" )->ephemerisSettings = std::make_shared< InterpolatedSpiceEphemerisSettings >(
+            initialEpoch - 86400.0, finalEpoch + 86400.0, 3600.0, globalFrameOrigin, globalFrameOrientation );
+    bodySettings.at( "Io" )->ephemerisSettings = std::make_shared< InterpolatedSpiceEphemerisSettings >(
+            initialEpoch - 86400.0, finalEpoch + 86400.0, 3600.0, globalFrameOrigin, globalFrameOrientation );
+    bodySettings.at( "Europa" )->ephemerisSettings = std::make_shared< InterpolatedSpiceEphemerisSettings >(
+            initialEpoch - 86400.0, finalEpoch + 86400.0, 3600.0, globalFrameOrigin, globalFrameOrientation );
+    bodySettings.at( "Ganymede" )->ephemerisSettings = std::make_shared< InterpolatedSpiceEphemerisSettings >(
+            initialEpoch - 86400.0, finalEpoch + 86400.0, 3600.0, globalFrameOrigin, globalFrameOrientation );
+    bodySettings.at( "Callisto" )->ephemerisSettings = std::make_shared< InterpolatedSpiceEphemerisSettings >(
+            initialEpoch - 86400.0, finalEpoch + 86400.0, 3600.0, globalFrameOrigin, globalFrameOrientation );
+
+    bodySettings.at( "Io" )->ephemerisSettings->resetMakeMultiArcEphemeris( true );
+    bodySettings.at( "Europa" )->ephemerisSettings->resetMakeMultiArcEphemeris( true );
+    bodySettings.at( "Ganymede" )->ephemerisSettings->resetMakeMultiArcEphemeris( true );
+    bodySettings.at( "Callisto" )->ephemerisSettings->resetMakeMultiArcEphemeris( true );
+
+
+    SystemOfBodies bodies = createSystemOfBodies( bodySettings );
+
+    //! Define bodies to propagate.
+    std::vector< std::string > singleArcBodiesToPropagate = { "Jupiter" };
+    std::vector< std::string > singleArcCentralBodies = { "Sun" };
+
+    std::vector< std::string > multiArcBodiesToPropagate = { "Io", "Europa", "Ganymede", "Callisto" };
+    std::vector< std::string > multiArcCentralBodies = { "Jupiter", "Jupiter", "Jupiter", "Jupiter" };
+
+    // Set multi-arc accelerations.
+    SelectedAccelerationMap multiArcAccelerationSettings;
+    for ( unsigned int i = 0 ; i < multiArcBodiesToPropagate.size( ) ; i++ )
+    {
+        std::map< std::string, std::vector< std::shared_ptr< AccelerationSettings > > > accelerationsOfSatellite;
+        accelerationsOfSatellite[ "Jupiter" ].push_back( std::make_shared< MutualSphericalHarmonicAccelerationSettings >( 8, 0, 2, 2  )  );
+        for ( unsigned int j = 0 ; j < multiArcBodiesToPropagate.size( ) ; j++ )
+        {
+            if ( i != j )
+            {
+                accelerationsOfSatellite[ multiArcBodiesToPropagate[ j ] ].push_back( std::make_shared< MutualSphericalHarmonicAccelerationSettings >( 2, 2, 2, 2, 8, 0 ) );
+            }
+        }
+        multiArcAccelerationSettings[ multiArcBodiesToPropagate[ i ] ] = accelerationsOfSatellite;
+    }
+
+    basic_astrodynamics::AccelerationMap multiArcAccelerationsMap = createAccelerationModelsMap(
+            bodies, multiArcAccelerationSettings, multiArcBodiesToPropagate, multiArcCentralBodies );
+
+    // Set single-arc accelerations.
+    SelectedAccelerationMap singleArcAccelerationSettings;
+    singleArcAccelerationSettings[ "Jupiter" ][ "Sun" ].push_back( std::make_shared< AccelerationSettings >( basic_astrodynamics::point_mass_gravity ) );
+    singleArcAccelerationSettings[ "Jupiter" ][ "Saturn" ].push_back( std::make_shared< AccelerationSettings >( basic_astrodynamics::point_mass_gravity ) );
+    basic_astrodynamics::AccelerationMap singleArcAccelerationsMap = createAccelerationModelsMap(
+            bodies, singleArcAccelerationSettings, singleArcBodiesToPropagate, singleArcCentralBodies );
+
+    // Define integrator settings
+    double timeStep = 3600.0;
+    std::shared_ptr< numerical_integrators::IntegratorSettings< > > forwardIntegratorSettings =
+            std::make_shared< numerical_integrators::RungeKuttaVariableStepSizeSettingsScalarTolerances< double > >(
+                    midSingleArc, timeStep, CoefficientSets::rungeKuttaFehlberg78, timeStep, timeStep, 1.0e3, 1.0e3 );
+
+    std::shared_ptr< numerical_integrators::IntegratorSettings< > > backwardIntegratorSettings =
+            std::make_shared< numerical_integrators::RungeKuttaVariableStepSizeSettingsScalarTolerances< double > >(
+                    midSingleArc, - timeStep, CoefficientSets::rungeKuttaFehlberg78, - timeStep, - timeStep, 1.0e3, 1.0e3 );
+
+    // Define arc-wise initial states
+    std::vector< Eigen::VectorXd > midArcStatesMoons;
+    Eigen::VectorXd arcWiseMidStatesIo, arcWiseMidStatesEuropa, arcWiseMidStatesGanymede, arcWiseMidStatesCallisto;
+    arcWiseMidStatesIo.resize( 6 * nbArcs );
+    arcWiseMidStatesEuropa.resize( 6 * nbArcs );
+    arcWiseMidStatesGanymede.resize( 6 * nbArcs );
+    arcWiseMidStatesCallisto.resize( 6 * nbArcs );
+    for ( unsigned int i = 0 ; i < nbArcs ; i++ )
+    {
+        midArcStatesMoons.push_back( propagators::getInitialStatesOfBodies( multiArcBodiesToPropagate, multiArcCentralBodies, bodies, midArcTimes.at( i ) ) );
+        arcWiseMidStatesIo.segment( i * 6, 6 ) = midArcStatesMoons[ i ].segment( 0, 6 );
+        arcWiseMidStatesEuropa.segment( i * 6, 6 ) = midArcStatesMoons[ i ].segment( 6, 6 );
+        arcWiseMidStatesGanymede.segment( i * 6, 6 ) = midArcStatesMoons[ i ].segment( 12, 6 );
+        arcWiseMidStatesCallisto.segment( i * 6, 6 ) = midArcStatesMoons[ i ].segment( 18, 6 );
+    }
+    std::map< std::string, Eigen::VectorXd > concatenatedArcWiseStatesPerBody;
+    concatenatedArcWiseStatesPerBody[ "Io" ] =  arcWiseMidStatesIo;
+    concatenatedArcWiseStatesPerBody[ "Europa" ] =  arcWiseMidStatesEuropa;
+    concatenatedArcWiseStatesPerBody[ "Ganymede" ] =  arcWiseMidStatesGanymede;
+    concatenatedArcWiseStatesPerBody[ "Callisto" ] =  arcWiseMidStatesCallisto;
+
+    // Define single-arc initial states
+    Eigen::VectorXd midArcStatesJupiter = propagators::getInitialStatesOfBodies( singleArcBodiesToPropagate, singleArcCentralBodies, bodies, midSingleArc );
+
+    // Define multi-arc propagator settings lists.
+    std::vector< std::shared_ptr< SingleArcPropagatorSettings< double > > > forwardPropagationSettingsList, backwardPropagationSettingsList, nonSequentialPropagationSettingsList;
+    for ( unsigned int i = 0 ; i < nbArcs ; i++ )
+    {
+        forwardPropagationSettingsList.push_back( std::make_shared< TranslationalStatePropagatorSettings< double > >(
+                multiArcCentralBodies, multiArcAccelerationsMap, multiArcBodiesToPropagate, midArcStatesMoons.at( i ), midArcTimes.at( i ), forwardIntegratorSettings,
+                std::make_shared< PropagationTimeTerminationSettings >( arcEndTimes.at( i ) ) ) );
+        backwardPropagationSettingsList.push_back( std::make_shared< TranslationalStatePropagatorSettings< double > >(
+                multiArcCentralBodies, multiArcAccelerationsMap, multiArcBodiesToPropagate, midArcStatesMoons.at( i ), midArcTimes.at( i ), backwardIntegratorSettings,
+                std::make_shared< PropagationTimeTerminationSettings >( arcStartTimes.at( i ) ) ) );
+
+        std::shared_ptr< NonSequentialPropagationTerminationSettings > terminationSettings = std::make_shared< NonSequentialPropagationTerminationSettings >(
+                std::make_shared< PropagationTimeTerminationSettings >( arcEndTimes.at( i ) ),
+                std::make_shared< PropagationTimeTerminationSettings >( arcStartTimes.at( i ) ) );
+        std::shared_ptr< TranslationalStatePropagatorSettings< > > nonSequentialPropagatorSettings = std::make_shared< TranslationalStatePropagatorSettings< > >(
+                multiArcCentralBodies, multiArcAccelerationsMap, multiArcBodiesToPropagate, midArcStatesMoons.at( i ),
+                midArcTimes.at( i ), forwardIntegratorSettings, terminationSettings );
+        nonSequentialPropagationSettingsList.push_back( nonSequentialPropagatorSettings );
+    }
+
+    // Create single-arc propagator settings
+    std::shared_ptr< TranslationalStatePropagatorSettings< > > forwardSingleArcPropagatorSettings = std::make_shared< TranslationalStatePropagatorSettings< > >(
+            singleArcCentralBodies, singleArcAccelerationsMap, singleArcBodiesToPropagate, midArcStatesJupiter, midSingleArc, forwardIntegratorSettings,
+            std::make_shared< PropagationTimeTerminationSettings >( finalEpoch ) );
+    std::shared_ptr< TranslationalStatePropagatorSettings< > > backwardSingleArcPropagatorSettings = std::make_shared< TranslationalStatePropagatorSettings< > >(
+            singleArcCentralBodies, singleArcAccelerationsMap, singleArcBodiesToPropagate, midArcStatesJupiter, midSingleArc, backwardIntegratorSettings,
+            std::make_shared< PropagationTimeTerminationSettings >( initialEpoch ) );
+    
+    std::shared_ptr< NonSequentialPropagationTerminationSettings > terminationSettings = std::make_shared< NonSequentialPropagationTerminationSettings >(
+            std::make_shared< PropagationTimeTerminationSettings >( finalEpoch ),
+            std::make_shared< PropagationTimeTerminationSettings >( initialEpoch ) );
+    std::shared_ptr< TranslationalStatePropagatorSettings< > > nonSequentialSingleArcPropagatorSettings = std::make_shared< TranslationalStatePropagatorSettings< > >(
+            singleArcCentralBodies, singleArcAccelerationsMap, singleArcBodiesToPropagate, midArcStatesJupiter, midSingleArc, forwardIntegratorSettings, terminationSettings );
+
+    // Create hybrid-arc propagator settings
+    std::shared_ptr< HybridArcPropagatorSettings< > > forwardPropagatorSettings = std::make_shared< HybridArcPropagatorSettings< > >(
+            forwardSingleArcPropagatorSettings, std::make_shared< MultiArcPropagatorSettings< > >( forwardPropagationSettingsList ) );
+    std::shared_ptr< HybridArcPropagatorSettings< > > backwardPropagatorSettings = std::make_shared< HybridArcPropagatorSettings< > >(
+            backwardSingleArcPropagatorSettings, std::make_shared< MultiArcPropagatorSettings< > >( backwardPropagationSettingsList ) );
+
+    std::shared_ptr< HybridArcPropagatorSettings< > > nonSequentialPropagatorSettings = std::make_shared< HybridArcPropagatorSettings< > >(
+            nonSequentialSingleArcPropagatorSettings, std::make_shared< MultiArcPropagatorSettings< > >( nonSequentialPropagationSettingsList ) );
+
+        // Define parameters to estimate
+        std::vector< std::shared_ptr< EstimatableParameterSettings > > parameterNames;
+        parameterNames.push_back( std::make_shared< InitialTranslationalStateEstimatableParameterSettings< double > >(
+                singleArcBodiesToPropagate.at( 0 ), midArcStatesJupiter, singleArcCentralBodies.at( 0 ) ) );
+
+        for( unsigned int i = 0; i < multiArcBodiesToPropagate.size( ); i++ )
+        {
+            parameterNames.push_back( std::make_shared< ArcWiseInitialTranslationalStateEstimatableParameterSettings< double > >(
+                    multiArcBodiesToPropagate.at( i ), concatenatedArcWiseStatesPerBody.at( multiArcBodiesToPropagate.at( i ) ), midArcTimes, multiArcCentralBodies.at( i ) ) );
+
+            parameterNames.push_back( std::make_shared< EstimatableParameterSettings >(
+                    multiArcBodiesToPropagate.at( i ), gravitational_parameter ) );
+
+            parameterNames.push_back( std::make_shared< SphericalHarmonicEstimatableParameterSettings >(
+                    2, 0, 2, 2, multiArcBodiesToPropagate.at( i ), spherical_harmonics_cosine_coefficient_block ) );
+            parameterNames.push_back( std::make_shared< SphericalHarmonicEstimatableParameterSettings >(
+                    2, 1, 2, 2, multiArcBodiesToPropagate.at( i ), spherical_harmonics_sine_coefficient_block ) );
+        }
+
+        std::shared_ptr< estimatable_parameters::EstimatableParameterSet< double > > forwardPropagationParametersToEstimate =
+                createParametersToEstimate< double >( parameterNames, bodies, forwardPropagatorSettings );
+        printEstimatableParameterEntries( forwardPropagationParametersToEstimate );
+
+        std::shared_ptr< estimatable_parameters::EstimatableParameterSet< double > > backwardPropagationParametersToEstimate =
+                createParametersToEstimate< double >( parameterNames, bodies, backwardPropagatorSettings );
+
+        std::shared_ptr< estimatable_parameters::EstimatableParameterSet< double > > nonSequentialPropagationParametersToEstimate =
+                createParametersToEstimate< double >( parameterNames, bodies, nonSequentialPropagatorSettings );
+
+        // Propagate variational equations (forward leg)
+        HybridArcVariationalEquationsSolver< > forwardVariationalEquationsSolver = HybridArcVariationalEquationsSolver< >(
+                bodies, forwardPropagatorSettings, forwardPropagationParametersToEstimate, true );
+
+        // Propagate variational equations (backward leg)
+        HybridArcVariationalEquationsSolver< > backwardVariationalEquationsSolver = HybridArcVariationalEquationsSolver< >(
+                bodies, backwardPropagatorSettings, backwardPropagationParametersToEstimate, true );
+
+        // Propagate variational equations non-sequentially from mid-arc.
+        HybridArcVariationalEquationsSolver< > nonSequentialVariationalEquationsSolver = HybridArcVariationalEquationsSolver< >(
+                bodies, nonSequentialPropagatorSettings, nonSequentialPropagationParametersToEstimate, true );
+
+        std::vector< std::map< double, Eigen::MatrixXd > > forwardSingleArcVariationalHistory =
+                forwardVariationalEquationsSolver.getSingleArcSolver( )->getNumericalVariationalEquationsSolution( );
+        std::vector< std::map< double, Eigen::MatrixXd > > backwardSingleArcVariationalHistory =
+                backwardVariationalEquationsSolver.getSingleArcSolver( )->getNumericalVariationalEquationsSolution( );
+        std::vector< std::map< double, Eigen::MatrixXd > > nonSequentialSingleArcVariationalHistory =
+                nonSequentialVariationalEquationsSolver.getSingleArcSolver( )->getNumericalVariationalEquationsSolution( );
+
+        std::vector< std::vector< std::map< double, Eigen::MatrixXd > > > forwardMultiArcVariationalHistory =
+                forwardVariationalEquationsSolver.getMultiArcSolver( )->getNumericalVariationalEquationsSolution( );
+        std::vector< std::vector< std::map< double, Eigen::MatrixXd > > > backwardMultiArcVariationalHistory =
+                backwardVariationalEquationsSolver.getMultiArcSolver( )->getNumericalVariationalEquationsSolution( );
+        std::vector< std::vector< std::map< double, Eigen::MatrixXd > > > nonSequentialMultiArcVariationalHistory =
+                nonSequentialVariationalEquationsSolver.getMultiArcSolver( )->getNumericalVariationalEquationsSolution( );
+
+        BOOST_CHECK_EQUAL( nonSequentialSingleArcVariationalHistory[ 0 ].size( ),
+                           forwardSingleArcVariationalHistory[ 0 ].size( ) + backwardSingleArcVariationalHistory[ 0 ].size( ) - 1 );
+        BOOST_CHECK_EQUAL( nonSequentialSingleArcVariationalHistory[ 1 ].size( ),
+                           forwardSingleArcVariationalHistory[ 1 ].size( ) + backwardSingleArcVariationalHistory[ 1 ].size( ) - 1 );
+
+        TUDAT_CHECK_MATRIX_CLOSE_FRACTION( nonSequentialSingleArcVariationalHistory[ 0 ].begin( )->second, backwardSingleArcVariationalHistory[ 0 ].begin( )->second, std::numeric_limits< double >::epsilon( ) );
+        TUDAT_CHECK_MATRIX_CLOSE_FRACTION( nonSequentialSingleArcVariationalHistory[ 1 ].begin( )->second, backwardSingleArcVariationalHistory[ 1 ].begin( )->second, std::numeric_limits< double >::epsilon( ) );
+        TUDAT_CHECK_MATRIX_CLOSE_FRACTION( nonSequentialSingleArcVariationalHistory[ 0 ].rbegin( )->second, forwardSingleArcVariationalHistory[ 0 ].rbegin( )->second, std::numeric_limits< double >::epsilon( ) );
+        TUDAT_CHECK_MATRIX_CLOSE_FRACTION( nonSequentialSingleArcVariationalHistory[ 1 ].rbegin( )->second, forwardSingleArcVariationalHistory[ 1 ].rbegin( )->second, std::numeric_limits< double >::epsilon( ) );
+
+        for ( unsigned int arc = 0 ; arc < arcStartTimes.size( ) ; arc++ )
+        {
+            BOOST_CHECK_EQUAL( nonSequentialMultiArcVariationalHistory.at( arc )[ 0 ].size( ),
+                               forwardMultiArcVariationalHistory.at( arc )[ 0 ].size( ) + backwardMultiArcVariationalHistory.at( arc )[ 0 ].size( ) - 1 );
+            BOOST_CHECK_EQUAL( nonSequentialMultiArcVariationalHistory.at( arc )[ 1 ].size( ),
+                               forwardMultiArcVariationalHistory.at( arc )[ 1 ].size( ) + backwardMultiArcVariationalHistory.at( arc )[ 1 ].size( ) - 1 );
+
+            TUDAT_CHECK_MATRIX_CLOSE_FRACTION( nonSequentialMultiArcVariationalHistory.at( arc )[ 0 ].begin( )->second,
+                                               backwardMultiArcVariationalHistory.at( arc )[ 0 ].begin( )->second, std::numeric_limits< double >::epsilon( ) );
+            TUDAT_CHECK_MATRIX_CLOSE_FRACTION( nonSequentialMultiArcVariationalHistory.at( arc )[ 1 ].begin( )->second,
+                                               backwardMultiArcVariationalHistory.at( arc )[ 1 ].begin( )->second, std::numeric_limits< double >::epsilon( ) );
+            TUDAT_CHECK_MATRIX_CLOSE_FRACTION( nonSequentialMultiArcVariationalHistory.at( arc )[ 0 ].rbegin( )->second,
+                                               forwardMultiArcVariationalHistory.at( arc )[ 0 ].rbegin( )->second, std::numeric_limits< double >::epsilon( ) );
+            TUDAT_CHECK_MATRIX_CLOSE_FRACTION( nonSequentialMultiArcVariationalHistory.at( arc )[ 1 ].rbegin( )->second,
+                                               forwardMultiArcVariationalHistory.at( arc )[ 1 ].rbegin( )->second, std::numeric_limits< double >::epsilon( ) );
+        }
+
+}
+
 BOOST_AUTO_TEST_SUITE_END( )
 
 }
